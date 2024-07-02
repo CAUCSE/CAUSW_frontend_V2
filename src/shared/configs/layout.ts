5df1892b5fdbc9f70cb8be8f1e6a62b366d95f1d
selfishAltruism <officialkyus@gmail.com>
2024-07-02 16:51:41 +0900

    chore: fix media query (#1)

--- src/shared/configs/layout.ts
+++ src/shared/configs/layout.ts
@@ -1,7 +1,8 @@
 export const breakpoint = { sm: 576, md: 768, lg: 992, xl: 1200 };
+
 export const media = Object.fromEntries(
   Object.entries(breakpoint).map(([key, value]) => [
     key,
-    `@media (min-width: ${value}px)`,
+    `@media (max-width: ${value}px)`,
   ])
 );
