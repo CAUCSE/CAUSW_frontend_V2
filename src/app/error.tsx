4179ec0fcd47b85811e6d8322dddbf4e796feeab
selfishAltruism <officialkyus@gmail.com>
2024-07-18 16:54:17 +0900

    feat: error page implement (#1)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,6 +5,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 import {
   useLayoutStore,
@@ -30,15 +31,25 @@ const Error = ({
   }, [error, router, setErrorMessage]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center text-xl font-bold">
+      <Image
+        src="/images/puang-proud.png"
+        alt="404"
+        width={200}
+        height={250}
+      ></Image>
+      <span>일시적으로 서비스를 이용할 수 없습니다.</span>
+      <span>문제가 지속적으로 발생하는 경우</span>
+      <span>아래 이메일로 문의해주세요.</span>
+      <span>( caucsedongne@gmail.com )</span>
       <button
+        className="w-40 h-12 rounded-xl bg-focus text-lg text-white text-center mt-3"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
-        Try again
+        Try Again...
       </button>
     </div>
   );
