b8f8ab0365c8d7423d28fd855194ce7d9bd43979
omjinLTS <omjin7g@gmail.com>
2024-07-22 00:19:35 +0900

    feat: homepage Banner component (#8)
    Using Swiper

--- src/entities/home/banner/Banner.tsx
+++ src/entities/home/banner/Banner.tsx
@@ -12,29 +12,27 @@ interface BannerProps {
 
 export function Banner({ images }: BannerProps) {
   return (
-    <>
-      <Swiper
-        pagination={true}
-        modules={[Pagination, Autoplay]}
-        autoplay={true}
-        loop={true}
-        className='w-[60vw] h-[8.526vw] rounded-2xl border'
-      >
-        {images.map((image, idx) => (
-          <SwiperSlide
-            key={idx}
-            className='text-center flex justify-center items-center'
-          >
-            <Image
-              src={image}
-              alt={image}
-              width={1004 * 10}
-              height={141 * 10}
-              className='object-scale-down object-center h-full'
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+    <Swiper
+      pagination={true}
+      modules={[Pagination, Autoplay]}
+      autoplay={true}
+      loop={true}
+      className='w-[60vw] h-[8.526vw] rounded-2xl border'
+    >
+      {images.map((image, idx) => (
+        <SwiperSlide
+          key={idx}
+          className='text-center flex justify-center items-center'
+        >
+          <Image
+            src={image}
+            alt={image}
+            width={1004 * 10}
+            height={141 * 10}
+            className='object-scale-down object-center h-full'
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
