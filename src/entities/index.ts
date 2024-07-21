0a821f87b9594b20d617270fead8540589befe42
omjinLTS <omjin7g@gmail.com>
2024-07-22 00:19:35 +0900

    feat: homepage Banner component (#8)
    Using Swiper

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,12 +1,13 @@
+export { ErrorMessage } from "./layout/ErrorMessage";
 export { LoadingComponent } from "./layout/LoadingComponent";
 export { NavigationBar } from "./layout/NavigationBar";
-export { ErrorMessage } from "./layout/ErrorMessage";
 export { ProfileImage } from "./layout/ProfileImage";
 
-export { VideoBackground } from "./background/VideoBackground";
 export { ImageBackground } from "./background/ImageBackground";
+export { VideoBackground } from "./background/VideoBackground";
 
 export { FormInput, FormSubmitButton } from "./input/FormInput";
 
 export { Header } from "./header/Header";
 export { SubHeader } from "./header/SubHeader";
+export { Banner } from "./home/banner/Banner";
