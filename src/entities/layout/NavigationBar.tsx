1d1f38b45d0e0cfe4a6763f976fbedb64c74c803
selfishAltruism <officialkyus@gmail.com>
2024-07-24 19:49:09 +0900

    feat: response design navigation bar (#1)

--- src/entities/layout/NavigationBar.tsx
+++ src/entities/layout/NavigationBar.tsx
@@ -1,33 +1,38 @@
 "use client";
 
-import Image from "next/image";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import Link from "next/link";
+
+import { useLayoutStore } from "@/shared";
 
 export const NavigationBar = () => {
   const firstRouter = `/${usePathname().split("/")[1]}`;
+  const sm = useLayoutStore((state) => state.sm);
 
   return (
-    <div className="w-40 h-screen fixed top-0 left-0 bg-default flex flex-col justify-center items-end space-y-10 rounded-r-3xl">
-      <Image
-        src="/images/cau-logo.png"
-        alt="cau logo"
-        width={95}
-        height={54}
-        className="absolute top-6 left-1/2 transform -translate-x-1/2"
-      />
+    <div className="w-full h-20 md:w-40 md:h-screen fixed bottom-0 md:top-0 left-0 bg-default flex md:flex-col justify-center items-center md:items-end space-x-3 md:space-y-10 rounded-t-3xl md:rounded-r-3xl">
+      {sm ? null : (
+        <Image
+          src="/images/cau-logo.png"
+          alt="cau logo"
+          width={95}
+          height={54}
+          className="absolute top-6 left-1/2 transform -translate-x-1/2"
+        />
+      )}
       {icons.map((iconClass) => (
         <Link
           key={iconClass.href}
           href={iconClass.href}
-          className={`w-11/12 h-24 ${
+          className={`w-20 md:w-11/12 h-16 md:h-24 ${
             firstRouter === iconClass.href ? "bg-white" : "bg-default"
-          } rounded-l-3xl flex flex-col justify-center items-center`}
+          } rounded-2xl md:rounded-l-3xl md:rounded-r-none flex flex-col justify-center items-center`}
         >
           <span
             className={`${iconClass.icon} ${
               firstRouter === iconClass.href ? "text-default" : "text-white"
-            } text-5xl mr-3`}
+            } text-5xl md:mr-3`}
           ></span>
         </Link>
       ))}
