86f1486cc0be5818800c8bcd763418087c2f7fed
selfishAltruism <officialkyus@gmail.com>
2024-07-24 18:00:12 +0900

    design: response design in sign in page (#1)

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -51,9 +51,9 @@ const SignInPage = () => {
         alt="sign in page background img"
         darkBackground
       />
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center">
+      <div className="absolute top-1/3 left-1/2 w-full transform -translate-x-1/2 flex flex-col justify-center items-center">
         <div className="text-white text-xl mb-4">함께라면 더 밝은 미래로</div>
-        <div className="text-white text-5xl font-bold tracking-widest mb-10">
+        <div className="text-center text-white text-3xl sm:text-5xl font-bold tracking-widest mb-10">
           우리들의 동문 네트워크
         </div>
         <form
