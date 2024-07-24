06618f91e3e4ada752a0b639f6f1b26fd4054a3e
selfishAltruism <officialkyus@gmail.com>
2024-07-24 20:03:33 +0900

    feat: response design side bar (#1)

--- src/widget/SideBar.tsx
+++ src/widget/SideBar.tsx
@@ -3,11 +3,17 @@
 import { useEffect } from "react";
 
 import { ProfileImage, Header, SubHeader } from "@/entities";
-import { UserService, useUserStore, AuthRscService } from "@/shared";
+import {
+  UserService,
+  useUserStore,
+  AuthRscService,
+  useLayoutStore,
+} from "@/shared";
 
 export const SideBar = () => {
   const { getUserInfo } = UserService();
   const { signout } = AuthRscService();
+  const sm = useLayoutStore((state) => state.sm);
 
   const name = useUserStore((state) => state.name);
   const email = useUserStore((state) => state.email);
@@ -18,21 +24,24 @@ export const SideBar = () => {
 
   return (
     <>
-      <div className="w-72 h-screen fixed top-0 right-0 flex flex-col justify-center items-center space-y-4">
-        <div className="absolute flex flex-col items-center top-11 right-11 text-black">
+      <div className="w-full md:w-72 h-28 md:h-screen fixed top-0 right-0 flex md:flex-col justify-end md:justify-center items-center space-y-4">
+        <div className="absolute flex flex-col items-center top-2 md:top-11 left-2 md:right-11 text-black">
           <span
-            className="icon-[codicon--sign-out] text-4xl"
+            className="icon-[codicon--sign-out] text-2xl md:text-4xl"
             onClick={() => {
               signout();
             }}
           ></span>
-          <span className="text-sm text-black underline">로그아웃</span>
+          <span className="text-xs md:text-sm text-black underline">
+            로그아웃
+          </span>
         </div>
-        <ProfileImage />
+        {sm ? null : <ProfileImage />}
         <div className="flex flex-col items-center">
           <Header wide>{name}</Header>
           <SubHeader gray>{email}</SubHeader>
         </div>
+        {sm ? <ProfileImage /> : null}
       </div>
     </>
   );
