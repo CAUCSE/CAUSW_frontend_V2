b076664b2ac4c0bab8ab0f8682a6ae9f6ee6c997
selfishAltruism <officialkyus@gmail.com>
2024-08-12 18:24:56 +0900

    feat: complete circle element (#13)

--- src/entities/element/CircleElement.tsx
+++ src/entities/element/CircleElement.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 
 export const CircleElement = ({
   id,
+  mainImage,
+  name,
+  description,
 }: {
   id: string;
   name: string;
@@ -9,6 +12,18 @@ export const CircleElement = ({
   description: string;
 }) => (
   <Link href={`circle/${id}`}>
-    <div className="w-40 h-40 bg-red-200"></div>
+    <div className="relative w-72 h-80 bg-white flex-col items-center border-2 rounded-lg flex justify-center">
+      <div className="w-52 h-52 flex justify-center items-center overflow-hidden rounded-xl">
+        <img
+          src={mainImage}
+          alt={"Circle Image"}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <span className="font-bold text-lg mt-4">{name}</span>
+      <span className="text-gray-400 text-center truncate w-3/4 mt-1">
+        {description}
+      </span>
+    </div>
   </Link>
 );
