53648c37d78af9160d49894083b342a0dc7b93c2
lee0jae330 <xingxing2001@cau.ac.kr>
2024-08-16 02:08:41 +0900

    chore: 모달창 외부 배경 클릭 시 창 닫힘 기능 삭제(#6)

--- src/shared/ui/modal.tsx
+++ src/shared/ui/modal.tsx
@@ -7,10 +7,7 @@ interface IModalProps {
 
 export const Modal = ({ closeModal, children }: IModalProps) => {
   return (
-    <div
-      className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 p-4"
-      onClick={closeModal}
-    >
+    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 p-4">
       <div className="relative flex flex-col items-center rounded-lg bg-white p-8 md:w-1/2">
         <button
           className="absolute left-0 top-0 px-4 py-2 text-xl font-bold"
