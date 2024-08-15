dc87d664a8fb09df4dca7d024fb650df6a694a78
selfishAltruism <officialkyus@gmail.com>
2024-08-15 15:22:42 +0900

    design: scroll bar (#13)

--- src/app/(causw)/circle/page.tsx
+++ src/app/(causw)/circle/page.tsx
@@ -8,13 +8,13 @@ const CirclePage = async () => {
   return (
     <>
       <div
-        className="absolute top-32 left-0 w-full h-full transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
-        md:top-0 md:left-40 md:w-[71%]"
+        className="absolute top-32 left-0 w-full h-3/4 transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
+        md:top-0 md:left-40 md:w-[71%] md:h-screen"
       >
-        <div className="text-2xl font-bold w-72 ml-16 mt-6 md:mt-16">
+        <div className="text-2xl font-bold w-72 ml-20 mt-6 md:mt-16">
           동아리 목록
         </div>
-        <div className="flex flex-row flex-wrap justify-center">
+        <div className="flex flex-row flex-wrap ml-16">
           {data.map((circle) => (
             <CircleElement
               key={circle.id}
