ff602db0e61d4632a09f04fea1c963740141efa5
selfishAltruism <officialkyus@gmail.com>
2024-08-22 16:44:53 +0900

    design: main contianer design update (#16)

--- src/app/(causw)/layout.tsx
+++ src/app/(causw)/layout.tsx
@@ -11,8 +11,8 @@ export default function HomeLayout({
       <NavigationBar />
       <SideBar />
       <div
-        className="absolute top-32 left-0 w-full h-3/4 transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
-        lg:top-0 lg:left-40 lg:w-[71%] lg:h-screen"
+        className="absolute top-32 left-0 w-full h-[calc(100%-14rem)] transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
+        lg:top-0 lg:left-40 lg:w-[calc(100%-29rem)] lg:h-screen"
       >
         {children}
       </div>
