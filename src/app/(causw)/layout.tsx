9da362e320da0d774c875a8389c802b8e261e598
selfishAltruism <officialkyus@gmail.com>
2024-08-22 16:35:39 +0900

    chore: implement main container (#16)

--- src/app/(causw)/layout.tsx
+++ src/app/(causw)/layout.tsx
@@ -10,7 +10,12 @@ export default function HomeLayout({
     <>
       <NavigationBar />
       <SideBar />
-      {children}
+      <div
+        className="absolute top-32 left-0 w-full h-3/4 transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
+        lg:top-0 lg:left-40 lg:w-[71%] lg:h-screen"
+      >
+        {children}
+      </div>
     </>
   );
 }
