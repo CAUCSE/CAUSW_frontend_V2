c7ca38b8e78c229be49ec65a958ce7aac8ac40a8
selfishAltruism <officialkyus@gmail.com>
2024-08-24 15:48:38 +0900

    design: circle main, detail page (#13)

--- src/app/(causw)/circle/[id]/page.tsx
+++ src/app/(causw)/circle/[id]/page.tsx
@@ -10,47 +10,44 @@ const Circle = async ({ params: { id } }: { params: { id: string } }) => {
 
   return (
     <>
-      <div
-        className="flex items-center justify-center absolute top-32 left-0 w-full h-3/4 transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
-        md:top-0 md:left-40 md:w-[71%] md:h-screen"
-      >
-        <div className="w-5/6 h-5/6 grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_5fr_1fr] gap-4">
+      <div className="flex w-full items-center justify-center overflow-y-scroll md:h-screen">
+        <div className="grid h-5/6 w-5/6 grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_5fr_1fr] gap-4">
           <Header bold>{data.name}</Header>
-          <div className="col-span-2 flex justify-end items-center gap-3">
-            <div className="w-48 h-full flex rounded-2xl items-center justify-center text-lg border-2 border-black">
+          <div className="col-span-2 flex items-center justify-end gap-3">
+            <div className="flex h-full w-48 items-center justify-center rounded-2xl border-2 border-black text-lg">
               가입 신청 받기
             </div>
-            <div className="w-48 h-full flex rounded-2xl items-center justify-center text-lg border-2 border-black">
+            <div className="flex h-full w-48 items-center justify-center rounded-2xl border-2 border-black text-lg">
               신청 현황 보기
             </div>
             <span className="icon-[icon-park-outline--write] text-4xl"></span>
           </div>
 
-          <div className="row-span-3 w-64 h-64 flex justify-center items-center overflow-hidden rounded-xl">
+          <div className="row-span-3 flex h-64 w-64 items-center justify-center overflow-hidden rounded-xl">
             <img
               src={data.mainImage ?? "/images/signin-logo.png"}
               alt={"Circle Image"}
-              className="w-full h-full object-cover"
+              className="h-full w-full object-cover"
             />
           </div>
 
           <div className="col-span-2 flex flex-row items-end text-lg">
-            <div className="font-bold mr-6">동아리 회비</div>
+            <div className="mr-6 font-bold">동아리 회비</div>
             <div>40000만원</div>
           </div>
 
           <div className="col-span-2 flex flex-row items-center text-lg">
-            <div className="font-bold mr-[39px]">모집 인원</div>
+            <div className="mr-[39px] font-bold">모집 인원</div>
             <div>40000만원</div>
           </div>
 
           <div className="col-span-2 flex flex-row items-start text-lg">
-            <div className="font-bold mr-[39px]">동아리원</div>
+            <div className="mr-[39px] font-bold">동아리원</div>
             <div>{data.numMember}명</div>
           </div>
 
-          <div className="flex flex-col items-center w-32 gap-2">
-            <div className="text-2xl font-bold mt-6 mb-6">운영진</div>
+          <div className="flex w-32 flex-col items-center gap-2">
+            <div className="mb-6 mt-6 text-2xl font-bold">운영진</div>
             <ProfileImage src={leader.profileImage}></ProfileImage>
             <SubHeader bold>
               회장 {data.leaderName} ({leader.admissionYear % 100})
@@ -58,17 +55,17 @@ const Circle = async ({ params: { id } }: { params: { id: string } }) => {
           </div>
 
           <div className="col-span-2">
-            <div className="text-2xl font-bold mt-6 mb-6">설명</div>
+            <div className="mb-6 mt-6 text-2xl font-bold">설명</div>
             <div dangerouslySetInnerHTML={{ __html: data.description }} />
           </div>
 
-          <div className="flex bg-account rounded-2xl items-center justify-center text-lg text-white">
+          <div className="flex items-center justify-center rounded-2xl bg-account text-lg text-white">
             신청하기
           </div>
-          <div className="flex bg-barkblue rounded-2xl items-center justify-center text-lg text-white">
+          <div className="flex items-center justify-center rounded-2xl bg-barkblue text-lg text-white">
             동아리 게시판
           </div>
-          <div className="flex bg-default rounded-2xl items-center justify-center text-lg text-white">
+          <div className="flex items-center justify-center rounded-2xl bg-default text-lg text-white">
             부원 명단 보기
           </div>
         </div>
--- src/app/(causw)/circle/page.tsx
+++ src/app/(causw)/circle/page.tsx
@@ -7,26 +7,21 @@ const CirclePage = async () => {
 
   return (
     <>
-      <div
-        className="absolute top-32 left-0 w-full h-3/4 transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
-        md:top-0 md:left-40 md:w-[71%] md:h-screen"
-      >
-        <div className="text-2xl font-bold w-72 ml-20 mt-6 md:mt-16">
-          동아리 목록
-        </div>
-        <div className="flex flex-row flex-wrap ml-16">
-          {data.map((circle) =>
-            !circle.isDeleted ? (
-              <CircleElement
-                key={circle.id}
-                id={circle.id}
-                name={circle.name}
-                description={circle.description}
-                mainImage={circle.mainImage ?? "/images/signin-logo.png"}
-              />
-            ) : null
-          )}
-        </div>
+      <div className="ml-20 mt-6 w-72 text-2xl font-bold md:mt-16">
+        동아리 목록
+      </div>
+      <div className="ml-16 flex flex-row flex-wrap">
+        {data.map((circle) =>
+          !circle.isDeleted ? (
+            <CircleElement
+              key={circle.id}
+              id={circle.id}
+              name={circle.name}
+              description={circle.description}
+              mainImage={circle.mainImage ?? "/images/signin-logo.png"}
+            />
+          ) : null,
+        )}
       </div>
     </>
   );
