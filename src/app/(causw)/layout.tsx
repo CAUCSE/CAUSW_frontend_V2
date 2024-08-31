5f3cd91eaf09b284d9c178e23092ef6be6d306c8
leeyeongjae <xingxing2001@cau.ac.kr>
2024-08-31 21:41:24 +0900

    chore: layout scroll 변경

--- src/app/(causw)/layout.tsx
+++ src/app/(causw)/layout.tsx
@@ -10,7 +10,7 @@ export default function HomeLayout({
     <>
       <NavigationBar />
       <SideBar />
-      <div className="absolute left-0 top-32 h-[calc(100%-14rem)] w-full overflow-y-scroll rounded-3xl bg-[#F8F8F8] lg:left-40 lg:top-0 lg:h-screen lg:w-[calc(100%-29rem)]">
+      <div className="absolute left-0 top-32 h-[calc(100%-14rem)] w-full overflow-y-auto rounded-3xl bg-[#F8F8F8] lg:left-40 lg:top-0 lg:h-screen lg:w-[calc(100%-29rem)]">
         {children}
       </div>
     </>
