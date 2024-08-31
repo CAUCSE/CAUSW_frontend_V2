99ec4bb40bd80629f2b9494e8293112d44ccd27a
selfishAltruism <officialkyus@gmail.com>
2024-08-31 16:45:37 +0900

    chore: causw layout update (#13)

--- src/app/(causw)/layout.tsx
+++ src/app/(causw)/layout.tsx
@@ -10,10 +10,7 @@ export default function HomeLayout({
     <>
       <NavigationBar />
       <SideBar />
-      <div
-        className="absolute top-32 left-0 w-full h-[calc(100%-14rem)] transform overflow-y-scroll bg-[#F8F8F8] rounded-3xl
-        lg:top-0 lg:left-40 lg:w-[calc(100%-29rem)] lg:h-screen"
-      >
+      <div className="absolute left-0 top-32 h-[calc(100%-14rem)] w-full overflow-y-auto rounded-3xl bg-[#F8F8F8] lg:left-40 lg:top-0 lg:h-screen lg:w-[calc(100%-29rem)]">
         {children}
       </div>
     </>
