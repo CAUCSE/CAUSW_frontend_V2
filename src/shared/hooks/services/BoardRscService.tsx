eb950189de5185b9c572d9473868195afbbef9f6
leeyeongjae <xingxing2001@cau.ac.kr>
2024-09-13 02:46:23 +0900

    feat: 게시판 rsc추가

--- src/shared/hooks/services/BoardRscService.tsx
+++ src/shared/hooks/services/BoardRscService.tsx
@@ -1,26 +1,53 @@
+import { setRscHeader } from "@/shared/configs/fetch";
 import { BASEURL } from "@/shared/configs/url";
 
-export const BoardRscService = async () => {
-  const searchPost = async (
-    boardId: string,
-    keyword: string,
-    pageNum: string,
-  ) => {
+export const BoardRscService = () => {
+  const getBoardList = async (boardId: string | undefined, pageNum: number) => {
+    const URI = BASEURL + "/api/v1/posts";
     try {
-      const encodedKeyword = encodeURIComponent(keyword);
-      const URI =
-        BASEURL +
-        `/api/v1/posts/search?boardId=${boardId}&keyword=${encodedKeyword}&pageNum=${pageNum}`;
-      const response = await fetch(`${URI}`, { method: "GET" });
-      if (!response.ok) {
+      const headers = await setRscHeader();
+      const response = await fetch(
+        `${URI}?boardId=${boardId}&pageNum=${pageNum}`,
+        { headers: headers },
+      );
+
+      if (response.status !== 200) {
         throw new Error(`${response.status} : ${response.statusText}`);
       }
-      console.log(await response.json());
+      const boardList = await response.json();
+      return boardList;
     } catch (error) {
       console.error(error);
       throw error;
     }
   };
 
-  return { searchPost };
+  const searchPost = async (
+    boardId: string,
+    keyword: string,
+    pageNum: number,
+  ) => {
+    try {
+      const headers = await setRscHeader();
+      const encodedKeyword = encodeURIComponent(keyword);
+      const URI =
+        BASEURL +
+        `/api/v1/posts/search?boardId=${boardId}&keyword=${encodedKeyword}&pageNum=${pageNum}`;
+      const response = await fetch(`${URI}`, {
+        headers: headers,
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status} : ${response.statusText}`);
+      }
+      const searchResult = await response.json();
+      console.log(searchResult);
+
+      return searchResult;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  return { getBoardList, searchPost };
 };
