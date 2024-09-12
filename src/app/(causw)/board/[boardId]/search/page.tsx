2564f3d88cee0451978fed3ab04b42f772ebc762
leeyeongjae <xingxing2001@cau.ac.kr>
2024-09-13 03:04:55 +0900

    feat: 게시물 검색 api 연동 및 페이지 구현

--- src/app/(causw)/board/[boardId]/search/page.tsx
+++ src/app/(causw)/board/[boardId]/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BoardRscService, PreviousButton } from "@/shared";
+import { BoardRscService, Icon, PreviousButton } from "@/shared";
 import {
   FieldErrors,
   SubmitHandler,
@@ -10,9 +10,39 @@ import {
 } from "react-hook-form";
 
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 interface IFormInput {
   searchContent: string;
+}
+
+interface IContent {
+  createdAt: string;
+  id: string;
+  isAnonymous: boolean;
+  isDeleted: boolean;
+  isQuestion: boolean;
+  numComment: number;
+  numFavorite: number;
+  numLike: number;
+  title: string;
+  updatedAt: Date;
+  writerAdmissionYear: number;
+  writerName: string;
+}
+
+interface IPost {
+  content: Array<IContent>;
+  totalPages: number;
+}
+
+interface IBoard {
+  boardId: "string";
+  boardName: "string";
+  isFavorite: "false";
+  post: IPost;
+  writeable: "true";
 }
 
 const ModalContent = () => (
@@ -25,6 +55,28 @@ const ModalContent = () => (
   </div>
 );
 
+const getTimeDifference = (ISOtime: string) => {
+  const createdTime = new Date(ISOtime);
+  const now = new Date();
+  const diffMSec = now.getTime() - createdTime.getTime();
+  const diffMin = Math.round(diffMSec / (60 * 1000));
+  if (diffMin === 0) {
+    return `방금 전`;
+  } else if (diffMin < 60) {
+    return `${diffMin}분 전`;
+  } else if (
+    now.getFullYear() === createdTime.getFullYear() &&
+    now.getMonth() === createdTime.getMonth() &&
+    now.getDate() === createdTime.getDate()
+  ) {
+    return `${createdTime.getHours()}:${createdTime.getMinutes()}`;
+  } else if (now.getFullYear() === createdTime.getFullYear()) {
+    return `${createdTime.getMonth() + 1}/${createdTime.getDate()}`;
+  } else {
+    return `${now.getFullYear() - createdTime.getFullYear()}년 전`;
+  }
+};
+
 const SearchPost = () => {
   const {
     register,
@@ -32,31 +84,163 @@ const SearchPost = () => {
     formState: { errors },
   } = useForm<IFormInput>();
 
+  const pathName = usePathname();
+  const path = pathName.split("/");
+  const boardId = path[path.length - 2];
+
   const { searchPost } = BoardRscService();
+
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isSearch, setIsSearch] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const lastPostElementRef = useRef(null);
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     //TODO 검색 api 연동
+    setIsSearch(true);
+    setPosts([]);
+    setPage(0);
     console.log(data);
+    setKeyword(() => data.searchContent);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await searchPost(boardId, keyword, page);
+      setPosts((prev) => [...prev, ...response.post.content]);
+    };
+    console.log(`keyword`, keyword);
+    console.log(`page: `, page);
+    if (keyword !== "") fetchData();
+  }, [keyword, page]);
+
+  useEffect(() => {
+    if (loading || !hasMore) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore && !loading) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 0.1 },
+    );
+
+    if (lastPostElementRef.current) {
+      observer.observe(lastPostElementRef.current);
+    }
+
+    return () => {
+      if (lastPostElementRef.current) {
+        observer.unobserve(lastPostElementRef.current);
+      }
+    };
+  }, [loading, hasMore]);
+
   return (
-    <div className="absolute bottom-0 top-0 w-full overflow-y-auto bg-boardPageBackground p-5">
+    <div className="bottom-0 top-0 h-full w-full bg-boardPageBackground p-5">
       <div className="flex h-full w-full flex-col items-center">
-        <PreviousButton />
-        <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-          <Image
-            src="/images/search_bg.png"
-            alt="검색화면배경사진"
-            width={150}
-            height={150}
-          />
-          <p className="text-2xl">게시물을 검색해주세요 !</p>
+        <div className="h-[60px] w-full bg-[#F8F8F8]">
+          <PreviousButton />
         </div>
-        <div className="flex h-12 w-full justify-center">
+
+        <div className="h-full w-full overflow-y-auto p-4">
+          {isSearch ? (
+            <div className="h-full w-full">
+              <div className="flex flex-col gap-4">
+                {posts.map((content: IContent, idx: number) => (
+                  <div
+                    key={idx}
+                    ref={idx === posts.length - 1 ? lastPostElementRef : null}
+                    className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
+                  >
+                    <div className="flex w-full flex-col">
+                      <div className="flex w-full items-center justify-between">
+                        <div className="flex w-2/3 flex-col">
+                          <div className="flex-auto truncate">
+                            <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
+                              {content.title}
+                            </p>
+                          </div>
+
+                          {/* todo content도 2줄 정도만 미리 보이게 하기 */}
+                          <div className="md:text-md pb-2 text-sm">
+                            <p>게시글 내용입니다</p>
+                            <p>게시글 내용입니다</p>
+                          </div>
+                        </div>
+                        <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
+                          <Image
+                            src="/images/post_default_thumbnail.png"
+                            alt="default_thumbnail"
+                            width={100}
+                            height={100}
+                            className="object-cover"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="flex gap-2 divide-x-2 sm:gap-4">
+                        <div className="flex gap-2 lg:gap-4">
+                          <div className="flex items-center gap-1 sm:gap-2">
+                            <Icon iconName="like" />
+                            <p className="text-red-500">
+                              {content.numLike > 999 ? "999+" : content.numLike}
+                            </p>
+                          </div>
+                          <div className="hidden items-center gap-2 sm:flex">
+                            <Icon iconName="scrap" />
+                            <p className="text-yellow-500">
+                              {content.numFavorite > 999
+                                ? "999+"
+                                : content.numFavorite}
+                            </p>
+                          </div>
+                          <div className="flex items-center gap-1 sm:gap-2">
+                            <Icon iconName="comment" />
+                            <p className="text-blue-300">
+                              {content.numFavorite > 999
+                                ? "999+"
+                                : content.numFavorite}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex items-center gap-2 pl-2 lg:pl-4">
+                          <Icon iconName="vote_inactive" />
+                          <Icon iconName="apply_inactive" />
+                        </div>
+                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                          {getTimeDifference(content.createdAt)}
+                        </div>
+                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                          {content.isAnonymous ? "익명" : content.writerName}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+              <Image
+                src="/images/search_bg.png"
+                alt="검색화면배경사진"
+                width={150}
+                height={150}
+              />
+              <p className="text-2xl">게시물을 검색해주세요 !</p>
+            </div>
+          )}
+        </div>
+
+        <div className="flex h-14 w-full items-center justify-center">
           <form
             action=""
             onSubmit={handleSubmit(onSubmit)}
-            className="flex w-full justify-between gap-4 lg:w-3/4"
+            className="flex h-full w-full justify-between gap-4 pt-4 lg:w-3/4"
           >
             <input
               className="w-full rounded-3xl border border-black text-center"
