1674c65871c7305d2cc6c9957a13f4f3029de3bd
leeyeongjae <xingxing2001@cau.ac.kr>
2024-09-18 21:37:20 +0900

    chore: 신청서 작성폼 재설계를 위해 코드 일부 삭제

--- src/app/(causw)/board/[boardId]/create/apply/page.tsx
+++ src/app/(causw)/board/[boardId]/create/apply/page.tsx
@@ -3,10 +3,10 @@
 import { ChangeEventHandler, useEffect, useState } from "react";
 
 import { IconButton, PreviousButton } from "@/shared";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 
 interface ICustomCheckBox {
-  colSize: number;
+  colSize: 1 | 2 | 3 | 4 | 5;
   targetValue: any;
   callback: ChangeEventHandler<HTMLInputElement>;
   value: any;
@@ -41,69 +41,47 @@ const CustomCheckBox: React.FC<ICustomCheckBox> = ({
 const ApplyCreatePage = () => {
   const {
     register,
+    control,
     handleSubmit,
+    setValue,
+    setError,
+    clearErrors,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      applyName: "",
+      status: [],
+      surveys: [],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "surveys" as never,
+  });
 
   const onSubmit = (data) => {
     //TODO 신청서 생성 완료 -> api 연동
+    if (selectedStatus.length === 0) {
+      setError("status", {
+        type: "manual",
+        message: "하나 이상의 항목을 선택해야 합니다.",
+      });
+      return;
+    }
     const formData = {
       ...data,
       status: selectedStatus,
-      grade: selectedGrade,
     };
     console.log(formData);
   };
 
   const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
-  const [selectedGrade, setSelectedGrade] = useState<string[]>([]);
 
   const [surveyForms, setSurveysForms] = useState([]);
 
   const addSurveyForm = () => {
-    setSurveysForms([
-      ...surveyForms,
-      { id: Date.now(), type: "주관식", question: "", options: [] },
-    ]);
-  };
-
-  const removeSurveyForm = (id) => {
-    setSurveysForms(surveyForms.filter((form) => form.id !== id));
-  };
-
-  const handleSurveyTypeChange = (id, newType) => {
-    setSurveysForms(
-      surveyForms.map((form) =>
-        form.id === id
-          ? {
-              ...form,
-              type: newType,
-              options: newType === "객관식" ? [""] : [],
-            }
-          : form,
-      ),
-    );
-  };
-
-  const addOption = (id) => {
-    setSurveysForms(
-      surveyForms.map((form) =>
-        form.id === id ? { ...form, options: [...form.options, ""] } : form,
-      ),
-    );
-  };
-
-  const removeOption = (id, optionIndex) => {
-    setSurveysForms(
-      surveyForms.map((form) =>
-        form.id === id
-          ? {
-              ...form,
-              options: form.options.filter((_, idx) => idx !== optionIndex),
-            }
-          : form,
-      ),
-    );
+    append({ question: "", type: "주관식", options: [""] });
   };
 
   const handleStatusChange = (status: string) => {
@@ -116,17 +94,9 @@ const ApplyCreatePage = () => {
           : [...prevStatus, status],
       );
     }
-  };
-
-  const handleGradeChange = (grade: string) => {
-    if (grade === "상관없음" || selectedGrade.includes("상관없음")) {
-      setSelectedGrade([grade]);
-    } else {
-      setSelectedGrade((prevGrade) =>
-        prevGrade.includes(grade)
-          ? prevGrade.filter((element) => element !== grade)
-          : [...prevGrade, grade],
-      );
+    setValue("status", selectedStatus as never);
+    if (selectedStatus.length > 0) {
+      clearErrors("status");
     }
   };
 
@@ -142,12 +112,15 @@ const ApplyCreatePage = () => {
       window.removeEventListener("resize", checkWidth);
     };
   }, []);
+
+  const statusOptions = [{ colSize: 1, name: "재학생" }];
+
   return (
     <div className="h-full w-full">
-      <div className="fixed h-14 w-full bg-[#F8F8F8] lg:w-[calc(100%-29rem)]">
+      <div className="fixed h-20 w-full bg-[#F8F8F8] lg:w-[calc(100%-29rem-40px)]">
         <PreviousButton />
       </div>
-      <div className="absolute top-[80px] h-[calc(100%-3.5rem)] w-full overflow-y-auto">
+      <div className="absolute top-20 h-[calc(100%-5rem)] w-full overflow-y-auto">
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex w-full flex-col gap-8 px-5 lg:px-8"
@@ -180,6 +153,18 @@ const ApplyCreatePage = () => {
                   targetValue={selectedStatus}
                   callback={() => handleStatusChange("학생회비 납부자")}
                   value="학생회비 납부자"
+                />
+                <CustomCheckBox
+                  colSize={isViewPointLg ? 1 : 1}
+                  targetValue={selectedStatus}
+                  callback={() => handleStatusChange("휴학생")}
+                  value="휴학생"
+                />
+                <CustomCheckBox
+                  colSize={isViewPointLg ? 4 : 1}
+                  targetValue={selectedStatus}
+                  callback={() => handleStatusChange("졸업생")}
+                  value="졸업생"
                 />
                 <CustomCheckBox
                   colSize={isViewPointLg ? 1 : 2}
@@ -243,129 +228,57 @@ const ApplyCreatePage = () => {
                 />
               </div>
             </div>
-            <hr className="w-3/4 min-w-[260px] border-dashed border-black lg:min-w-[500px]" />
-            <div className="flex w-3/4 min-w-[260px] items-center justify-around rounded-2xl bg-[#FDE4DE] py-10 lg:min-w-[500px]">
-              <div className="grid grid-cols-2 gap-1 lg:grid-cols-5 lg:gap-2">
-                <CustomCheckBox
-                  colSize={isViewPointLg ? 1 : 1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("휴학생")}
-                  value="휴학생"
-                />
-                <CustomCheckBox
-                  colSize={isViewPointLg ? 4 : 1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("졸업생")}
-                  value="졸업생"
-                />
-                <CustomCheckBox
-                  colSize={isViewPointLg ? 1 : 2}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("상관없음")}
-                  value="상관없음"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("1-1 수료")}
-                  value="1-1 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("1-2 수료")}
-                  value="1-2 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("2-1 수료")}
-                  value="2-1 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("2-2 수료")}
-                  value="2-2 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("3-1 수료")}
-                  value="3-1 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("3-2 수료")}
-                  value="3-2 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("4-1 수료")}
-                  value="4-1 수료"
-                />
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("4-2 수료")}
-                  value="4-2 수료"
-                />
-
-                <CustomCheckBox
-                  colSize={1}
-                  targetValue={selectedGrade}
-                  callback={() => handleGradeChange("5-1 재학중")}
-                  value="5-1 재학중"
-                />
-              </div>
-            </div>
+            {errors.status && (
+              <p className="text-red-500">{errors.status.message}</p>
+            )}
             <hr className="w-3/4 min-w-[260px] border-dashed border-black lg:min-w-[500px]" />
             <div className="flex w-3/4 min-w-[260px] flex-col bg-white lg:min-w-[500px]"></div>
           </div>
-          {surveyForms.map((form, idx) => (
+          {/* {surveyForms.map((form, idx) => (
             <div
               key={form.id}
               className="flex w-3/4 min-w-[260px] flex-col rounded-lg border border-black p-4 lg:min-w-[500px]"
             >
-              <div className="flex gap-4">
-                <label className="flex items-center p-2 text-xl">
-                  <input
-                    type="radio"
-                    name={`type-${form.id}`}
-                    checked={form.type === "객관식"}
-                    onChange={() => handleSurveyTypeChange(form.id, "객관식")}
-                    className="peer hidden"
-                  />
-                  <span className="m-4 inline-block h-4 w-4 cursor-pointer rounded-full border-2 border-black peer-checked:bg-black peer-checked:shadow-[0_0_0_2px_rgba(255,255,255,1),0_0_0_3px_rgba(0,0,0,1)]"></span>
-                  객관식
-                </label>
-                <label className="flex items-center p-2 text-xl">
-                  <input
-                    type="radio"
-                    name={`type-${form.id}`}
-                    checked={form.type === "주관식"}
-                    onChange={() => handleSurveyTypeChange(form.id, "주관식")}
-                    className="peer hidden cursor-pointer"
-                  />
-                  <span className="m-4 inline-block h-4 w-4 cursor-pointer rounded-full border-2 border-black peer-checked:bg-black peer-checked:shadow-[0_0_0_2px_rgba(255,255,255,1),0_0_0_3px_rgba(0,0,0,1)]"></span>
-                  주관식
-                </label>
+              <div className="flex w-full justify-between">
+                <div className="flex gap-4">
+                  <label className="flex items-center p-2 text-xl">
+                    <input
+                      type="radio"
+                      name={`type-${form.id}`}
+                      checked={form.type === "객관식"}
+                      onChange={() => handleSurveyTypeChange(form.id, "객관식")}
+                      className="peer hidden"
+                    />
+                    <span className="m-4 inline-block h-4 w-4 cursor-pointer rounded-full border-2 border-black peer-checked:bg-black peer-checked:shadow-[0_0_0_2px_rgba(255,255,255,1),0_0_0_3px_rgba(0,0,0,1)]"></span>
+                    객관식
+                  </label>
+                  <label className="flex items-center p-2 text-xl">
+                    <input
+                      type="radio"
+                      name={`type-${form.id}`}
+                      checked={form.type === "주관식"}
+                      onChange={() => handleSurveyTypeChange(form.id, "주관식")}
+                      className="peer hidden cursor-pointer"
+                    />
+                    <span className="m-4 inline-block h-4 w-4 cursor-pointer rounded-full border-2 border-black peer-checked:bg-black peer-checked:shadow-[0_0_0_2px_rgba(255,255,255,1),0_0_0_3px_rgba(0,0,0,1)]"></span>
+                    주관식
+                  </label>
+                </div>
+                <IconButton
+                  iconName={"remove"}
+                  callback={() => console.log("hello")}
+                />
               </div>
-              <IconButton
-                iconName={"remove"}
-                callback={() => console.log("hello")}
-              />
+              <input type="text" name="" id="" />
             </div>
           ))}
           <button
             type="button"
             onClick={addSurveyForm}
-            className="flex h-12 w-3/4 min-w-[260px] items-center justify-center bg-[#D9D9D9] text-[46px] font-bold lg:min-w-[500px]"
+            className="flex h-12 w-3/4 min-w-[260px] items-center justify-center rounded-xl bg-[#D9D9D9] text-[46px] font-bold lg:min-w-[500px]"
           >
             +
-          </button>
+          </button> */}
           <button type="submit">완료</button>
         </form>
       </div>
