27f138b8b658043062282fb4c69d7ea4393b9597
leeyeongjae <xingxing2001@cau.ac.kr>
2024-09-18 17:04:34 +0900

    feat: 게시물 목록 페이지, 게시글 검색 페이지 렌더링 시 로딩 효과 추가 및 무한 스크롤 시 로딩 위젯 추가

--- src/app/(causw)/board/[boardId]/search/page.tsx
+++ src/app/(causw)/board/[boardId]/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BoardRscService, Icon, PreviousButton } from "@/shared";
+import { BoardRscService, Icon, Loading, PreviousButton } from "@/shared";
 import {
   FieldErrors,
   SubmitHandler,
@@ -12,6 +12,7 @@ import {
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import { LoadingComponent } from "@/entities";
 
 interface IFormInput {
   searchContent: string;
@@ -95,34 +96,52 @@ const SearchPost = () => {
   const [page, setPage] = useState(0);
   const [isSearch, setIsSearch] = useState(false);
   const [keyword, setKeyword] = useState("");
-  const [loading, setLoading] = useState(false);
+
+  const [searchLoading, setSearchLoading] = useState(false);
+  const [scrollLoading, setScrollLoading] = useState(false);
+
   const [hasMore, setHasMore] = useState(true);
   const lastPostElementRef = useRef(null);
 
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     //TODO 검색 api 연동
     setIsSearch(true);
     setPosts([]);
     setPage(0);
-    console.log(data);
     setKeyword(() => data.searchContent);
+    try {
+      setSearchLoading(true);
+      const response = await searchPost(boardId, keyword, page);
+      setPosts((prev) => [...prev, ...response.post.content]);
+      setHasMore(response.post.totalPages - 1 > page);
+
+      setSearchLoading(false);
+    } catch (error) {
+      throw error;
+    }
   };
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await searchPost(boardId, keyword, page);
-      setPosts((prev) => [...prev, ...response.post.content]);
+      try {
+        setScrollLoading(true);
+        const response = await searchPost(boardId, keyword, page);
+        setPosts((prev) => [...prev, ...response.post.content]);
+        setHasMore(response.post.totalPages - 1 > page);
+      } catch (error) {
+        throw error;
+      } finally {
+        setScrollLoading(false);
+      }
     };
-    console.log(`keyword`, keyword);
-    console.log(`page: `, page);
-    if (keyword !== "") fetchData();
-  }, [keyword, page]);
+    fetchData();
+  }, [page]);
 
   useEffect(() => {
-    if (loading || !hasMore) return;
+    if (scrollLoading || !hasMore) return;
     const observer = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting && hasMore && !loading) {
+        if (entries[0].isIntersecting && hasMore && !searchLoading) {
           setPage((prevPage) => prevPage + 1);
         }
       },
@@ -138,93 +157,125 @@ const SearchPost = () => {
         observer.unobserve(lastPostElementRef.current);
       }
     };
-  }, [loading, hasMore]);
+  }, [searchLoading, hasMore]);
 
   return (
     <div className="bottom-0 top-0 h-full w-full bg-boardPageBackground p-5">
-      <div className="flex h-full w-full flex-col items-center">
-        <div className="h-[60px] w-full bg-[#F8F8F8]">
+      <div className="flex h-full w-full flex-col items-center gap-[20px]">
+        <div className="h-[50px] w-full bg-[#F8F8F8]">
           <PreviousButton />
         </div>
 
         <div className="h-full w-full overflow-y-auto p-4">
           {isSearch ? (
-            <div className="h-full w-full">
-              <div className="flex flex-col gap-4">
-                {posts.map((content: IContent, idx: number) => (
-                  <div
-                    key={idx}
-                    ref={idx === posts.length - 1 ? lastPostElementRef : null}
-                    className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
-                  >
-                    <div className="flex w-full flex-col">
-                      <div className="flex w-full items-center justify-between">
-                        <div className="flex w-2/3 flex-col">
-                          <div className="flex-auto truncate">
-                            <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
-                              {content.title}
-                            </p>
-                          </div>
+            <>
+              <div className="h-full w-full">
+                {searchLoading ? (
+                  <LoadingComponent />
+                ) : (
+                  <>
+                    <div className="flex flex-col gap-4">
+                      {posts.map((content: IContent, idx: number) => (
+                        <div
+                          key={idx}
+                          ref={
+                            idx === posts.length - 1 ? lastPostElementRef : null
+                          }
+                          className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
+                        >
+                          <div className="flex w-full flex-col">
+                            <div className="flex w-full items-center justify-between">
+                              <div className="flex w-2/3 flex-col">
+                                <div className="flex-auto truncate">
+                                  <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
+                                    {content.title}
+                                  </p>
+                                </div>
 
-                          {/* todo content도 2줄 정도만 미리 보이게 하기 */}
-                          <div className="md:text-md w-full pb-2 text-sm">
-                            <p className="whitespace-normal">
-                              {content.content}
-                            </p>
-                          </div>
-                        </div>
-                        <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
-                          <Image
-                            src="/images/post_default_thumbnail.png"
-                            alt="default_thumbnail"
-                            width={100}
-                            height={100}
-                            className="object-cover"
-                          />
-                        </div>
-                      </div>
+                                {/* todo content도 2줄 정도만 미리 보이게 하기 */}
+                                <div className="md:text-md pb-2 text-sm">
+                                  {content.content ? (
+                                    content.content
+                                      .split("\n")
+                                      .slice(0, 2)
+                                      .map((str, idx) => {
+                                        if (
+                                          idx === 1 &&
+                                          content.content.split("\n").length > 2
+                                        ) {
+                                          return <p key={idx}>{str}...</p>;
+                                        }
+                                        return <p key={idx}>{str}</p>;
+                                      })
+                                  ) : (
+                                    <p></p>
+                                  )}
+                                </div>
+                              </div>
+                              <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
+                                <Image
+                                  src="/images/post_default_thumbnail.png"
+                                  alt="default_thumbnail"
+                                  width={100}
+                                  height={100}
+                                  className="object-cover"
+                                />
+                              </div>
+                            </div>
 
-                      <div className="flex gap-2 divide-x-2 sm:gap-4">
-                        <div className="flex gap-2 lg:gap-4">
-                          <div className="flex items-center gap-1 sm:gap-2">
-                            <Icon iconName="like" />
-                            <p className="text-red-500">
-                              {content.numLike > 999 ? "999+" : content.numLike}
-                            </p>
+                            <div className="flex gap-2 divide-x-2 sm:gap-4">
+                              <div className="flex gap-2 lg:gap-4">
+                                <div className="flex items-center gap-1 sm:gap-2">
+                                  <Icon iconName="like" />
+                                  <p className="text-red-500">
+                                    {content.numLike > 999
+                                      ? "999+"
+                                      : content.numLike}
+                                  </p>
+                                </div>
+                                <div className="hidden items-center gap-2 sm:flex">
+                                  <Icon iconName="scrap" />
+                                  <p className="text-yellow-500">
+                                    {content.numFavorite > 999
+                                      ? "999+"
+                                      : content.numFavorite}
+                                  </p>
+                                </div>
+                                <div className="flex items-center gap-1 sm:gap-2">
+                                  <Icon iconName="comment" />
+                                  <p className="text-blue-300">
+                                    {content.numFavorite > 999
+                                      ? "999+"
+                                      : content.numFavorite}
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="flex items-center gap-2 pl-2 lg:pl-4">
+                                <Icon iconName="vote_inactive" />
+                                <Icon iconName="apply_inactive" />
+                              </div>
+                              <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                                {getTimeDifference(content.createdAt)}
+                              </div>
+                              <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                                {content.isAnonymous
+                                  ? "익명"
+                                  : content.writerName}
+                              </div>
+                            </div>
                           </div>
-                          <div className="hidden items-center gap-2 sm:flex">
-                            <Icon iconName="scrap" />
-                            <p className="text-yellow-500">
-                              {content.numFavorite > 999
-                                ? "999+"
-                                : content.numFavorite}
-                            </p>
-                          </div>
-                          <div className="flex items-center gap-1 sm:gap-2">
-                            <Icon iconName="comment" />
-                            <p className="text-blue-300">
-                              {content.numFavorite > 999
-                                ? "999+"
-                                : content.numFavorite}
-                            </p>
-                          </div>
                         </div>
-                        <div className="flex items-center gap-2 pl-2 lg:pl-4">
-                          <Icon iconName="vote_inactive" />
-                          <Icon iconName="apply_inactive" />
-                        </div>
-                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                          {getTimeDifference(content.createdAt)}
-                        </div>
-                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                          {content.isAnonymous ? "익명" : content.writerName}
-                        </div>
-                      </div>
+                      ))}
                     </div>
-                  </div>
-                ))}
+                    {scrollLoading && (
+                      <div className="pt-5">
+                        <Loading loading={scrollLoading} size={50} />
+                      </div>
+                    )}
+                  </>
+                )}
               </div>
-            </div>
+            </>
           ) : (
             <div className="flex h-full w-full flex-col items-center justify-center gap-4">
               <Image
@@ -251,6 +302,7 @@ const SearchPost = () => {
               id="searchContent"
               placeholder="글 제목, 내용, 해시태그"
             />
+
             <button
               className="w-36 rounded-3xl bg-red-500 text-white"
               type="submit"
