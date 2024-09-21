d72730567fa8bf23676821529a3c3a126064d8b3
ontheway-01 <eunhwa6087@naver.com>
2024-09-21 20:13:26 +0900

    feat: 게시글 생성 변경된 UI 적용

--- src/app/(causw)/board/[boardId]/create/page.tsx
+++ src/app/(causw)/board/[boardId]/create/page.tsx
@@ -9,13 +9,9 @@ import Image from "next/image";
 const CreatePostPage = (props: any) => {
   console.log(props.params.boardId);
   //추후에 boardId =  params로 변경
-  const defaultBoardId = "2c9faf078e17552c018e179d31bf0001";
+  const boardId = props.params.boardId;
   const { createPost } = PostRscService();
   const router = useRouter();
-  const defaultPostAttachment: Post.AttachmentDto = {
-    downloadFilePath: "",
-    originalFileName: "",
-  };
 
   const [isQuestion, setIsQuestion] = useState(false);
   const [isAnonymous, setIsAnonymous] = useState(false);
@@ -23,12 +19,15 @@ const CreatePostPage = (props: any) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  const [voteTitle, setVoteTitle] = useState('');
   const [isMultipleChoice, setIsMultipleChoice] = useState(false);
   const [allowAnonymous, setAllowAnonymousVote] = useState(false);
   const [options, setOptions] = useState(['', '']);
 
+  // vote 관련 handle 함수들
   const handleVoteButton = () => {
     setIsVote(!isVote);
+    console.log(isVote);
   }
 
   const handleOptionChange = (index: number, value: string) => {
@@ -59,6 +58,7 @@ const CreatePostPage = (props: any) => {
     setAllowAnonymousVote(!allowAnonymous);
   };
   
+  // 기본 post 관련 handle 함수
   const handleQuestionCheckbox = () => {
     setIsQuestion(!isQuestion);
   }
@@ -66,11 +66,12 @@ const CreatePostPage = (props: any) => {
     setIsAnonymous(!isAnonymous);
   }
 
+  // 일반 게시글 post api
   const handleSubmitPost = async () => {
     const postRequest: Post.CreatePostDto = {
       title,
       content,
-      boardId: defaultBoardId,
+      boardId: boardId,
       attachmentList: [
         "http://example.com/file1.jpg",
         "http://example.com/file2.jpg"
@@ -92,64 +93,7 @@ const CreatePostPage = (props: any) => {
       <div className="w-full flex-col items-center">
         <PreviousButton />
       </div>
-      {isVote ?
-      <div className="h-full flex flex-col p-10 pt-10">
-        <div className="flex items-center justify-between mb-6">
-          <div className="flex items-center space-x-4 w-full">
-            <div className="mt-4 w-full">
-              <input
-                type="text"
-                placeholder="투표 이름"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="w-full p-2 border-b-post-title-input border-black bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center space-x-4 mt-4">
-              <label className="flex items-center justify-center space-x-3 w-[120px]" onClick={handelSelectMultiple}>
-                <span className={`w-5 h-5 rounded-full ${isMultipleChoice ? 'bg-red-500' :'bg-gray-400'}`}></span>
-                <span className="text-gray-700">복수 선택</span>
-              </label>
-              <label className="flex items-center justify-center space-x-3 w-[120px]" onClick={handleAllowAnonymous}>
-                <span className={`w-5 h-5 rounded-full ${allowAnonymous ? 'bg-red-500' :'bg-gray-400'}`}></span>
-                <span className="text-gray-700">익명 투표</span>
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-2 gap-4 mb-4">
-          {options.map((option, index) => (
-            <div key={index} className="relative">
-              <input
-                type="text"
-                placeholder="항목 입력"
-                className="p-2 border-2 border-gray-300 rounded w-full focus:outline-none focus:border-gray-600"
-                value={option}
-                onChange={(e) => handleOptionChange(index, e.target.value)}
-              />
-              <button
-                onClick={() => handleRemoveOption(index)}
-                className="absolute top-0 right-0 -mt-2 -mr-2 bg-red-500 text-white rounded-full w-5 h-5"
-              >
-                -
-              </button>
-            </div>
-          ))}
-        
-          <div 
-            className="flex justify-center p-2 border-2 border-gray-300 rounded" 
-            onClick={handleAddOption}
-          >
-            <button
-              onClick={handleAddOption}
-            >
-              +
-            </button>
-          </div>
-        </div>
-      </div>
-      :
+      {/* 게시글 공통 부분 - 제목 / 내용 */}
       <div className="h-full flex flex-col p-10 pt-10">
         <div className="flex items-center justify-between mb-2">
           <div className="flex items-center space-x-4 w-full">
@@ -159,7 +103,7 @@ const CreatePostPage = (props: any) => {
                 placeholder="제목"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
-                className="w-full p-2 border-b-post-title-input border-black bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none"
+                className="w-full pb-2 mb-2 p-2 border-b-post-title-input border-black bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none"
               />
             </div>
             <div className="flex items-center space-x-2 w-[85px] mt-4">
@@ -202,18 +146,83 @@ const CreatePostPage = (props: any) => {
             </div>
           </div>
         </div>
-        <div className="h-full mb-10">
+        <div className={`relative ${isVote ? 'h-60' : 'h-full'}`}>
+          {content === "" && (
+            <div className="absolute inset-0 p-2 pointer-events-none">
+              <span className="bg-transparent text-[24px] text-create-post-text ">내용을 입력하세요!</span>
+              <br/>
+              <span className="bg-transparent text-[20px] text-create-post-text ">{"(게시글과 댓글은 작성 후에는 수정할 수 없습니다.)"} </span>
+            </div>
+          )}
           <textarea
-            placeholder="내용을 입력하세요!"
             value={content}
             onChange={(e) => setContent(e.target.value)}
-            className="w-full h-full p-2 h-40 bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none resize-none"
+            className="w-full h-full p-2 bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none resize-none"
           />
         </div>
-      </div>
-      }
+        {/* 투표 파트 */}
+        {isVote ?
+        <div className="h-full flex flex-col w-full">
+          <div className="flex items-center justify-between mb-6">
+            <div className="flex items-center space-x-4 w-full">
+              <div className="mt-4 w-full">
+                <input
+                  type="text"
+                  placeholder="투표 이름"
+                  value={voteTitle}
+                  onChange={(e) => setVoteTitle(e.target.value)}
+                  className="w-full p-2 border-b-post-title-input border-black bg-transparent text-[24px] placeholder:text-create-post-text focus:outline-none"
+                />
+              </div>
+              <div className="flex items-center space-x-4 mt-4">
+                <label className="flex items-center justify-center space-x-3 w-[120px]" onClick={handelSelectMultiple}>
+                  <span className={`w-5 h-5 rounded-full ${isMultipleChoice ? 'bg-red-500' :'bg-gray-400'}`}></span>
+                  <span className="text-gray-700">복수 선택</span>
+                </label>
+                <label className="flex items-center justify-center space-x-3 w-[120px]" onClick={handleAllowAnonymous}>
+                  <span className={`w-5 h-5 rounded-full ${allowAnonymous ? 'bg-red-500' :'bg-gray-400'}`}></span>
+                  <span className="text-gray-700">익명 투표</span>
+                </label>
+              </div>
+            </div>
+          </div>
 
-      <div className="absolute bottom-0 left-4 flex space-x-20">
+          <div className="grid grid-cols-2 gap-4 pt-2 pr-2 mb-4 max-h-80 overflow-y-scroll overflow-x-hidden">
+            {options.map((option, index) => (
+              <div key={index} className="relative">
+                <input
+                  type="text"
+                  placeholder="항목 입력"
+                  className="h-14 pl-3 border-2 border-gray-300 rounded w-full focus:outline-none focus:border-gray-600"
+                  value={option}
+                  onChange={(e) => handleOptionChange(index, e.target.value)}
+                />
+                <button
+                  onClick={() => handleRemoveOption(index)}
+                  className="absolute top-0 right-0 -mt-2 -mr-2 bg-red-500 text-white rounded-full w-5 h-5"
+                >
+                  -
+                </button>
+              </div>
+            ))}
+          
+            <div 
+              className="flex h-14 justify-center border-2 border-gray-300 rounded" 
+              onClick={handleAddOption}
+            >
+              <button
+                onClick={handleAddOption}
+                className="text-[16pt]"
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        : ''}
+      </div>
+      
+      <div className="absolute left-10 bottom-2 flex space-x-24">
         <div className="flex space-x-4">
           <button className={`flex justify-center p-2 bg-comment-input rounded-full w-[80px]`}>
             <Image
