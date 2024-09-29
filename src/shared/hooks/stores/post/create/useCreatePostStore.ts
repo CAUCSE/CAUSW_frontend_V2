ddee5778322de2a8b2b35e8a9754ae8784e325d5
leeyeongjae <xingxing2001@cau.ac.kr>
2024-09-29 19:24:07 +0900

    feat: isQuestion, isAnonymous setState 메서드 추가

--- src/shared/hooks/stores/post/create/useCreatePostStore.ts
+++ src/shared/hooks/stores/post/create/useCreatePostStore.ts
@@ -1,5 +1,5 @@
-import { create } from 'zustand';
-import { PostRscService } from '@/shared';
+import { create } from "zustand";
+import { PostRscService } from "@/shared";
 
 interface CreatePostState {
   title: string;
@@ -8,21 +8,25 @@ interface CreatePostState {
   isAnonymous: boolean;
   setTitle: (title: string) => void;
   setContent: (content: string) => void;
+  setIsQuestion: (isQuestion: boolean) => void;
+  setIsAnonymous: (isAnonymous: boolean) => void;
   toggleQuestion: () => void;
   toggleAnonymous: () => void;
   clearPost: () => void;
 }
 
 export const useCreatePostStore = create<CreatePostState>((set) => ({
-  title: '',
-  content: '',
+  title: "",
+  content: "",
   isQuestion: false,
   isAnonymous: false,
   setTitle: (title) => set({ title }),
   setContent: (content) => set({ content }),
+  setIsQuestion: (isQuestion) => set({ isQuestion }),
+  setIsAnonymous: (isAnonymous) => set({ isAnonymous }),
   toggleQuestion: () => set((state) => ({ isQuestion: !state.isQuestion })),
   toggleAnonymous: () => set((state) => ({ isAnonymous: !state.isAnonymous })),
   clearPost: () => {
-    set({title:'', content:'',isAnonymous: false, isQuestion:false})
-  }
+    set({ title: "", content: "", isAnonymous: false, isQuestion: false });
+  },
 }));
