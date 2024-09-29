94fcee01b2c57d7a93feb775a045080711787b23
lee0jae330 <xingxing2001@cau.ac.kr>
2024-09-29 23:09:29 +0900

    feat: 삭제된 게시글 게시물 목록에서 안보이게끔 처리

--- src/app/(causw)/board/[boardId]/page.tsx
+++ src/app/(causw)/board/[boardId]/page.tsx
@@ -189,93 +189,97 @@ const BoardPage = () => {
               </div>
             ) : (
               <>
-                {posts.map((content: IContent, idx: number) => (
-                  <div
-                    key={idx}
-                    ref={idx === posts.length - 1 ? lastPostElementRef : null}
-                    className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
-                    onClick={() => {
-                      router.push(`/board/${boardId}/${content.id}`);
-                    }}
-                  >
-                    <div className="flex w-full flex-col">
-                      <div className="flex w-full items-center justify-between">
-                        <div className="flex w-2/3 flex-col">
-                          <div className="flex-auto truncate">
-                            <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
-                              {content.title}
-                            </p>
-                          </div>
+                {posts
+                  .filter((post) => !post.isDeleted)
+                  .map((content: IContent, idx: number) => (
+                    <div
+                      key={idx}
+                      ref={idx === posts.length - 1 ? lastPostElementRef : null}
+                      className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
+                      onClick={() => {
+                        router.push(`/board/${boardId}/${content.id}`);
+                      }}
+                    >
+                      <div className="flex w-full flex-col">
+                        <div className="flex w-full items-center justify-between">
+                          <div className="flex w-2/3 flex-col">
+                            <div className="flex-auto truncate">
+                              <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
+                                {content.title}
+                              </p>
+                            </div>
 
-                          {/* todo content도 2줄 정도만 미리 보이게 하기 */}
-                          <div className="md:text-md pb-2 text-sm">
-                            {content.content ? (
-                              content.content
-                                .split("\n")
-                                .slice(0, 2)
-                                .map((str, idx) => {
-                                  if (
-                                    idx === 1 &&
-                                    content.content.split("\n").length > 2
-                                  ) {
-                                    return <p key={idx}>{str}...</p>;
-                                  }
-                                  return <p key={idx}>{str}</p>;
-                                })
-                            ) : (
-                              <p></p>
-                            )}
+                            {/* todo content도 2줄 정도만 미리 보이게 하기 */}
+                            <div className="md:text-md pb-2 text-sm">
+                              {content.content ? (
+                                content.content
+                                  .split("\n")
+                                  .slice(0, 2)
+                                  .map((str, idx) => {
+                                    if (
+                                      idx === 1 &&
+                                      content.content.split("\n").length > 2
+                                    ) {
+                                      return <p key={idx}>{str}...</p>;
+                                    }
+                                    return <p key={idx}>{str}</p>;
+                                  })
+                              ) : (
+                                <p></p>
+                              )}
+                            </div>
+                          </div>
+                          <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
+                            <Image
+                              src="/images/post_default_thumbnail.png"
+                              alt="default_thumbnail"
+                              width={100}
+                              height={100}
+                              className="object-cover"
+                            />
                           </div>
                         </div>
-                        <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
-                          <Image
-                            src="/images/post_default_thumbnail.png"
-                            alt="default_thumbnail"
-                            width={100}
-                            height={100}
-                            className="object-cover"
-                          />
-                        </div>
-                      </div>
-                      <div className="flex gap-2 divide-x-2 sm:gap-4">
-                        <div className="flex gap-2 lg:gap-4">
-                          <div className="flex items-center gap-1 sm:gap-2">
-                            <Icon iconName="like" />
-                            <p className="text-red-500">
-                              {content.numLike > 999 ? "999+" : content.numLike}
-                            </p>
+                        <div className="flex gap-2 divide-x-2 sm:gap-4">
+                          <div className="flex gap-2 lg:gap-4">
+                            <div className="flex items-center gap-1 sm:gap-2">
+                              <Icon iconName="like" />
+                              <p className="text-red-500">
+                                {content.numLike > 999
+                                  ? "999+"
+                                  : content.numLike}
+                              </p>
+                            </div>
+                            <div className="hidden items-center gap-2 sm:flex">
+                              <Icon iconName="scrap" />
+                              <p className="text-yellow-500">
+                                {content.numFavorite > 999
+                                  ? "999+"
+                                  : content.numFavorite}
+                              </p>
+                            </div>
+                            <div className="flex items-center gap-1 sm:gap-2">
+                              <Icon iconName="comment" />
+                              <p className="text-blue-300">
+                                {content.numComment > 999
+                                  ? "999+"
+                                  : content.numComment}
+                              </p>
+                            </div>
                           </div>
-                          <div className="hidden items-center gap-2 sm:flex">
-                            <Icon iconName="scrap" />
-                            <p className="text-yellow-500">
-                              {content.numFavorite > 999
-                                ? "999+"
-                                : content.numFavorite}
-                            </p>
+                          <div className="flex items-center gap-2 pl-2 lg:pl-4">
+                            <Icon iconName="vote_inactive" />
+                            <Icon iconName="apply_inactive" />
                           </div>
-                          <div className="flex items-center gap-1 sm:gap-2">
-                            <Icon iconName="comment" />
-                            <p className="text-blue-300">
-                              {content.numComment > 999
-                                ? "999+"
-                                : content.numComment}
-                            </p>
+                          <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                            {getTimeDifference(content.createdAt)}
                           </div>
-                        </div>
-                        <div className="flex items-center gap-2 pl-2 lg:pl-4">
-                          <Icon iconName="vote_inactive" />
-                          <Icon iconName="apply_inactive" />
-                        </div>
-                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                          {getTimeDifference(content.createdAt)}
-                        </div>
-                        <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                          {content.isAnonymous ? "익명" : content.writerName}
+                          <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                            {content.isAnonymous ? "익명" : content.writerName}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
                 {scrollLoading && (
                   <div className="pt-5">
                     <Loading loading={scrollLoading} size={50} />
