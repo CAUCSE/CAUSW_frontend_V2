b42fa008c2a760ee94fec134da00849a5e681845
ontheway-01 <eunhwa6087@naver.com>
2024-10-01 17:53:36 +0900

    feat: 투표 현황 화면 연결 완료 (익명 여부 포함)

--- src/app/(causw)/board/[boardId]/[postId]/result/page.tsx
+++ src/app/(causw)/board/[boardId]/[postId]/result/page.tsx
@@ -15,14 +15,18 @@ const VoteStatusPage = () => {
         <h1 className="text-[32px] mb-3">투표 현황</h1>
         {vote.options.map((option, index) => (
           <div key={index} className="mb-3">
-            <h2 className="text-[28px] pb-3 border-b-comment-bw border-black">{option.optionName}</h2>
-            <div className="grid grid-cols-4 gap-2 border-b-comment-bw py-2 border-black border-dashed">
-              {option.voteUsers.map((voter, voterIndex) => (
-                <span key={voterIndex} className="flex flex-row text-[20px]">
-                  {`${voter.name}(${voter.studentId})`}
-                </span>
-              ))}
-            </div>
+            <h2 className="text-[28px] pb-3 border-b-comment-bw border-black">
+              {vote.allowAnonymous ? `${option.optionName} - ${option.voteCount}명`:`${option.optionName}`}
+            </h2>
+            {!vote.allowAnonymous && (
+              <div className="grid grid-cols-4 gap-2 border-b-comment-bw py-2 border-black border-dashed">
+                {option.voteUsers.map((voter, voterIndex) => (
+                  <span key={voterIndex} className="flex flex-row text-[20px]">
+                    {`${voter.name}(${voter.studentId})`}
+                  </span>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       </div>
