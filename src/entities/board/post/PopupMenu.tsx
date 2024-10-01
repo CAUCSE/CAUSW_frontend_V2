e85c04e9aced03c60352c6c05658b264709865f1
leeyeongjae <xingxing2001@cau.ac.kr>
2024-10-01 22:34:54 +0900

    feat: props를 배열로 바꿔 재사용성을 높힘

--- src/entities/board/post/PopupMenu.tsx
+++ src/entities/board/post/PopupMenu.tsx
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 interface PopupMenuProps {
-  message: string,
+  PopupMenuChildren: PopupMenuChild[];
+}
+
+interface PopupMenuChild {
+  message: string;
   handleBtn: () => void;
 }
 
-export const PopupMenu: React.FC<PopupMenuProps> = ({ message,handleBtn }) => {
+export const PopupMenu: React.FC<PopupMenuProps> = ({ PopupMenuChildren }) => {
   return (
-    <div className="absolute top-4 right-10 w-32 bg-white border border-gray-300 rounded-lg shadow-lg z-10">
-      <button 
-        className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-        onClick={handleBtn}
-      >
-        {message}
-      </button>
+    <div className="absolute right-10 top-4 z-10 flex w-32 flex-col divide-y-[1px] rounded-lg border border-gray-300 bg-white shadow-lg">
+      {PopupMenuChildren.map((child: PopupMenuChild, idx: number) => (
+        <>
+          <button
+            className="w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+            onClick={child.handleBtn}
+          >
+            {child.message}
+          </button>
+        </>
+      ))}
     </div>
   );
 };
