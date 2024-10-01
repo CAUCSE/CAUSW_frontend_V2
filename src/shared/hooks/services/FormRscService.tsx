62cdaab40d9e580bbd9ddf139c6f070d5b0b00c0
lee0jae330 <xingxing2001@cau.ac.kr>
2024-10-02 05:37:37 +0900

    feat: form관련 api 연동

--- src/shared/hooks/services/FormRscService.tsx
+++ src/shared/hooks/services/FormRscService.tsx
@@ -1,5 +1,5 @@
-import { setRscHeader } from "@/shared/configs/fetch";
 import { BASEURL } from "@/shared/configs/url";
+import { setRscHeader } from "@/shared/configs/fetch";
 
 export const FormRscService = () => {
   const getFormData = async (formId: string | string[]) => {
@@ -35,5 +35,92 @@ export const FormRscService = () => {
       throw error;
     }
   };
-  return { getFormData, submitFormReply };
+
+  const getFormResultBySummary = async (formId: string | string[]) => {
+    const URI = `${BASEURL}/api/v1/forms/${formId}/summary`;
+    try {
+      const headers = await setRscHeader();
+      const response = await fetch(URI, { headers: headers });
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const res = await response.json();
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const getTotalFormResult = async (
+    formId: string | string[],
+    page: number,
+    size: number,
+  ) => {
+    const URI = `${BASEURL}/api/v1/forms/${formId}/results?page=${page}&size=${size}`;
+    try {
+      const headers = await setRscHeader();
+      const response = await fetch(URI, { headers: headers });
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      const res = await response.json();
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const exportExcelFile = async (formId: string | string[]) => {
+    const URI = `${BASEURL}/api/v1/forms/${formId}/export`;
+    try {
+      const headers = await setRscHeader();
+      const response = await fetch(URI, { headers: headers });
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const blob = await response.blob();
+      const downloadUrl = window.URL.createObjectURL(blob);
+      console.log(downloadUrl);
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = `form-${formId}-result.xlsx`;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(downloadUrl);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const setFormFinished = async (
+    formId: string | string[],
+    targetIsClosed: boolean,
+  ) => {
+    const URI = `${BASEURL}/api/v1/forms/${formId}/set-closed`;
+    try {
+      const headers = await setRscHeader();
+      const response = await fetch(URI, {
+        method: "PUT",
+        headers: { ...headers, targetIsClosed: targetIsClosed },
+      });
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+      console.log(response.status);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return {
+    getFormData,
+    submitFormReply,
+    getFormResultBySummary,
+    getTotalFormResult,
+    exportExcelFile,
+    setFormFinished,
+  };
 };
