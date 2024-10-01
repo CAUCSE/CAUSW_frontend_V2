6a06499b04afb63dea53cab7b2e954f4c232146c
leeyeongjae <xingxing2001@cau.ac.kr>
2024-10-01 22:36:10 +0900

    chore: 모달창 닫기 버튼 변경 및 css 조정

--- src/shared/ui/modal.tsx
+++ src/shared/ui/modal.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface IModalProps {
@@ -9,14 +10,19 @@ export const Modal = ({ closeModal, children }: IModalProps) => {
   return (
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 p-4">
       <div className="relative flex flex-col items-center rounded-lg bg-white p-8 md:w-1/2">
-        <button
-          className="absolute left-0 top-0 px-4 py-2 text-xl font-bold"
-          onClick={closeModal}
-        >
-          x
+        <button className="absolute left-0 top-0 p-2" onClick={closeModal}>
+          <Image
+            src="/images/modal_close_icon.png"
+            alt="modal-close-btn"
+            width={15}
+            height={15}
+          />
         </button>
         {children}
-        <button className="h-10 w-28 rounded bg-default" onClick={closeModal}>
+        <button
+          className="mt-5 h-10 w-28 rounded bg-default"
+          onClick={closeModal}
+        >
           닫기
         </button>
       </div>
