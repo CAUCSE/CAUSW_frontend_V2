aff720033d4df739013e256b53d2ce6dcac0827b
omjinLTS <omjin7g@gmail.com>
2024-10-02 19:03:30 +0900

    fix: passing userId (#8)

--- src/app/(causw)/setting/management/user/[state]/[userId]/page.tsx
+++ src/app/(causw)/setting/management/user/[state]/[userId]/page.tsx
@@ -1,13 +1,14 @@
 import { ManagementDetail, ManagementState } from "@/widget";
 
 const UserManagementDetail = async ({
-  params: { state },
+  params,
 }: {
   params: { state: string; userId: string };
 }) => {
   // const { getByState, getAllAdmissions } = SettingRscService();
+  const { state, userId } = params;
 
-  return <ManagementDetail state={state as ManagementState} />;
+  return <ManagementDetail state={state as ManagementState} userId={userId} />;
 };
 
 export default UserManagementDetail;
