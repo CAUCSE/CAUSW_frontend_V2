48d9710f8a2e8c9756c3dfa9656136be32fce64e
lee0jae330 <xingxing2001@cau.ac.kr>
2024-10-05 04:16:13 +0900

    feat: 검색 페이지에서 검색 결과가 없을 때 검색 결과가 없다는걸 알려주는 페이지 추가

--- src/app/(causw)/board/[boardId]/search/page.tsx
+++ src/app/(causw)/board/[boardId]/search/page.tsx
@@ -8,11 +8,11 @@ import {
   UseFormRegister,
   useForm,
 } from "react-hook-form";
+import { LoadingComponent, getTimeDifference } from "@/entities";
+import { useEffect, useRef, useState } from "react";
 
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { LoadingComponent } from "@/entities";
 
 interface IFormInput {
   searchContent: string;
@@ -47,38 +47,6 @@ interface IBoard {
   writeable: "true";
 }
 
-const ModalContent = () => (
-  <div className="flex flex-col items-center">
-    <h1 className="pb-10 text-lg font-bold md:text-xl">
-      이미 존재하는 게시판 이름
-    </h1>
-    <p className="text-center">동일한 이름의 게시판이 이미 존재합니다.</p>
-    <p className="pb-10 text-center">다른 이름을 입력해주세요</p>
-  </div>
-);
-
-const getTimeDifference = (ISOtime: string) => {
-  const createdTime = new Date(ISOtime);
-  const now = new Date();
-  const diffMSec = now.getTime() - createdTime.getTime();
-  const diffMin = Math.round(diffMSec / (60 * 1000));
-  if (diffMin === 0) {
-    return `방금 전`;
-  } else if (diffMin < 60) {
-    return `${diffMin}분 전`;
-  } else if (
-    now.getFullYear() === createdTime.getFullYear() &&
-    now.getMonth() === createdTime.getMonth() &&
-    now.getDate() === createdTime.getDate()
-  ) {
-    return `${createdTime.getHours()}:${createdTime.getMinutes()}`;
-  } else if (now.getFullYear() === createdTime.getFullYear()) {
-    return `${createdTime.getMonth() + 1}/${createdTime.getDate()}`;
-  } else {
-    return `${now.getFullYear() - createdTime.getFullYear()}년 전`;
-  }
-};
-
 const SearchPost = () => {
   const {
     register,
@@ -92,7 +60,7 @@ const SearchPost = () => {
 
   const { searchPost } = BoardRscService();
 
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post.PostResponseDto[]>([]);
   const [page, setPage] = useState(0);
   const [isSearch, setIsSearch] = useState(false);
   const [keyword, setKeyword] = useState("");
@@ -180,103 +148,127 @@ const SearchPost = () => {
                   <LoadingComponent />
                 ) : (
                   <>
-                    <div className="flex flex-col gap-4">
-                      {posts.map((content: IContent, idx: number) => (
-                        <div
-                          key={idx}
-                          ref={
-                            idx === posts.length - 1 ? lastPostElementRef : null
-                          }
-                          className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
-                        >
-                          <div className="flex w-full flex-col">
-                            <div className="flex w-full items-center justify-between">
-                              <div className="flex w-2/3 flex-col">
-                                <div className="flex-auto truncate">
-                                  <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
-                                    {content.title}
-                                  </p>
-                                </div>
-
-                                {/* todo content도 2줄 정도만 미리 보이게 하기 */}
-                                <div className="md:text-md pb-2 text-sm">
-                                  {content.content ? (
-                                    content.content
-                                      .split("\n")
-                                      .slice(0, 2)
-                                      .map((str, idx) => {
-                                        if (
-                                          idx === 1 &&
-                                          content.content.split("\n").length > 2
-                                        ) {
-                                          return <p key={idx}>{str}...</p>;
-                                        }
-                                        return <p key={idx}>{str}</p>;
-                                      })
-                                  ) : (
-                                    <p></p>
-                                  )}
-                                </div>
-                              </div>
-                              <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
-                                <Image
-                                  src="/images/post_default_thumbnail.png"
-                                  alt="default_thumbnail"
-                                  width={100}
-                                  height={100}
-                                  className="object-cover"
-                                />
-                              </div>
-                            </div>
-
-                            <div className="flex gap-2 divide-x-2 sm:gap-4">
-                              <div className="flex gap-2 lg:gap-4">
-                                <div className="flex items-center gap-1 sm:gap-2">
-                                  <Icon iconName="like" />
-                                  <p className="text-red-500">
-                                    {content.numLike > 999
-                                      ? "999+"
-                                      : content.numLike}
-                                  </p>
-                                </div>
-                                <div className="hidden items-center gap-2 sm:flex">
-                                  <Icon iconName="scrap" />
-                                  <p className="text-yellow-500">
-                                    {content.numFavorite > 999
-                                      ? "999+"
-                                      : content.numFavorite}
-                                  </p>
-                                </div>
-                                <div className="flex items-center gap-1 sm:gap-2">
-                                  <Icon iconName="comment" />
-                                  <p className="text-blue-300">
-                                    {content.numFavorite > 999
-                                      ? "999+"
-                                      : content.numFavorite}
-                                  </p>
-                                </div>
-                              </div>
-                              <div className="flex items-center gap-2 pl-2 lg:pl-4">
-                                <Icon iconName="vote_inactive" />
-                                <Icon iconName="apply_inactive" />
-                              </div>
-                              <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                                {getTimeDifference(content.createdAt)}
-                              </div>
-                              <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
-                                {content.isAnonymous
-                                  ? "익명"
-                                  : content.writerName}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                    {scrollLoading && (
-                      <div className="pt-5">
-                        <Loading loading={scrollLoading} size={50} />
+                    {posts.length === 0 ? (
+                      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
+                        <Image
+                          src="/images/puang-proud.png"
+                          alt="no-content"
+                          width={200}
+                          height={250}
+                        ></Image>
+                        <span>검색된 게시물이 없습니다.</span>
                       </div>
+                    ) : (
+                      <>
+                        {posts.length === 0}
+                        <div className="flex flex-col gap-4 pb-4">
+                          {posts
+                            .filter((post) => !post.isDeleted)
+                            .map((post: Post.PostResponseDto, idx: number) => (
+                              <div
+                                key={post.id}
+                                ref={
+                                  idx ===
+                                  posts.filter((post) => !post.isDeleted)
+                                    .length -
+                                    1
+                                    ? lastPostElementRef
+                                    : null
+                                }
+                                className="flex w-full items-center rounded-xl bg-white p-4 shadow-lg lg:p-6"
+                              >
+                                <div className="flex w-full flex-col">
+                                  <div className="flex w-full items-center justify-between">
+                                    <div className="flex w-2/3 flex-col">
+                                      <div className="flex-auto truncate">
+                                        <p className="overflow-hidden text-ellipsis whitespace-nowrap pb-2 text-sm font-bold md:text-2xl">
+                                          {post.title}
+                                        </p>
+                                      </div>
+
+                                      {/* todo content도 2줄 정도만 미리 보이게 하기 */}
+                                      <div className="md:text-md pb-2 text-sm">
+                                        {post.content ? (
+                                          post.content
+                                            .split("\n")
+                                            .slice(0, 2)
+                                            .map((str, idx) => {
+                                              if (
+                                                idx === 1 &&
+                                                post.content.split("\n")
+                                                  .length > 2
+                                              ) {
+                                                return (
+                                                  <p key={idx}>{str}...</p>
+                                                );
+                                              }
+                                              return <p key={idx}>{str}</p>;
+                                            })
+                                        ) : (
+                                          <p></p>
+                                        )}
+                                      </div>
+                                    </div>
+                                    <div className="h-16 w-16 flex-shrink-0 sm:h-24 sm:w-24">
+                                      <Image
+                                        src="/images/post_default_thumbnail.png"
+                                        alt="default_thumbnail"
+                                        width={100}
+                                        height={100}
+                                        className="object-cover"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div className="flex gap-2 divide-x-2 sm:gap-4">
+                                    <div className="flex gap-2 lg:gap-4">
+                                      <div className="flex items-center gap-1 sm:gap-2">
+                                        <Icon iconName="like" />
+                                        <p className="text-red-500">
+                                          {post.numLike > 999
+                                            ? "999+"
+                                            : post.numLike}
+                                        </p>
+                                      </div>
+                                      <div className="hidden items-center gap-2 sm:flex">
+                                        <Icon iconName="scrap" />
+                                        <p className="text-yellow-500">
+                                          {post.numFavorite > 999
+                                            ? "999+"
+                                            : post.numFavorite}
+                                        </p>
+                                      </div>
+                                      <div className="flex items-center gap-1 sm:gap-2">
+                                        <Icon iconName="comment" />
+                                        <p className="text-blue-300">
+                                          {post.numFavorite > 999
+                                            ? "999+"
+                                            : post.numFavorite}
+                                        </p>
+                                      </div>
+                                    </div>
+                                    <div className="flex items-center gap-2 pl-2 lg:pl-4">
+                                      <Icon iconName="vote_inactive" />
+                                      <Icon iconName="apply_inactive" />
+                                    </div>
+                                    <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                                      {getTimeDifference(post.createdAt)}
+                                    </div>
+                                    <div className="sm:text-md flex items-center pl-2 text-center text-xs text-gray-300 lg:pl-4">
+                                      {post.isAnonymous
+                                        ? "익명"
+                                        : post.writerName}
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            ))}
+                        </div>
+                        {scrollLoading && (
+                          <div className="pt-5">
+                            <Loading loading={scrollLoading} size={50} />
+                          </div>
+                        )}
+                      </>
                     )}
                   </>
                 )}
