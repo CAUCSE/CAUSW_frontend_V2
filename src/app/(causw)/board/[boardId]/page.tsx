a6dbdfcf4f080614d1845963bca46a2cf3f21e2f
lee0jae330 <xingxing2001@cau.ac.kr>
2024-10-06 20:42:53 +0900

    chore: 불필요한 코드 삭제

--- src/app/(causw)/board/[boardId]/page.tsx
+++ src/app/(causw)/board/[boardId]/page.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import {
-  BoardRscService,
-  Icon,
-  IconButton,
-  Loading,
-  PostRscService,
-  PreviousButton,
-  usePostListStore,
-} from "@/shared";
 import { LoadingComponent, PostList, PostListHeader } from "@/entities";
-import { notFound, useParams, usePathname, useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-
-import Image from "next/image";
-
-// TODO 게시판 ID로 게시물 목록 조회 API 연동 필요
-// TODO 알람 설정 API 연동 필요 -> 아
+import { PostRscService, usePostListStore } from "@/shared";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
 
 const BoardPage = () => {
   const router = useRouter();
@@ -79,15 +66,6 @@ const BoardPage = () => {
   if (!boardIdValidation) {
     router.push("/not-found");
   }
-
-  //TODO 게시판 즐겨찾기 api 추가되면 연동하기
-  // useEffect(() => {
-  //   const setBoardFavorite = async () => {
-  //     await toggleBoardFavorite(boardId, isBoardFavorite);
-  //   };
-  //   if (loading) return;
-  //   setBoardFavorite();
-  // }, [isBoardFavorite]);
 
   return (
     <div className="h-full w-full">
