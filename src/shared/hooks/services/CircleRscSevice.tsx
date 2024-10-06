1db72e2d324e19bdb7c35802ec4a29be0f89ac9a
leeyeongjae <xingxing2001@cau.ac.kr>
2024-10-06 18:33:27 +0900

    feat: `${URI}/${id}/users?circleMemberStatus=${state}` 를 통해 받아온 사용자들 중에서 userId와 일치하는 사용자만 반환하는 api 추가

--- src/shared/hooks/services/CircleRscSevice.tsx
+++ src/shared/hooks/services/CircleRscSevice.tsx
@@ -1,3 +1,4 @@
+import Circle from "@/app/(causw)/circle/[id]/page";
 import { BASEURL, setRscHeader } from "@/shared";
 
 export const CircleRscService = () => {
@@ -93,11 +94,39 @@ export const CircleRscService = () => {
     }
   };
 
+  const getCircleUserByStateAndId = async (
+    id: string,
+    state: Circle.JoinStatus,
+    userId: string,
+  ) => {
+    try {
+      const headers = await setRscHeader();
+      const response = await fetch(
+        `${URI}/${id}/users?circleMemberStatus=${state}`,
+        { headers: headers },
+      );
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const res: Circle.CircleUser[] = await response.json();
+      console.log(res);
+      const data = res.filter((memberInfo) => memberInfo.user.id === userId)[0];
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
   return {
     getCircles,
     getCircle,
     getCircleMembers,
     getCircleBoards,
     getCircleUsersByState,
+    getCircleUserByStateAndId,
   };
 };
