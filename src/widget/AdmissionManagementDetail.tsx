24756bec52f88f76aae3988cb1ce5b57b402e58b
younghyun753 <younghyun75398@gmail.com>
2025-01-01 19:18:05 +0900

    style: adjust title font size on user info page (#102)

--- src/widget/AdmissionManagementDetail.tsx
+++ src/widget/AdmissionManagementDetail.tsx
@@ -61,7 +61,7 @@ export async function ManagementDetail({
 
   return (
     <div className="flex w-full flex-col items-center gap-[30px] px-2 py-8">
-      <p className="text-[18px] font-semibold lg:text-[40px] mt-8">{`${name}(${studentId})의 ${titleSuffix}`}</p>
+      <p className="text-[18px] font-semibold lg:text-[25px] mt-8">{`${name}(${studentId})의 ${titleSuffix}`}</p>
 {admission && (<><ManagementDetailInfoTable
         data={convertAdmissionDataToTableEntity(admission)}
         titleMapping={titleMapping}
