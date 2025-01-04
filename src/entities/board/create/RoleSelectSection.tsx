72853fea7e71987c27774c3ddbccca24922830d0
lee0jae330 <xingxing2001@cau.ac.kr>
2025-01-05 02:50:11 +0900

    style: 기존 고정값으로 설정되어 있던 폰트 크기를 rem 단위로 변경 ('모바일 뷰에서는 더 작게' 라는 요구사항 반영)

--- src/entities/board/create/RoleSelectSection.tsx
+++ src/entities/board/create/RoleSelectSection.tsx
@@ -28,9 +28,11 @@ export const RoleSelectSection = ({ roles }: RoleSelectSectionProps) => {
 
   return (
     <div className="mb-2">
-      <div className="mb-2lg:mb-4 text-[28px]">게시글 작성 권한 명단</div>
+      <div className="mb-2 text-2xl xl:mb-4 xl:text-3xl">
+        게시글 작성 권한 명단
+      </div>
       {hasAuth ? (
-        <div className="rounded-2xl bg-notice-board-role p-4">
+        <div className="text-md rounded-2xl bg-notice-board-role p-4 xl:text-lg">
           <div className="mb-2 flex items-center space-x-3">
             <span onClick={toggleAnyRole}>
               {selectedRoles.includes("ALL") ? (
@@ -52,7 +54,7 @@ export const RoleSelectSection = ({ roles }: RoleSelectSectionProps) => {
             <span>상관없음</span>
           </div>
 
-          <div className="grid grid-cols-3 gap-2">
+          <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
             {Object.entries(roles).map(([roleLabel, roleEnums]) => (
               <div key={roleLabel} className="flex items-center space-x-3">
                 <span onClick={() => toggleRole(roleEnums)}>
@@ -92,7 +94,7 @@ export const RoleSelectSection = ({ roles }: RoleSelectSectionProps) => {
           </div>
 
           <div className="grid grid-cols-3 gap-2">
-            {Object.entries(roles).map(([roleLabel, roleEnums]) => (
+            {Object.entries(roles).map(([roleLabel]) => (
               <div key={roleLabel} className="flex items-center space-x-3">
                 <span>
                   <Image
