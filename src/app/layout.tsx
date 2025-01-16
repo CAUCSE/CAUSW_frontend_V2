af896ebb09befceb3c0e67c40542ac67c0e24234
selfishAltruism <officialkyus@gmail.com>
2025-01-16 19:17:09 +0900

    deploy: pwa full screnn setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
+        <head>
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+        </head>
         <body>
           <WindowSizeListener />
           <ErrorMessage />
