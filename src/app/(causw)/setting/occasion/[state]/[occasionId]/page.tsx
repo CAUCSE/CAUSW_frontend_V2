ed9598b0e09f66bd5e07bb32b0c8a45da09f9c8f
lee0jae330 <xingxing2001@cau.ac.kr>
2025-01-19 04:07:25 +0900

    feat: 경조사 등록 신청 관리 상세 페이지 추가가

--- src/app/(causw)/setting/occasion/[state]/[occasionId]/page.tsx
+++ src/app/(causw)/setting/occasion/[state]/[occasionId]/page.tsx
@@ -1,5 +1,27 @@
-const OccassionRequestDetialPage = () => {
-  return <div>OccassionRequestDetialPage</div>;
+"use client";
+
+import { Header } from "@/entities";
+import Link from "next/link";
+import { OccasionManagementDetail } from "@/widget";
+import { useParams } from "next/navigation";
+
+const OccasionRequestDetailPage = () => {
+  const { occasionId } = useParams<{ occasionId: string }>();
+  return (
+    <div className="relative left-4 top-3 w-[calc(100%-2rem)] md:left-14 md:top-14 md:w-[calc(100%-7rem)]">
+      <Link
+        href="/setting/occasion/request"
+        className="mb-7 flex items-center text-lg"
+      >
+        <span className="icon-[weui--back-filled] mr-6 text-3xl font-bold"></span>
+        이전
+      </Link>
+      <Header bold big>
+        경조사 정보
+      </Header>
+      <OccasionManagementDetail occasionId={occasionId} />
+    </div>
+  );
 };
 
-export default OccassionRequestDetialPage;
+export default OccasionRequestDetailPage;
