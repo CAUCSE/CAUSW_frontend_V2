9d79a30e3cd1b4edcf4a9a356556c79cdcfc2fc2
lee0jae330 <xingxing2001@cau.ac.kr>
2025-01-19 04:13:20 +0900

    chore:  단어 단위로 텍스트 줄바꿈이 되도록 css 변경

--- src/entities/occasion/OccasionApproveModal.tsx
+++ src/entities/occasion/OccasionApproveModal.tsx
@@ -12,8 +12,8 @@ export const OccasionApproveModal = ({
   return (
     <Modal closeModal={closeModal}>
       <div className="flex flex-col items-center gap-10 text-center">
-        <h1 className="text-2xl font-bold">경조사 등록 성공</h1>
-        <p className="text-xl">
+        <h1 className="text-xl font-bold md:text-2xl">경조사 등록 성공</h1>
+        <p className="break-keep text-lg md:text-xl">
           <span className="text-[#FF0000]">{occasionTitle}</span> 경조사 등록
           신청이 정상적으로 승인되었습니다.
         </p>
