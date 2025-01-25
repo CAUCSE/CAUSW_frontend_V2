e3cda068c57ada3e6b0f8f72c3f834b2f29c4d86
younghyun753 <younghyun75398@gmail.com>
2025-01-25 19:01:30 +0900

    chore: widget export 추가 (#114)

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -6,3 +6,6 @@ export { BoardList } from "./board/BoardList";
 export { BoardCreationForm } from "./board/BoardCreationForm";
 export { OccasionManagement } from "./occasion/OccasionManagement";
 export { OccasionManagementDetail } from "./occasion/OccasionManagementDetail";
+export { OccasionRegistrationForm } from "./occasion/OccasionRegistrationForm"
+export { OccasionNotification } from "./notification/OccasionNotification";
+export { OccasionNotificationDetail } from "./notification/OccasionNotificationDetail";
