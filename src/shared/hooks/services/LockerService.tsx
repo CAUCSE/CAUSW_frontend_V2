d1093274cc67a1abf0b103d9108dd4f9ca36e5ef
lee0jae330 <xingxing2001@cau.ac.kr>
2025-02-11 16:47:07 +0900

    feat: LockerService 사물함 정보 get 요청 추가가

--- src/shared/hooks/services/LockerService.tsx
+++ src/shared/hooks/services/LockerService.tsx
@@ -1,6 +1,19 @@
 "use client";
 
 import { API } from "@/shared";
-import { AxiosResponse } from "axios";
+import { lockerQueryKey } from "@/shared/configs/lockerQueryKey";
+import { useQuery } from "@tanstack/react-query";
 
-export const LockerService = () => {};
+export const LockerService = () => {
+  const useGetLockerLocations = () => {
+    return useQuery({
+      queryKey: lockerQueryKey.list(),
+      queryFn: async () => {
+        const { data }: { data: Locker.LockerLocationsResponseDto } =
+          await API.get("/api/v1/lockers/locations");
+        return data;
+      },
+    });
+  };
+  return { useGetLockerLocations };
+};
