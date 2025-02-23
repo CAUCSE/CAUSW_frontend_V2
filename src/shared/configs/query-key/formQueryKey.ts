70c19408bb28e94963a39916699fb43dd9867ed5
lee0jae330 <xingxing2001@cau.ac.kr>
2025-02-23 23:28:03 +0900

    chore: 신청서 요약 결과, 전체 결과 관련 쿼리 키 추가

--- src/shared/configs/query-key/formQueryKey.ts
+++ src/shared/configs/query-key/formQueryKey.ts
@@ -3,4 +3,8 @@ export const formQueryKey = {
   detail: (formId: string) => [...formQueryKey.all, "detail", formId] as const,
   canReply: (formId: string) =>
     [...formQueryKey.all, "canReply", formId] as const,
+  summaryResult: (formId: string) =>
+    [...formQueryKey.all, "summaryResult", formId] as const,
+  totalResult: (formId: string) =>
+    [...formQueryKey.all, "totalResult", formId] as const,
 };
