8c801e1b3333a1765c1713474f8e8a0aa2836065
lee0jae330 <xingxing2001@cau.ac.kr>
2025-02-23 23:26:25 +0900

    refactor: form 관련 컴포넌트는 전부 client component 이기에 rsc service => service로 마이그레이션

--- src/shared/hooks/services/FormService.tsx
+++ src/shared/hooks/services/FormService.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import { API, useResponseFormStore } from "@/shared";
+import { API, useFormResultStore, useResponseFormStore } from "@/shared";
 import { useEffect, useState } from "react";
-import { useMutation, useQueries } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueries,
+  useQuery,
+} from "@tanstack/react-query";
 
 import { formQueryKey } from "@/shared/configs/query-key/formQueryKey";
-import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { useShallow } from "zustand/react/shallow";
 
 export const FormService = () => {
-  const useGetFormInfo = (formId: string) => {
+  const getFormResponseDto = async (formId: string) => {
+    const { data }: { data: Post.FormResponseDto } = await API.get(
+      `/api/v1/forms/${formId}`,
+    );
+    return data;
+  };
+
+  const useGetFormResponseInfo = (formId: string) => {
     const setForm = useResponseFormStore((state) => state.setForm);
     const { data, isPending, isError, isSuccess } = useQueries({
       queries: [
         {
           queryKey: formQueryKey.detail(formId),
-          queryFn: async () => {
-            const { data }: { data: Post.FormResponseDto } = await API.get(
-              `/api/v1/forms/${formId}`,
-            );
-            return data;
-          },
+          queryFn: () => getFormResponseDto(formId),
         },
         {
           queryKey: formQueryKey.canReply(formId),
@@ -42,10 +50,10 @@ export const FormService = () => {
     });
 
     useEffect(() => {
-      if (isSuccess) {
+      if (isSuccess && data) {
         setForm(data[0] as Post.FormResponseDto);
       }
-    }, [isSuccess]);
+    }, [isSuccess, data]);
 
     return { data, isPending, isError };
   };
@@ -78,5 +86,114 @@ export const FormService = () => {
     return { modalOpen, modalMessage, setModalOpen, mutate };
   };
 
-  return { useGetFormInfo, useSubmitFormReply };
+  const useGetFormInfo = (formId: string) => {
+    const { formData, setFormData } = useFormResultStore(
+      useShallow((state) => ({
+        formData: state.formData,
+        setFormData: state.setFormData,
+      })),
+    );
+    const { data, isPending, isError, isSuccess } = useQuery({
+      queryKey: formQueryKey.detail(formId),
+      queryFn: () => getFormResponseDto(formId),
+    });
+
+    useEffect(() => {
+      if (isSuccess && data) {
+        setFormData(data);
+      }
+    }, [isSuccess, data]);
+    return { data, isPending, isError, isSuccess };
+  };
+
+  const useGetFormSummaryResult = (formId: string) => {
+    return useQuery({
+      queryKey: formQueryKey.summaryResult(formId),
+      queryFn: async () => {
+        const { data }: { data: Form.QuestionSummaryResponseDto[] } =
+          await API.get(`/api/v1/forms/${formId}/summary`);
+        return data;
+      },
+    });
+  };
+
+  const useGetFormTotalResult = (formId: string, size: number) => {
+    return useInfiniteQuery({
+      queryKey: formQueryKey.totalResult(formId),
+      queryFn: async ({ pageParam }) => {
+        const { data }: { data: Form.ReplyPageResponseDto } = await API.get(
+          `/api/v1/forms/${formId}/results?page=${pageParam}&size=${size}`,
+        );
+        return data;
+      },
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => {
+        return lastPage.replyResponseDtoPage.last
+          ? null
+          : lastPage.replyResponseDtoPage.pageable.pageNumber + 1;
+      },
+      select: (results) => {
+        return results.pages.flatMap((result) => result);
+      },
+    });
+  };
+
+  const useUpdateFormClosedStatus = () => {
+    const { formData, setFormClosedStatus } = useFormResultStore();
+    return useMutation({
+      mutationFn: async ({ formId }: { formId: string }) => {
+        await API.put(
+          `/api/v1/forms/${formId}/set-closed`,
+          {},
+          {
+            headers: {
+              ...API.defaults.headers.common,
+              targetIsClosed: `${!formData?.isClosed}`,
+            },
+          },
+        );
+      },
+      onSuccess: () => {
+        toast.success("신청서 마감 상태 변경 완료");
+        setFormClosedStatus(!formData?.isClosed);
+      },
+      onError: () => {
+        toast.error("신청서 마감 상태 변경 실패");
+      },
+    });
+  };
+
+  const useExportExcelFile = () => {
+    return useMutation({
+      mutationFn: async ({ formId }: { formId: string }) => {
+        API.get(`/api/v1/forms/${formId}/export`, {
+          responseType: "blob",
+        }).then((response) => {
+          const downloadUrl = window.URL.createObjectURL(
+            new Blob([response.data]),
+          );
+          const link = document.createElement("a");
+          link.href = downloadUrl;
+          link.download = `form-${formId}-result.xlsx`;
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+          window.URL.revokeObjectURL(downloadUrl);
+        });
+      },
+      onError: () => {
+        toast.error("엑셀 다운로드 실패");
+      },
+    });
+  };
+
+  return {
+    useGetFormResponseInfo,
+    useSubmitFormReply,
+    useGetFormInfo,
+    useGetFormSummaryResult,
+    useGetFormTotalResult,
+    useUpdateFormClosedStatus,
+    useExportExcelFile,
+  };
 };
