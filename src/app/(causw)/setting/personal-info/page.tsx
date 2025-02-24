429e1b098679c0ec1e104b91c400a7303807c96e
younghyun753 <younghyun75398@gmail.com>
2025-02-25 02:01:29 +0900

    refactor: 리액트 쿼리 이용한 리팩토링 (#114)

--- src/app/(causw)/setting/personal-info/page.tsx
+++ src/app/(causw)/setting/personal-info/page.tsx
@@ -2,16 +2,17 @@
 
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import {
   UserService,
   UserRscService,
   UserCouncilFeeService,
-  useUserStore,
-  Modal,
   AuthService,
 } from "@/shared";
 import toast from "react-hot-toast";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { LoadingComponent } from "@/entities";
+
 
 const PersonalInfoPage = () => {
   const {
@@ -34,45 +35,37 @@ const PersonalInfoPage = () => {
   const [profileImagePreview, setProfileImagePreview] = useState(
     "/images/default_profile.png",
   );
-  const [errorMessage, setErrorMessage] = useState("");
-  // 원래의 학적 상태 저장
 
+  // 원래의 학적 상태 저장
   const router = useRouter();
   const { getUserCouncilFeeInfo } = UserCouncilFeeService();
   const { updateInfo } = UserRscService();
   const { checkNicknameDuplicate } = AuthService();
 
   // 제출 시 모달
-  const [isFailModalOpen, setIsFailModalOpen] = useState(false);
+  const {data: userData, isLoading, error } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: async() => {
+      const response = await getMyInfo();
+      return response.data;
+    },
+    staleTime: 1000 * 60 * 5, // ✅ 5분 동안 데이터를 최신 상태로 유지
+  })
 
-  const user = useUserStore((state) => ({
-    email: state.email,
-    name: state.name,
-    admissionYear: state.admissionYear,
-    profileImage: state.profileImageUrl,
-    studentId: state.studentId,
-    currentCompletedSemester: state.currentCompletedSemester,
-    nickname: state.nickname,
-    major: state.major,
-    academicStatus: state.academicStatus,
-    graduationYear: state.graduationYear,
-    phoneNumber: state.phoneNumber,
-  }));
+  useEffect(() => {
+    if (userData) {
+      setProfileImagePreview(
+        userData.profileImage ?? "/images/default_profile.png",
+      );
+      setValue("nickname", userData.nickname);
+      setValue("phoneNumber", userData.phoneNumber ?? "");
+    }
+  }, [userData, setProfileImagePreview, setValue])
 
   const { getMyInfo } = UserService();
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        // 유저 기본 정보 받아오기
-
-        // formData에 유저 정보 값들 넣어두기
-        const response = await getMyInfo();
-        // 유저에 맞게 값들 대입입
-        setProfileImagePreview(
-          user.profileImage ?? "/images/default_profile.png",
-        );
-        setValue("nickname", user.nickname);
-        setValue("phoneNumber", user.phoneNumber ?? "");
 
         //  학생회비 납부 정보 받아오기
         const responseUserCouncilFeeData = await getUserCouncilFeeInfo();
@@ -103,11 +96,12 @@ const PersonalInfoPage = () => {
       setValue("profileImage", file);
     }
   };
+
   // 닉네임 중복 검사 및 형식 검사
   const handleNicknameBlur = async (e: React.FocusEvent<HTMLInputElement>) => {
     const nickname = e.target.value;
 
-    if (!nickname || nickname === user.nickname) return; // 빈 값일 경우 무시
+    if (!nickname || nickname === userData.nickname) return; // 빈 값일 경우 무시
 
     // 닉네임 길이 및 형식 검사
     if (nickname.length < 1 || nickname.length > 16) {
@@ -133,11 +127,12 @@ const PersonalInfoPage = () => {
   };
 
   // 개인정보 수정한 내용 제출하는 함수
-  const onSubmit = async (data: User.userUpdateDto) => {
+  const onSubmit = async (data: User.userUpdateDto, event) => {
     try {
+      event.preventDefault();
       await updateInfo(data);
       toast.success("변경 사항이 저장되었습니다.")
-    } catch (error: any) {
+} catch (error: any) {
       if (error.status === 400) {
         toast.error("중복된 닉네임입니다.")
       } else {
@@ -146,13 +141,20 @@ const PersonalInfoPage = () => {
     }
   };
 
+
+
+
+if (isLoading) return <LoadingComponent />;
+if (error) return <p>에러 발생!</p>;
+
+
   return (
     <div className="p-3">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* 이전 버튼 */}
+      {/* 이전 버튼 */}
         <div className="justify-left sticky top-0 mb-4 flex w-full items-center bg-[#F8F8F8] py-2">
           <button
-            onClick={() => router.back()}
+            onClick={() => router.push("/setting")}
             className="text-black-500 flex items-center hover:text-gray-500"
           >
             <svg
@@ -223,13 +225,13 @@ const PersonalInfoPage = () => {
                   </label>
                   <div className="flex w-full flex-row flex-wrap rounded-md sm:flex-nowrap lg:w-5/6">
                     <div className="mb-2 mr-2 w-full rounded-md border border-gray-300 p-2 text-center lg:w-3/6">
-                      {user.academicStatus === "ENROLLED" && <>재학</>}
-                      {user.academicStatus === "LEAVE_OF_ABSENCE" && <>휴학</>}
-                      {user.academicStatus === "GRADUATED" && <>졸업</>}
+                      {userData.academicStatus === "ENROLLED" && <>재학</>}
+                      {userData.academicStatus === "LEAVE_OF_ABSENCE" && <>휴학</>}
+                      {userData.academicStatus === "GRADUATED" && <>졸업</>}
                     </div>
                     <button
                       onClick={() => {
-                        router.push("./updateacademicrecord");
+                        window.location.href = "/setting/personal-info/update-academic-record";
                       }}
                       className="mb-2 mr-2 w-full rounded-md border border-gray-300 bg-focus p-2 text-center text-white lg:w-5/6"
                     >
@@ -248,34 +250,34 @@ const PersonalInfoPage = () => {
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   이름
                 </label>
-                <p className="text-gray-700">{user.name}</p>
+                <p className="text-gray-700">{userData.name}</p>
               </div>
               <div className="mb-4">
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   이메일
                 </label>
-                <p className="text-gray-700">{user.email}</p>
+                <p className="text-gray-700">{userData.email}</p>
               </div>
               <div className="mb-4">
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   학번
                 </label>
-                <p className="text-gray-700">{user.studentId}</p>
+                <p className="text-gray-700">{userData.studentId}</p>
               </div>
 
               <div className="mb-4">
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   입학 년도
                 </label>
-                <p className="text-gray-700">{user.admissionYear}</p>
+                <p className="text-gray-700">{userData.admissionYear}</p>
               </div>
 
-              {user.academicStatus === "GRADUATED" && (
+              {userData.academicStatus === "GRADUATED" && (
                 <div className="mb-4">
                   <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                     졸업 년도
                   </label>
-                  <p className="text-gray-700">{user.graduationYear}</p>
+                  <p className="text-gray-700">{userData.graduationYear}</p>
                 </div>
               )}
             </div>
@@ -285,13 +287,13 @@ const PersonalInfoPage = () => {
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   등록 완료 학기
                 </label>
-                <p className="text-gray-700">{user.currentCompletedSemester}</p>
+                <p className="text-gray-700">{userData.currentCompletedSemester}</p>
               </div>
               <div className="mb-4">
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
                   학부(학과)
                 </label>
-                <p className="text-gray-700">{user.major}</p>
+                <p className="text-gray-700">{userData.major}</p>
               </div>
               <div className="mb-4">
                 <label className="mb-1 block text-sm font-semibold sm:text-2xl lg:text-lg">
@@ -315,17 +317,7 @@ const PersonalInfoPage = () => {
           </div>
         </div>
 
-        {/* 오류 발생 시 모달 */}
-        {isFailModalOpen && (
-          <Modal closeModal={() => setIsFailModalOpen(false)}>
-            <div className="p-2 lg:p-4">
-              <div className="flex flex-col items-center justify-center">
-                <div>{errorMessage} </div>
-                <div>다시 시도해주세요</div>
-              </div>
-            </div>
-          </Modal>
-        )}
+
 
         {/* 변경 사항 저장 버튼 */}
         <div className="mt-8 flex justify-center">
