e75e6b03e905be00b957c8fbf036c2927ee91a8e
younghyun753 <younghyun75398@gmail.com>
2025-02-25 02:00:43 +0900

    feat: 쿼리 클라이언트 레이아웃 구성 (#114)

--- src/app/(causw)/setting/personal-info/layout.tsx
+++ src/app/(causw)/setting/personal-info/layout.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastWithMax } from "@/shared";
 
+const queryClient = new QueryClient();
 
-const PersonalInfoLayout = ({
+const QueryClientLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => (
+  <QueryClientProvider client={queryClient}>
     <>
-      <ToastWithMax/>
+      <ToastWithMax />
       {children}
     </>
+  </QueryClientProvider>
 );
 
-export default PersonalInfoLayout;
+export default QueryClientLayout;
