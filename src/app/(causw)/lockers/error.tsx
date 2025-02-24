548bf64090f1535cb0a313cb6df1632d0362c71a
lee0jae330 <xingxing2001@cau.ac.kr>
2025-02-24 16:58:29 +0900

    chore: home layout 제거

--- src/app/(causw)/lockers/error.tsx
+++ src/app/(causw)/lockers/error.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import HomeLayout from "@/app/(causw)/layout";
 import Image from "next/image";
 
 const Error = () => (
-  <HomeLayout>
-    <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
-      <Image
-        src="/images/puang-proud.png"
-        alt="404"
-        width={200}
-        height={250}
-      ></Image>
-      <span>사물함 조회 도중 에러가 발생했습니다.</span>
-    </div>
-  </HomeLayout>
+  <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
+    <Image
+      src="/images/puang-proud.png"
+      alt="404"
+      width={200}
+      height={250}
+    ></Image>
+    <span>사물함 조회 도중 에러가 발생했습니다.</span>
+  </div>
 );
 
 export default Error;
