b5a3897c3f06f5d163a59fb4c368d2df1965a13e
leeyeongjae <xingxing2001@cau.ac.kr>
2025-03-01 15:31:56 +0900

    feat: 사물함 선택 페이지 에러 바운더리 추가가

--- src/app/(causw)/lockers/[locationId]/page.tsx
+++ src/app/(causw)/lockers/[locationId]/page.tsx
@@ -21,18 +21,18 @@ const LockerSelectionPage = () => {
   const { useGetLockerList } = LockerService();
   const { data: lockerList, isLoading } = useGetLockerList(locationId);
 
-  if (isLoading || !lockerList) {
+  if (isLoading) {
     return <LoadingComponent />;
   }
 
   return (
     <div className="relative left-4 top-3 w-[calc(100%-2rem)] md:left-14 md:top-14 md:w-[calc(100%-7rem)]">
-      <LockerSelectionHeader lockerList={lockerList} />
+      <LockerSelectionHeader lockerList={lockerList!} />
       <div className="flex h-full flex-col items-center gap-4 md:flex-row md:justify-between">
-        <LockerSelectionMobileManual />
-        <LockerSelectionGrid lockerList={lockerList} />
-        <LockerSelectionDesktopManual />
-        <LockerMobileActionBtn />
+        <LockerSelectionMobileManual lockerPeriod={lockerList!.lockerPeriod} />
+        <LockerSelectionGrid lockerList={lockerList!} />
+        <LockerSelectionDesktopManual lockerPeriod={lockerList!.lockerPeriod} />
+        <LockerMobileActionBtn lockerPeriod={lockerList!.lockerPeriod} />
       </div>
     </div>
   );
