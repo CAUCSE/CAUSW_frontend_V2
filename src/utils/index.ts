874a41e9d075f6a7c4252e7dd15fd03e8b17e861
younghyun753 <younghyun75398@gmail.com>
2025-03-01 20:58:23 +0900

    feat: createFormData export 추가 (#114)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { formatDateString, formatUrlToFile, getTodayDate } from "./format";
 export { roles } from "./roles";
 export { FORM_CONSTANT } from "./form/constant";
 export { isValidDate, isPastDate, isStartDateBeforeEndDate } from './validation'
+export { createFormData } from './formDataUtil'
