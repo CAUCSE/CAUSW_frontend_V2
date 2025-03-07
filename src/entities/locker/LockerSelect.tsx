77f412134ad214667bac0e7af2a90e0572e0efd0
younghyun753 <younghyun75398@gmail.com>
2025-03-08 00:53:50 +0900

    chore: 사물함 클릭 시 스타일 수정 (#143)

--- src/entities/locker/LockerSelect.tsx
+++ src/entities/locker/LockerSelect.tsx
@@ -12,7 +12,7 @@ export const LockerSelect = ({ locker }: LockerSelectProps) => {
 
   return (
     <button
-      className={`h-16 w-16 ${clickedLockerId === locker.id ? "border-4 border-[#76C6D1] scale-110 shadow-md" : ""} ${locker.isActive ? "bg-white" : locker.isMine ? "bg-[#76C6D1]" : "bg-[#D9D9D9]"} ${locker.isActive && "border border-[#BABABA]"} text-[#686868]`}
+      className={`h-16 w-16 ${clickedLockerId === locker.id && "border-2 border-[#76C6D1] scale-110 shadow-md"} ${locker.isActive ? "bg-white" : locker.isMine ? "bg-[#76C6D1]" : "bg-[#D9D9D9]"} ${clickedLockerId !== locker.id && locker.isActive && "border border-[#BABABA]"} text-[#686868]`}
       onClick={() => handleLockerClick(locker)}
     >
       {locker.lockerNumber}
