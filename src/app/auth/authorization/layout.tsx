555027da98a4e16d58c4a9241663bfc6d31a9864
younghyun753 <younghyun75398@gmail.com>
2025-03-14 18:18:51 +0900

    feat: layout 구현 (#146)

---
+++ src/app/auth/authorization/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ToastWithMax } from "@/shared";
+
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => (
+    <>
+      <ToastWithMax/>
+      {children}
+    </>
+);
+
+export default Layout;
