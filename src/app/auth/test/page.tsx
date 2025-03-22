f7b4f674a7236f079502cd82a9949a2223a4eda6
younghyun753 <younghyun75398@gmail.com>
2025-03-22 11:09:54 +0900

    test: 푸시알람 테스트, 디바이스 정보 및 권한 요청 로직 추가 (#117)

--- src/app/auth/test/page.tsx
+++ src/app/auth/test/page.tsx
@@ -5,10 +5,47 @@ import { useEffect, useState } from "react";
 
 import "@/firebase-messaging-sw";
 
+const getDeviceType = () => {
+  const userAgent = navigator.userAgent || navigator.vendor;
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  if (/iPad/i.test(userAgent)) {
+    return "iPadOS";
+  }
+
+  if (/iPhone|iPod/i.test(userAgent) && !(window as any).MSStream) {
+    return "iOS";
+  }
+
+  if (/Win/i.test(userAgent)) {
+    return "Windows";
+  }
+
+  if (/Macintosh/i.test(userAgent)) {
+    return "Mac";
+  }
+
+  return "Web";
+};
+
+
 const Page = () => {
   const [token, setToken] = useState("NaN");
+  const [deviceType, setDeviceType] = useState("Unknown");
+
+  const requestPushPermission = async () => {
+    const permission = await Notification.requestPermission();
+    if (permission !== "granted") {
+      alert("알림 권한이 허용되지 않았습니다. 😢");
+      return;
+  }}
+
   useEffect(() => {
     const messaging = getMessaging();
+    setDeviceType(getDeviceType());
     getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_FB_VAPID_KEY })
       .then((currentToken) => {
         if (currentToken) {
@@ -32,8 +69,12 @@ const Page = () => {
   }, []);
   return (
     <>
-      <div className="w-full">
-        <span className="block w-full break-all">{token}</span>
+      <div className="w-full select-text">
+      <div onClick={requestPushPermission}>푸시 권한 요청하기 (iOS 대응)</div>
+      <div onClick = {() => {}}> 내 토큰 서버에 저장 </div>
+      <div onClick = {() => {}}> 서버에 저장된 내 토큰 조회 </div>
+      <span className="block w-full break-all">현재 접속한 기기 : {deviceType}</span>
+      <span className="block w-full break-all">{token}</span>
       </div>
     </>
   );
