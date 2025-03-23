eaa9afc31aa50b539a4e1fd1f8dea670d0ed85b2
leeyeongjae <xingxing2001@cau.ac.kr>
2025-03-23 21:20:27 +0900

    feat: 캘린더 삭제 커스텀훅 추가

--- src/shared/hooks/services/CalendarService.tsx
+++ src/shared/hooks/services/CalendarService.tsx
@@ -1,6 +1,8 @@
 import { API } from "@/shared/configs/axios";
 import { calendarQueryKey } from "@/shared/configs/query-key";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useCalendarStore } from "../stores/calendar/useCalendarStore";
 
 export const CalendarService = () => {
   const useGetCalendarList = ({ year }: { year: number }) => {
@@ -15,5 +17,34 @@ export const CalendarService = () => {
       },
     });
   };
-  return { useGetCalendarList };
+
+  const useDeleteCalendar = () => {
+    const closeModal = useCalendarStore((state) => state.closeModal);
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: async ({
+        calendarId,
+        calendarYear,
+      }: {
+        calendarId: string;
+        calendarYear: number;
+      }) => {
+        await API.delete(`/api/v1/calendars/${calendarId}`);
+        return calendarYear;
+      },
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: calendarQueryKey.year(data),
+        });
+        toast.success("캘린더가 삭제되었습니다");
+      },
+      onError: () => {
+        toast.error("캘린더 삭제를 실패했습니다.");
+      },
+      onSettled: () => {
+        closeModal();
+      },
+    });
+  };
+  return { useGetCalendarList, useDeleteCalendar };
 };
