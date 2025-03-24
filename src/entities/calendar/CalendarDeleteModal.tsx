ecb8538dd4cdfffa75f0dfb6ec3dde73ff76f996
lee0jae330 <xingxing2001@cau.ac.kr>
2025-03-24 23:21:06 +0900

    chore: 캘린더 전역 상태 변경에 따른 코드 변경

--- src/entities/calendar/CalendarDeleteModal.tsx
+++ src/entities/calendar/CalendarDeleteModal.tsx
@@ -1,46 +1,45 @@
 "use client";
 
-import { createPortal } from "react-dom";
-import { useDeleteCalendarModal } from "@/shared";
+import { PortalModal, useDeleteCalendarModal } from "@/shared";
 
-// TODO: focus 스트랩 적용, 재사용 가능한 모달 컴포넌트로 변경
 export const CalendarDeleteModal = () => {
   const {
-    clickOutSide,
     calendarYear,
     calendarMonth,
     cancelDelete,
     deleteCalendar,
+    closeDeleteModal,
   } = useDeleteCalendarModal();
 
-  return createPortal(
-    <div
-      className="absolute z-10 h-screen w-screen bg-black bg-opacity-50"
-      onClick={clickOutSide}
+  return (
+    <PortalModal
+      closeModal={closeDeleteModal}
+      className="flex min-w-72 flex-col items-center gap-4 rounded-lg bg-white p-8"
     >
-      <div className="absolute left-1/2 top-1/2 flex min-w-72 -translate-x-1/2 -translate-y-1/2 transform flex-col items-center gap-4 rounded-lg bg-white p-8">
+      <PortalModal.Header>
         <h1 className="text-xl">캘린더 삭제</h1>
+      </PortalModal.Header>
+      <PortalModal.Body>
         <p className="text-center text-red-500">
           {calendarYear}년 {calendarMonth}월 캘린더를 삭제하시겠습니까?
           <br />
           삭제하실 경우 다시 복구시킬 수 없습니다.
         </p>
-        <div className="flex gap-4">
-          <button
-            className="rounded-lg bg-gray-300 px-8 py-2 hover:bg-gray-400"
-            onClick={cancelDelete}
-          >
-            취소
-          </button>
-          <button
-            className="rounded-lg bg-red-500 px-8 py-2 text-white hover:bg-red-700"
-            onClick={deleteCalendar}
-          >
-            삭제
-          </button>
-        </div>
-      </div>
-    </div>,
-    document.body,
+      </PortalModal.Body>
+      <PortalModal.Footer className="flex gap-4">
+        <button
+          className="rounded-lg bg-gray-300 px-8 py-2 hover:bg-gray-400"
+          onClick={cancelDelete}
+        >
+          취소
+        </button>
+        <button
+          className="rounded-lg bg-red-500 px-8 py-2 text-white hover:bg-red-700"
+          onClick={deleteCalendar}
+        >
+          삭제
+        </button>
+      </PortalModal.Footer>
+    </PortalModal>
   );
 };
