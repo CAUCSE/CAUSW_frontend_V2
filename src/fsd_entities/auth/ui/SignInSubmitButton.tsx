fc150493ae0857e3a7e28a88707b220da05a1d5c
younghyun753 <younghyun75398@gmail.com>
2025-03-25 21:17:17 +0900

    feat:sign-in submit 엔티티 fsd로 추가 (#158)

---
+++ src/fsd_entities/auth/ui/SignInSubmitButton.tsx
@@ -0,0 +1,9 @@
+export const SignInSubmitButton = () => (
+  <button
+    type="submit"
+    className="text-mb mt-3 flex h-10 w-56 flex-row items-center justify-center rounded-xl border-2 border-focus bg-zinc-800 pl-2 text-center text-focus sm:w-80"
+  >
+    로그인
+    <span className="icon-[iconamoon--arrow-right-2-fill] text-3xl"></span>
+  </button>
+);
