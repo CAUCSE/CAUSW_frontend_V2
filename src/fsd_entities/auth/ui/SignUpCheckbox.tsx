1f42703556591f03df3c49d4904b6dfd0751e3cc
younghyun753 <younghyun75398@gmail.com>
2025-03-25 21:23:03 +0900

    refactor: sign-up 체크박스 fsd 폴더에 추가 (#158)

---
+++ src/fsd_entities/auth/ui/SignUpCheckbox.tsx
@@ -0,0 +1,49 @@
+import { UseTerms } from "@/shared/ui/useTerms";
+import { useState } from "react";
+import { FieldValues, Path, RegisterOptions, UseFormRegister } from "react-hook-form";
+
+interface Props<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  name: Path<T>;
+  label?: string;
+  rules?: RegisterOptions<T>;
+  errorMessage?: string;
+}
+export const SignUpCheckbox = <T extends FieldValues>({
+  register,
+  name,
+  label,
+  rules,
+  errorMessage,
+}: Props<T>) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div>
+    <div className="flex items-center gap-2 mt-2">
+      <input
+        type="checkbox"
+        id={name}
+        {...register(name, rules)}
+        className="w-4 h-4"
+      />
+      <label className="text-sm text-gray-700 underline" onClick = {() => setIsModalOpen(true)}>
+        {label}
+      </label>
+    </div>
+      {errorMessage && (
+        <span className="text-error text-sm">{errorMessage}</span>
+      )}
+            {isModalOpen && (
+              <div>
+                <UseTerms
+                  closeModal={() => {
+                    setIsModalOpen(false);
+                  }}
+                ></UseTerms>
+              </div>
+            )}
+    </div>
+  );
+};
