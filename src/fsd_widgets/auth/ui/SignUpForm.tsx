69ca37172e2097969bfb703eebf494e0035c1c0c
younghyun753 <younghyun75398@gmail.com>
2025-03-25 22:05:07 +0900

    refactor: sign-up form widget api 연결 (#158)

--- src/fsd_widgets/auth/ui/SignUpForm.tsx
+++ src/fsd_widgets/auth/ui/SignUpForm.tsx
@@ -1,11 +1,25 @@
 // src/widgets/sign-up-form/ui/SignUpForm.tsx
 "use client";
-
 import { useForm } from "react-hook-form";
 import { SignUpFormFields } from "./SignUpFormFields";
 import { SignUpFormSubmitButton } from "@/fsd_entities/auth/ui/SignUpSubmitButton";
 import { PreviousButton } from "@/shared";
 import { SignUpHeader } from "./SignUpHeader";
+import { useMutation } from "@tanstack/react-query";
+import { signup } from "@/fsd_entities/auth/api/post";
+import toast from "react-hot-toast";
+
+const allowedKeys = [
+    "email",
+    "nickname",
+    "password",
+    "studentId",
+    "name",
+    "major",
+    "phoneNumber",
+    "admissionYear",
+  ] as readonly string[];
+  
 
 export const SignUpForm = () => {
   const {
@@ -15,13 +29,35 @@ export const SignUpForm = () => {
     formState: { errors },
   } = useForm<User.SignUpForm>({mode: "onBlur"});
 
+  const mutation = useMutation({
+    mutationFn: signup,
+    onSuccess: () => {
+      toast.success("회원가입이 완료되었습니다!");
+    },
+    onError: (error: any) => {
+      toast.error("회원가입 실패:", error);
+    },
+  });
+  
+
   const onSubmit = (data: User.SignUpForm) => {
-    console.log("회원가입 데이터:", data);
-    // TODO: API 요청 연동
-  };
+    const postData = {
+        ...data,
+        admissionYear: data.admissionYearString,
+      };
+      
+    const newPostData = allowedKeys.reduce((acc, key) => {
+        
+        acc[key] = postData[key];
+        return acc;
+      }, {} as Partial<User.SignUpFormPost>);
+      mutation.mutate(newPostData as User.SignUpFormPost);
+    };
+    const onInvalid = () => {
+        toast.error("모든 항목을 조건에 맞게 입력해주세요."); };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center justify-center gap-y-4">
+    <form onSubmit={handleSubmit(onSubmit, onInvalid)} className="flex flex-col items-center justify-center gap-y-4">
       <PreviousButton variant="white"></PreviousButton>
       <SignUpHeader></SignUpHeader>
       <SignUpFormFields register={register} errors={errors} watch={watch} />
