37c27f63dd3321510a760bc6b02a84e1f89fe3d9
younghyun753 <younghyun75398@gmail.com>
2025-03-25 21:23:27 +0900

    refactor: sign-up 제출 버튼 fsd 폴더에 추가 (#158)

---
+++ src/fsd_entities/auth/ui/SignUpSubmitButton.tsx
@@ -0,0 +1,11 @@
+export const SignUpFormSubmitButton = () => {
+    return (
+      <button
+        type="submit"
+        className="flex items-center w-40 justify-center mt-6 h-10 rounded-lg bg-focus text-white hover:bg-blue-400 mb-4"
+      >
+        회원가입
+      </button>
+    );
+  };
+  
