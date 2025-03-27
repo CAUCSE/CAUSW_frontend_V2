c178c862fc9c0a42e60e84c3f47660bdabee291f
younghyun753 <younghyun75398@gmail.com>
2025-03-27 20:47:34 +0900

    feat: 위젯에서 form 구현하지 않고 custom hook 만들어서 사용 (#158)

--- src/fsd_widgets/auth/ui/SignUpForm.tsx
+++ src/fsd_widgets/auth/ui/SignUpForm.tsx
@@ -1,58 +1,13 @@
 "use client";
-import toast from "react-hot-toast";
-import { useForm } from "react-hook-form";
 import { PreviousButton } from "@/shared";
-import { useMutation } from "@tanstack/react-query";
-import { signup, SignUpSubmitButton } from "@/fsd_entities/auth";
+import {  SignUpSubmitButton, useSignUpForm } from "@/fsd_entities/auth";
 import { SignUpHeader } from "./SignUpHeader";
 import { SignUpFormFields } from "./SignUpFormFields";
 
-const allowedKeys = [
-    "email",
-    "nickname",
-    "password",
-    "studentId",
-    "name",
-    "major",
-    "phoneNumber",
-    "admissionYear",
-  ] as readonly string[];
-  
 
 export const SignUpForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<User.SignUpForm>({mode: "onBlur"});
 
-  const mutation = useMutation({
-    mutationFn: signup,
-    onSuccess: () => {
-      toast.success("회원가입이 완료되었습니다!");
-    },
-    onError: (error: any) => {
-      toast.error("회원가입 실패:", error);
-    },
-  });
-  
-
-  const onSubmit = (data: User.SignUpForm) => {
-    const postData = {
-        ...data,
-        admissionYear: data.admissionYearString,
-      };
-      
-    const newPostData = allowedKeys.reduce((acc, key) => {
-        
-        acc[key] = postData[key];
-        return acc;
-      }, {} as Partial<User.SignUpFormPost>);
-      mutation.mutate(newPostData as User.SignUpFormPost);
-    };
-    const onInvalid = () => {
-        toast.error("모든 항목을 조건에 맞게 입력해주세요."); };
+  const { register, handleSubmit, watch, errors, onSubmit, onInvalid } = useSignUpForm();
 
   return (
     <form onSubmit={handleSubmit(onSubmit, onInvalid)} className="flex flex-col items-center justify-center gap-y-4">
