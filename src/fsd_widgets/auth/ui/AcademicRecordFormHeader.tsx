2549ce2f3c7eee6bbe7e3d97ed89ea39b522f41e
younghyun753 <younghyun75398@gmail.com>
2025-03-27 20:48:51 +0900

    refactor: 재학증빙서류 제출 헤더 분리(#158)

---
+++ src/fsd_widgets/auth/ui/AcademicRecordFormHeader.tsx
@@ -0,0 +1,9 @@
+export const AcademicRecordFormHeader = () => {
+    return (
+        <>        
+        <h1 className="text-2xl font-bold mt-8 sm:mt-4">학부 재학 증빙 서류 제출</h1>
+        <p className="text-gray-600 hidden lg:block">
+          재학 중일 시 학부 사무실, 동문회 등의 사업/행사 신청을 위한 증빙 절차입니다. 증빙이 되지 않으면 휴학/졸업이 아닌 재학 중인 회원은 서비스 이용이 어렵습니다.
+        </p>
+        </>
+    );}
