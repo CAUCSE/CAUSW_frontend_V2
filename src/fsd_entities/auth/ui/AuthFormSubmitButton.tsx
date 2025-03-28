02b4ec5d3233605cef820a75db8012350c041c91
younghyun753 <younghyun75398@gmail.com>
2025-03-28 10:39:25 +0900

    refactor: auth에서 사용되는 제출 버튼 통일 (#158)

--- src/fsd_entities/auth/ui/AuthFormSubmitButton.tsx
+++ src/fsd_entities/auth/ui/AuthFormSubmitButton.tsx
@@ -1,10 +1,13 @@
-export const SignUpSubmitButton = () => {
+interface Props {
+  content: string;}
+
+export const AuthFormSubmitButton = ({content}: Props) => {
     return (
       <button
         type="submit"
         className="flex items-center w-40 justify-center mt-6 h-10 rounded-lg bg-focus text-white hover:bg-blue-400 mb-4"
       >
-        회원가입
+        {content}
       </button>
     );
   };
