6353be85d522fa9e3333087448b736b8f4e57f7a
younghyun753 <younghyun75398@gmail.com>
2025-03-28 17:43:43 +0900

    chore: 제출 버튼 스타일 수정(#158)

--- src/fsd_entities/auth/ui/AuthFormSubmitButton.tsx
+++ src/fsd_entities/auth/ui/AuthFormSubmitButton.tsx
@@ -3,12 +3,14 @@ interface Props {
 
 export const AuthFormSubmitButton = ({content}: Props) => {
     return (
+      <div className="w-full flex justify-center">
       <button
         type="submit"
         className="flex items-center w-40 justify-center mt-6 h-10 rounded-lg bg-focus text-white hover:bg-blue-400 mb-4"
       >
         {content}
       </button>
+      </div>
     );
   };
   
