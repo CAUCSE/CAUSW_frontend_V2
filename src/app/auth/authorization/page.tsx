c1d528d73c8034eabf26ac1456d950c3df9f9e24
younghyun753 <younghyun75398@gmail.com>
2025-03-28 17:49:05 +0900

    refactor: 기존 인증 페이지 fsd_widgets 끌어오는 걸로 변경 (#158)

--- src/app/auth/authorization/page.tsx
+++ src/app/auth/authorization/page.tsx
@@ -1,120 +1,39 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { PreviousButton, UserService } from "@/shared";
-import SubmitAcademicRecordModal from "@/entities/auth/AcademicRecordInput";
-import SubmitApplicationModal from "@/entities/auth/AdmissionApplicationPage";
-import { VerificationStatus } from "@/widget";
+import React from "react";
+import { PreviousButton } from "@/shared";
+import { useRouter } from "next/navigation";
+import { useVerification } from "@/fsd_entities/auth";
+import { VerificationHeader, VerificationStatus } from "@/fsd_widgets/auth";
 
 const VerificationPage: React.FC = () => {
-  const { getUserAdmissionInfo, getMyInfo, checkIsAcademicRecordSubmitted } =
-    UserService();
-  const [isAcademicRecordModalOpen, setIsAcademicModalOpen] = useState(false);
-  const [isAdmissionModalOpen, setIsAdmissionModalOpen] = useState(false);
-  const [emailValue, setEmailValue] = useState("");
+  const {
+    emailValue,
+    admissionApplicationStatus,
+    academicRecordApplicationStatus,
+    admissionRejectMessage,
+    academicRecordRejectMessage,
+  } = useVerification();
 
-  // UNDONE, AWAIT, REJECT, COMPLETE로 관리
-  const [admissionApplicationStatus, setAdmissionApplicationStatus] =
-    useState<User.StatusType>("BANNED");
-
-  // BANNED, UNDONE, COMPLETE로 관리
-  const [academicRecordApplicationStatus, setAcademicRecordApplicationStatus] =
-    useState<User.StatusType>("BANNED");
-
-  // 거절 사유 출력 부분
-  const [admissionRejectMessage, setAdmissionRejectMessage] = useState("");
-  const [academicRecordRejectMessage, setAcademicRecordRejectMessage] =
-    useState("");
-
-  useEffect(() => {
-    const getInfo = async () => {
-      try {
-        const response = await getMyInfo();
-        // 상태 값들 가져오기
-        const { state, rejectionOrDropReason, email } = response.data;
-        // 상태 업데이트
-        setEmailValue(email);
-        // 조건문 처리 (상태 값이 올바르게 설정된 후에 처리)
-        if (state === "REJECT") {
-          setAdmissionApplicationStatus("REJECTED");
-          setAcademicRecordApplicationStatus("BANNED");
-          setAdmissionRejectMessage(rejectionOrDropReason);
-          return;
-        }
-
-        if (state === "AWAIT") {
-          setAcademicRecordApplicationStatus("BANNED");
-          return checkAdmissionApplication(); // AWAIT인 경우 학적 상태 증빙 서류 제출 못하니까 admissionapplication 체크
-        }
-
-        if (state === "ACTIVE") {
-          setAdmissionApplicationStatus("COMPLETE");
-          return checkAcademicRecordApplication(); // AWAIT가 아닌 경우 학적 상태 증빙 서류 제출 필요 academicrecordapplication 체크
-        }
-      } catch (error) {}
-    };
-
-    if (!isAcademicRecordModalOpen && !isAdmissionModalOpen) {
-      getInfo();
-    }
-  }, [isAcademicRecordModalOpen, isAdmissionModalOpen]);
-
-  const checkAdmissionApplication = async () => {
-    try {
-      await getUserAdmissionInfo();
-      setAdmissionApplicationStatus("AWAIT");
-    } catch (error) {
-      setAdmissionApplicationStatus("UNDONE");
-    }
-  };
-
-  const checkAcademicRecordApplication = async () => {
-    try {
-      const { isRejected, rejectMessage } = (
-        await checkIsAcademicRecordSubmitted()
-      ).data;
-      if (isRejected) {
-        setAcademicRecordApplicationStatus("REJECTED");
-        setAcademicRecordRejectMessage(rejectMessage);
-      } else {
-        setAcademicRecordApplicationStatus("COMPLETE");
-      }
-    } catch (error) {
-      setAcademicRecordApplicationStatus("UNDONE");
-    }
-  };
-
+  const router = useRouter();
+  
   return (
-    <div>
-      {isAcademicRecordModalOpen && (
-        <SubmitAcademicRecordModal
-          onClose={() => {
-            setIsAcademicModalOpen(false);
-          }}
-          rejectMessage={academicRecordRejectMessage}
-        />
-      )}
-      {isAdmissionModalOpen && (
-        <SubmitApplicationModal
-          onClose={() => {
-            setIsAdmissionModalOpen(false);
-          }}
-          emailValue={`${emailValue}`}
-          rejectMessage={admissionRejectMessage}
-        />
-      )}
-      {!isAcademicRecordModalOpen && !isAdmissionModalOpen && (
-        <>
+        <div className="flex h-screen items-center justify-center bg-boardPageBackground p-4">
+          <div className="w-full max-w-lg rounded-lg bg-white p-8 text-center shadow-md">
           <PreviousButton />
+          <VerificationHeader/>
+
+          { (admissionRejectMessage || academicRecordRejectMessage) && 
+          <p className="text-error font-bold break-words"> {academicRecordRejectMessage === ""? "가입 신청서" : "재학 증빙 서류"} 거절 사유
+            : {admissionRejectMessage || academicRecordRejectMessage}</p>}
+
           <VerificationStatus
-            admissionApplicationStatus={admissionApplicationStatus}
-            academicRecordApplicationStatus={academicRecordApplicationStatus}
-            onAdmissionClick={() => setIsAdmissionModalOpen(true)}
-            onAcademicRecordClick={() => setIsAcademicModalOpen(true)}
-          />
-        </>
-      )}
-    </div>
+          admissionApplicationStatus={admissionApplicationStatus}
+          academicRecordApplicationStatus={academicRecordApplicationStatus}
+          onAdmissionClick={() => router.push("/auth/authorization/admission")}
+          onAcademicRecordClick={() => router.push("/auth/authorization/academic-record")}/>
+          </div>
+        </div>
   );
 };
 
