40f4bbe72832b637b2488e7fb3d319879b561f0c
younghyun753 <younghyun75398@gmail.com>
2025-03-28 17:48:38 +0900

    refactor: 위젯 export 추가 (#158)

--- src/fsd_widgets/auth/index.ts
+++ src/fsd_widgets/auth/index.ts
@@ -7,3 +7,11 @@ export { SignInFooter } from "./ui/SignInFooter";
 export { SignUpHeader } from "./ui/SignUpHeader";
 export { SignUpFormFields } from "./ui/SignUpFormFields";
 export { SignUpForm } from "./ui/SignUpForm";
+
+export { AcademicRecordForm } from "./ui/AcademicRecordForm";
+export { AcademicRecordFormHeader } from "./ui/AcademicRecordFormHeader";
+
+export { AdmissionForm } from "./ui/AdmissionForm";
+
+export { VerificationStatus } from "./ui/VerificationStatus";
+export { VerificationHeader } from "./ui/VerificationHeader";
