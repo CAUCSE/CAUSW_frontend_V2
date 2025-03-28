783e0c40bbd57589df6b72f0fe9d79fdaaf2fa28
younghyun753 <younghyun75398@gmail.com>
2025-03-28 18:12:56 +0900

    refactor: 로그인 기능 공용 api에 추가 (#158)

---
+++ src/fsd_shared/hooks/AuthService.ts
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  API,
+  UserService,
+  setRccToken,
+  setRscToken,
+  useLayoutStore,
+} from "@/shared";
+import { AxiosResponse } from "axios";
+
+import { useRouter } from "next/navigation";
+
+const URI = "/api/v1/users";
+const { getMyInfo } = UserService();
+
+const router = useRouter();
+const setErrorMessage = useLayoutStore((state) => state.setErrorMessage);
+
+export const signin = async (body: User.SignInRequestDto) => {
+try {
+    const {
+    data: { accessToken, refreshToken },
+    } = (await API.post(`${URI}/sign-in`, body)) as AxiosResponse<{
+    accessToken: string;
+    refreshToken: string;
+    }>;
+
+    await setRscToken(accessToken, body.auto ? refreshToken : false);
+    await setRccToken(accessToken, body.auto ? refreshToken : false);
+
+    const AdmissionResponse = await getMyInfo();
+
+    if (AdmissionResponse.data.state === "AWAIT") {
+    router.push("/auth/authorization");
+    } else {
+    if (AdmissionResponse.data.academicStatus == "UNDETERMINED") {
+        router.push("/auth/authorization");
+    } else {
+        router.push("/home");
+    }
+    }
+} catch (error) {
+    setErrorMessage("로그인 정보가 일치하지 않습니다!");
+}
+};
+
