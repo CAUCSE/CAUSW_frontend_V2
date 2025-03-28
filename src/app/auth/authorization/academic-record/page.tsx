b87efe908c9e29c0ff6c1d173efd95a291e5d021
younghyun753 <younghyun75398@gmail.com>
2025-03-28 17:49:28 +0900

    refactor: 기존 admission, academic-record 모달 형식에서 페이지 형식으로 변경(#158)

---
+++ src/app/auth/authorization/academic-record/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { AcademicRecordForm } from "@/fsd_widgets/auth";
+import React from "react";
+
+const UpdataeAcademicRecordPage = () => {
+
+  return (
+        <AcademicRecordForm curAcademicStatus="UNDEFINED"/>
+  );
+};
+
+export default UpdataeAcademicRecordPage;
