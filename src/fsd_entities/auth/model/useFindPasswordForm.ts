92d8ecbbb38a72ded2abde1ee805691c34d66960
younghyun753 <younghyun75398@gmail.com>
2025-03-30 02:43:16 +0900

    refactor: 비밀번호, 이메일 찾기 폼 커스텀 훅 분리 (#158)

---
+++ src/fsd_entities/auth/model/useFindPasswordForm.ts
@@ -0,0 +1,52 @@
+"use client";
+
+import { useFindAccountStore } from "@/shared";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useShallow } from "zustand/react/shallow";
+import { useFindPassword } from "./useFindPassword";
+
+interface FormData {
+  name: string;
+  studentId: string;
+  email: string;
+}
+
+export const useFindPasswordForm = () => {
+    const router = useRouter();
+    const { studentId, name, email, resetFindAccountStore } = useFindAccountStore(
+      useShallow((state) => ({
+        studentId: state.studentId,
+        name: state.name,
+        email: state.email,
+        resetFindAccountStore: state.resetFindAccountStore,
+      })),
+    );
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<FormData>({
+      defaultValues: {
+        studentId,
+        name,
+        email,
+      },
+    });
+  
+    const { isSuccess, mutate: findpassword } = useFindPassword();
+  
+    const onSubmit = async (data: FormData) => {
+      findpassword({
+        name: data.name,
+        studentId: data.studentId,
+        email: data.email,
+      });
+    };
+  
+    const handleRouterToSignIn = () => {
+      router.push("/auth/signin");
+      resetFindAccountStore();
+    };
+  return { register, handleSubmit, formState: { errors }, onSubmit, handleRouterToSignIn, isSuccess };
+}
