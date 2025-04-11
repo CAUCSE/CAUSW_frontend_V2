83504980283f52cb08ca2d165850bc99eeb8be48
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-11 22:02:22 +0900

    chore: BoardList 폴더로 파일 이동

--- src/fsd_entities/board/ui/index.ts
+++ src/fsd_entities/board/ui/index.ts
@@ -1,4 +1,1 @@
-export { Board } from './Board';
-export { BoardEmptyPostPreview } from './BoardEmptyPostPreview';
-export { BoardPostPreview } from './BoardPostPreview';
-export { EmptyBoard } from './EmptyBoard';
+export * from './BoardList';
