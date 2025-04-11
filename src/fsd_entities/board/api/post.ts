824b96086a3c3586445d3168137dd5c73b59b9ba
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-12 02:29:48 +0900

    feat: 게시판 생성 관련 POST 메서드 추가

---
+++ src/fsd_entities/board/api/post.ts
@@ -0,0 +1,41 @@
+import { isAxiosError } from 'axios';
+
+import { API } from '@/shared';
+
+interface CreateNoticeBoardDto {
+  boardName: string;
+  description: string;
+  boardCategory?: 'APP_NOTICE' | 'APP_FREE';
+  createRoleList: User.Role[];
+  isAnonymousAllowed: boolean;
+  circleId?: string;
+}
+
+export const createNoticeBoard = async (payload: CreateNoticeBoardDto): Promise<void> => {
+  try {
+    await API.post('/api/v1/boards/create', payload);
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || '게시판 생성에 실패했습니다');
+    }
+    throw new Error('게시판 생성에 실패했습니다');
+  }
+};
+
+interface CreateCommonBoardDto {
+  boardName: string;
+  description: string;
+  isAnonymousAllowed: boolean;
+  circleId?: string;
+}
+
+export const createCommonBoard = async (payload: CreateCommonBoardDto): Promise<void> => {
+  try {
+    await API.post('/api/v1/boards/apply', payload);
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || '게시판 생성에 실패했습니다');
+    }
+    throw new Error('게시판 생성에 실패했습니다');
+  }
+};
