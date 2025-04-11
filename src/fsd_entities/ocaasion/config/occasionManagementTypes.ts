478784847e4a459075ea822a69eaa74918625cf8
causyj <jainayj804998@gmail.com>
2025-04-11 19:58:08 +0900

    chore: restruct ceremony types (#159)

--- src/fsd_entities/ocaasion/config/occasionManagementTypes.ts
+++ src/fsd_entities/ocaasion/config/occasionManagementTypes.ts
@@ -1,18 +1,3 @@
-interface OccasionRequestManagementProps {
-  state: string | undefined;
-  title: string;
-  firstNavigation: {
-    name: string;
-    state: string;
-    router: string;
-  };
-  navigation?: {
-    name: string;
-    state: string;
-    router: string;
-  }[];
-}
-
 interface Occasion {
   id: string;
   description: string;
@@ -23,18 +8,49 @@ interface Occasion {
   attachedImageUrlList: string[];
   note: string | null;
 }
+interface OccasionData {
+  content: Occasion[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+interface NavigationItem {
+  name: string;
+  state: string;
+  router: string;
+}
+interface OccasionRequestManagementProps {
+  state: string | undefined;
+  title: string;
+  firstNavigation: NavigationItem;
+  navigation?: NavigationItem[];
+}
 
 type OccasionListProps = {
   list: Occasion[];
-  firstNavigation: {
-    name: string;
-    state: string;
-    router: string;
-  };
-  navigation?: {
-    name: string;
-    state: string;
-    router: string;
-  }[];
+  firstNavigation: NavigationItem;
+  navigation?: NavigationItem[];
   state: string | undefined;
 };
