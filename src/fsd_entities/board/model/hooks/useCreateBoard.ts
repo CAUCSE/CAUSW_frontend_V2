4585de0f3d6a859e1d70b033d29860764dbbbf8c
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-12 02:33:25 +0900

    refactor: 게시판 생성 관련 훅 로직 변경 및 fsd_entity로 이동

---
+++ src/fsd_entities/board/model/hooks/useCreateBoard.ts
@@ -0,0 +1,27 @@
+'use client';
+
+import { toast } from 'react-hot-toast';
+
+import { useUserStore } from '@/shared';
+
+import { useCreateCommonBoard, useCreateNoticeBoard } from '../queries';
+import { useBoardCreationStore } from '../stores';
+
+export const useCreateBoard = () => {
+  const boardName = useBoardCreationStore(state => state.boardName);
+  const userRole = useUserStore(state => state.roles);
+  const hasAuth = userRole.includes('ADMIN') || userRole.includes('PRESIDENT') || userRole.includes('VICE_PRESIDENT');
+  console.log(hasAuth);
+  const { mutate: createNoticeBoard } = useCreateNoticeBoard();
+  const { mutate: createCommonBoard } = useCreateCommonBoard();
+
+  const handleSubmit = async () => {
+    if (boardName.trim() === '') {
+      toast.error('게시판 이름을 입력해주세요.');
+      return;
+    }
+    hasAuth ? createNoticeBoard() : createCommonBoard();
+  };
+
+  return { handleSubmit, hasAuth };
+};
