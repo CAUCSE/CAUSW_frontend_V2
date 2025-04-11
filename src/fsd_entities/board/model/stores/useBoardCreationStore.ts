87d733dbe4056fead89141a66217a7516d66f09e
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-12 02:32:58 +0900

    feat: 게시판 생성 관련 전역 상태 추가

---
+++ src/fsd_entities/board/model/stores/useBoardCreationStore.ts
@@ -0,0 +1,42 @@
+import { create, StateCreator } from 'zustand';
+
+type TRoleList = User.Role | 'ALL';
+
+interface BoardCreationState {
+  boardName: string;
+  boardDescription: string;
+  allowAnonymous: boolean;
+  selectedRoleList: TRoleList[];
+}
+
+interface BoardCreationAction {
+  setBoardName: (boardName: string) => void;
+  setBoardDescription: (boardDescription: string) => void;
+  setAllowAnonymous: (allowAnonymous: boolean) => void;
+  setSelectedRoleList: (selectedRoleList: TRoleList[]) => void;
+  resetBoardCreation: () => void;
+}
+
+type BoardCreationSlice = BoardCreationState & BoardCreationAction;
+
+const createBoardCreationSlice: StateCreator<BoardCreationSlice, [], [], BoardCreationSlice> = set => ({
+  boardName: '',
+  boardDescription: '',
+  allowAnonymous: false,
+  selectedRoleList: ['ALL'],
+  setBoardName: boardName => set({ boardName }),
+  setBoardDescription: boardDescription => set({ boardDescription }),
+  setAllowAnonymous: allowAnonymous => set({ allowAnonymous }),
+  setSelectedRoleList: selectedRoleList => set({ selectedRoleList }),
+  resetBoardCreation: () =>
+    set({
+      boardName: '',
+      boardDescription: '',
+      allowAnonymous: false,
+      selectedRoleList: ['ALL'],
+    }),
+});
+
+export const useBoardCreationStore = create<BoardCreationSlice>()((...args) => ({
+  ...createBoardCreationSlice(...args),
+}));
