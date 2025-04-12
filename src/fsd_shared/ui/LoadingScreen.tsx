b73e7b84c322cb73c494f16bafb76cb6f3d73af7
leeyeongjae <xingxing2001@cau.ac.kr>
2025-04-12 18:39:41 +0900

    feat: Loading화면 추가

---
+++ src/fsd_shared/ui/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+import { BarLoader } from 'react-spinners';
+
+export const LoadingScreen = () => {
+  return (
+    <div className="fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-60 text-2xl font-bold text-white">
+      <BarLoader color="white" speedMultiplier={1} />
+    </div>
+  );
+};
