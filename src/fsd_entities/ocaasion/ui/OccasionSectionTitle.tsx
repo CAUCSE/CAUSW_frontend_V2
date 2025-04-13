6b47bd2f2a0c7289df418d4aa517d053c8d53dd7
causyj <jainayj804998@gmail.com>
2025-04-13 13:32:40 +0900

    style: edit condition styling using clsx (#159)

--- src/fsd_entities/ocaasion/ui/OccasionSectionTitle.tsx
+++ src/fsd_entities/ocaasion/ui/OccasionSectionTitle.tsx
@@ -1,12 +1,12 @@
 import { MESSAGES } from '@/fsd_shared';
+import clsx from 'clsx';
 
 export const OccasionSectionTitle = ({ title, occasionContent }: OccasionSectionTitleProps) => {
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-lg font-semibold md:text-2xl">{title}</h1>
-      <p
-        className={`whitespace-pre-line break-keep text-xl font-medium ${title === MESSAGES.OCCASION.DETAIL_CONTENTS ? 'rounded-2xl border border-[#d9d9d9] p-6' : ''}`}
-      >
+    
+<p className={clsx(title === MESSAGES.OCCASION.DETAIL_CONTENTS && 'rounded-2xl border border-[#d9d9d9] p-6', 'whitespace-pre-line break-keep text-xl font-medium')}>
         {occasionContent}
       </p>
     </div>
