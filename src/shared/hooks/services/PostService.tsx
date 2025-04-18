9be36e725d34f892912a82f9a7edec75211382b0
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-18 22:45:37 +0900

    feat: 게시물 검색 리스트 훅 추가 및 PostService에서 관련 코드 제거

--- src/shared/hooks/services/PostService.tsx
+++ src/shared/hooks/services/PostService.tsx
@@ -2,52 +2,12 @@
 
 import { useRouter } from 'next/navigation';
 
-import { useInfiniteQuery, useMutation } from '@tanstack/react-query';
-import { clear } from 'console';
+import { useMutation } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 
-import { API, FORMAPI, postQueryKey, useCreatePostStore, useCreateVoteStore, useFileUploadStore } from '@/shared';
+import { API, FORMAPI, useCreatePostStore, useCreateVoteStore, useFileUploadStore } from '@/shared';
 
 export const PostService = () => {
-  const useGetPostList = boardId => {
-    return useInfiniteQuery({
-      queryKey: postQueryKey.list(boardId),
-      queryFn: async ({ pageParam }) => {
-        const { data }: { data: Board.BoardWithPostResponseDto } = await API.get(
-          `/api/v1/posts?boardId=${boardId}&pageNum=${pageParam}`,
-        );
-        return data;
-      },
-      initialPageParam: 0,
-      getNextPageParam: lastPage => {
-        return lastPage.post.last ? null : lastPage.post.number + 1;
-      },
-      select: data => {
-        return data.pages.flatMap(page => page.post.content);
-      },
-    });
-  };
-
-  const useGetSearchPostList = (boardId: string, keyword: string, isSearch: boolean) => {
-    return useInfiniteQuery({
-      queryKey: postQueryKey.searchResult(boardId, keyword),
-      queryFn: async ({ pageParam }) => {
-        const { data }: { data: Board.BoardWithPostResponseDto } = await API.get(
-          `/api/v1/posts/search?boardId=${boardId}&keyword=${encodeURIComponent(keyword)}&pageNum=${pageParam}`,
-        );
-        return data;
-      },
-      initialPageParam: 0,
-      getNextPageParam: lastPage => {
-        return lastPage.post.last ? null : lastPage.post.number + 1;
-      },
-      select: data => {
-        return data.pages.flatMap(page => page.post.content);
-      },
-      enabled: isSearch && keyword !== '',
-    });
-  };
-
   const useCreatePost = () => {
     const router = useRouter();
     const clearPost = useCreatePostStore(state => state.clearPost);
@@ -182,8 +142,6 @@ export const PostService = () => {
   };
 
   return {
-    useGetPostList,
-    useGetSearchPostList,
     useCreatePost,
     useCreatePostWithForm,
     useCreatePostWithVote,
