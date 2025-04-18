8b31ab4d68d4774169c71449fb375597cf4e3c6c
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-18 22:45:19 +0900

    feat: 게시물 검색 API 추가

--- src/fsd_entities/post/api/get.ts
+++ src/fsd_entities/post/api/get.ts
@@ -6,3 +6,18 @@ export const getPostList = async ({ boardId, pageNum }: { boardId: string; pageN
   );
   return data;
 };
+
+export const getSearchPostList = async ({
+  boardId,
+  keyword,
+  pageNum,
+}: {
+  boardId: string;
+  keyword: string;
+  pageNum: number;
+}) => {
+  const { data }: { data: Board.BoardWithPostResponseDto } = await API.get(
+    `/api/v1/posts/search?boardId=${boardId}&keyword=${encodeURIComponent(keyword)}&pageNum=${pageNum}`,
+  );
+  return data;
+};
