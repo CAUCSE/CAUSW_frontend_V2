1810bc0f8a8af625a4b599b292eb92f18b3689a2
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-18 22:47:26 +0900

    style: 에러 메시지에 텍스트-nowrap 클래스 추가

--- src/app/(causw)/board/[boardId]/search/error.tsx
+++ src/app/(causw)/board/[boardId]/search/error.tsx
@@ -8,7 +8,7 @@ const Error = () => (
   <HomeLayout>
     <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
       <Image src="/images/puang-proud.png" alt="404" width={200} height={250}></Image>
-      <span>게시물 검색 도중 에러가 발생했습니다.</span>
+      <span className="text-nowrap">게시물 검색 도중 에러가 발생했습니다.</span>
     </div>
   </HomeLayout>
 );
