c6802c7d4648ff84bc0e817ac7d95edcc56bbc97
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-19 02:08:07 +0900

    refactor: 기존 게시글 생성 관련 전역 상태 fsd_entities 레이어로 이동

---
+++ src/fsd_entities/post/model/stores/usePostCreationStore.ts
@@ -0,0 +1,56 @@
+import { create, StateCreator } from 'zustand';
+
+interface PostCreationState {
+  title: string;
+  content: string;
+  isQuestion: boolean;
+  isAnonymous: boolean;
+  isVote: boolean;
+  isApply: boolean;
+}
+
+interface PostCreationAction {
+  setTitle: (title: string) => void;
+  setContent: (content: string) => void;
+  setIsQuestion: (isQuestion: boolean) => void;
+  setIsAnonymous: (isAnonymous: boolean) => void;
+  toggleQuestion: () => void;
+  toggleAnonymous: () => void;
+  toggleVote: () => void;
+  toggleApply: () => void;
+  clearPost: () => void;
+}
+
+type PostCreationSlice = PostCreationState & PostCreationAction;
+
+const createPostCreationSlice: StateCreator<PostCreationSlice, [], [], PostCreationSlice> = set => ({
+  title: '',
+  content: '',
+  isQuestion: false,
+  isAnonymous: false,
+  isVote: false,
+  isApply: false,
+
+  setTitle: title => set({ title }),
+  setContent: content => set({ content }),
+  setIsQuestion: isQuestion => set({ isQuestion }),
+  setIsAnonymous: isAnonymous => set({ isAnonymous }),
+  toggleQuestion: () => set(state => ({ isQuestion: !state.isQuestion })),
+  toggleAnonymous: () => set(state => ({ isAnonymous: !state.isAnonymous })),
+  toggleVote: () => set(state => ({ isVote: !state.isVote, isApply: false })),
+  toggleApply: () => set(state => ({ isVote: false, isApply: !state.isApply })),
+  clearPost: () => {
+    set({
+      title: '',
+      content: '',
+      isAnonymous: false,
+      isQuestion: false,
+      isApply: false,
+      isVote: false,
+    });
+  },
+});
+
+export const usePostCreationStore = create<PostCreationSlice>()((...args) => ({
+  ...createPostCreationSlice(...args),
+}));
