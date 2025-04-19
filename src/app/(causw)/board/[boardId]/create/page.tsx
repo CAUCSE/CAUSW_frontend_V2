829adc34cc0706181148c7b45c97b164bde87a20
lee0jae330 <xingxing2001@cau.ac.kr>
2025-04-20 04:10:17 +0900

    refactor: Upload file preview 컴포넌트 fsd_widget 레이어로 이동

--- src/app/(causw)/board/[boardId]/create/page.tsx
+++ src/app/(causw)/board/[boardId]/create/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
+import { useShallow } from 'zustand/react/shallow';
+
 import { PostCreationForm, PostCreationFormButtonGroup } from '@/fsd_widgets/post';
 import { VoteCreationForm } from '@/fsd_widgets/vote';
 
-import { usePostCreationStore } from '@/fsd_entities/post';
+import { UploadFilePreview, usePostCreationStore, useUploadFileStore } from '@/fsd_entities/post';
 
-import { FilePreview } from '@/entities';
-import { PreviousButton, useCreateApply, useFileUpload, usePostForm } from '@/shared';
+import { PreviousButton, useCreateApply, usePostForm } from '@/shared';
 import { ApplyCreationForm } from '@/widget';
 
 const CreatePostPage = () => {
-  const { isApply, isVote } = usePostCreationStore();
+  const { isApply, isVote } = usePostCreationStore(
+    useShallow(state => ({
+      isApply: state.isApply,
+      isVote: state.isVote,
+    })),
+  );
 
-  const { selectedFiles } = useFileUpload();
+  const selectedFileList = useUploadFileStore(state => state.selectedFileList);
 
   const { methods, register, watch, errors, fields, remove, handleSubmit, addSurveyForm, onSubmit } = useCreateApply();
 
@@ -24,7 +30,7 @@ const CreatePostPage = () => {
         <div className="w-full flex-col items-center">
           <PreviousButton routeCallback={handleBack} />
         </div>
-        <div className="flex h-full flex-col p-2 pt-10 lg:px-5 lg:py-10">
+        <div className="flex h-full flex-col overflow-y-auto p-2 pt-10 lg:px-5 lg:py-10">
           {isApply ? (
             <ApplyCreationForm
               methods={methods}
@@ -42,7 +48,7 @@ const CreatePostPage = () => {
               <PostCreationForm />
               {/* 투표 파트 */}
               {isVote && <VoteCreationForm />}
-              {selectedFiles.length > 0 && <FilePreview />}
+              {selectedFileList.length > 0 && <UploadFilePreview />}
             </>
           )}
         </div>
