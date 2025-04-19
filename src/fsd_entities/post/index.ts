f08e9fa421d5beea652aa095ce4484d1a5e42e86
leeyeongjae <xingxing2001@cau.ac.kr>
2025-04-19 16:15:04 +0900

    feat: 서버에서 게시글 목록을 가져오는 기능 추가 및 배럴 파일 업데이트

--- src/fsd_entities/post/index.ts
+++ src/fsd_entities/post/index.ts
@@ -1,2 +1,4 @@
 export { PostCard, PostSkeletonList, PostTitleInput, QuestionToggle, AnonymousToggle, PostContentTextarea } from './ui';
 export { useGetPostList, useGetPostSearchList, useSearchPost, usePostCreationStore } from './model';
+export { postQueryKey } from './config';
+export { getPostListServer } from './api';
