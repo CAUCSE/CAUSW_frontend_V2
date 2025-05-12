8943cf1c7d71a61545b86b048fdbae84f9d338ed
selfishAltruism <officialkyus@gmail.com>
2025-05-12 12:49:26 +0900

    refactor: replace user get api folder (#172)

--- src/fsd_entities/user/api/get.ts
+++ src/fsd_entities/user/api/get.ts
@@ -1,8 +1,160 @@
-import { BASEURL, setRscHeader } from '@/fsd_shared';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 
-import { PAGE_SIZE, URI } from '../../config';
+import { API, setRscHeader, settingQueryKey } from '@/fsd_shared';
+import { BASEURL } from '@/fsd_shared';
+
+import { PAGE_SIZE, URI } from '../config';
 
 const SSR_URI = BASEURL + URI;
+
+// CSR API Method
+
+export const getMyInfo = async () => {
+  try {
+    const response = await API.get(`${URI}/me`); // 서버로부터 유저 정보를 가져옴
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserInfo = async (userId: string) => {
+  try {
+    const response = await API.get(`${URI}/${userId}`);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserAdmissionInfo = async () => {
+  const response = await API.get(`${URI}/admissions/self`);
+  return response;
+};
+
+export const checkCurrentAcademicStatus = async () => {
+  try {
+    const response = (await API.get(`${URI}/academic-record/current`)) as AxiosResponse;
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const checkIsAcademicRecordSubmitted = async () => {
+  try {
+    const response = (await API.get(`${URI}/academic-record/current/not-accepted`)) as AxiosResponse;
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const checkIsCurrentSemesterApplied = async (userId: string) => {
+  try {
+    const response =
+      (await API.get(`${URI}-council-fee/isCurrentSemesterApplied`),
+      {
+        headers: {
+          params: userId,
+        },
+      });
+    return response;
+  } catch (error) {
+    return false;
+  }
+};
+
+// 전 SettingService 리팩터링
+export const getUserByName = async (name: string) => {
+  const { data } = (await API.get(`${URI}/name/${name}`)) as AxiosResponse<User.User[]>;
+
+  return data;
+};
+
+export const useGetAttendanceUser = (id: string) => {
+  return useQuery({
+    queryKey: ['attendanceUser', id],
+    queryFn: async () => {
+      const { data } = (await API.get(
+        `${URI}/academic-record/record/${id}`,
+      )) as AxiosResponse<Setting.GetAttendanceUserResponseDto>;
+
+      return data;
+    },
+    enabled: !!id,
+  });
+};
+
+export const useGetWaitingUser = (userId: string, applicationId: string) => {
+  return useQuery({
+    queryKey: ['waitingUser', userId, applicationId],
+    queryFn: async () => {
+      const { data } = (await API.get(
+        `${URI}/academic-record/application/${userId}/${applicationId}`,
+      )) as AxiosResponse<Setting.GetWaitingUserResponseDto>;
+
+      return data;
+    },
+    enabled: !!userId,
+  });
+};
+
+export const useGetMyPosts = () => {
+  return useInfiniteQuery({
+    queryKey: settingQueryKey.myPost(),
+    queryFn: async ({ pageParam }) => {
+      const { data }: { data: User.UserPostsResponseDto } = await API.get(`${URI}/posts/written?pageNum=${pageParam}`);
+      return data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      return lastPage.posts.last ? null : lastPage.posts.number + 1;
+    },
+    select: data => {
+      return data.pages.flatMap(page => page.posts.content);
+    },
+  });
+};
+
+export const useGetMyCommentPosts = () => {
+  return useInfiniteQuery({
+    queryKey: settingQueryKey.myCommentPost(),
+    queryFn: async ({ pageParam }) => {
+      const { data }: { data: User.UserPostsResponseDto } = await API.get(
+        `${URI}/comments/written?pageNum=${pageParam}`,
+      );
+      return data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      return lastPage.posts.last ? null : lastPage.posts.number + 1;
+    },
+    select: data => {
+      return data.pages.flatMap(page => page.posts.content);
+    },
+  });
+};
+
+export const useGetMyFavoritePosts = () => {
+  return useInfiniteQuery({
+    queryKey: settingQueryKey.myFavoritePost(),
+    queryFn: async ({ pageParam }) => {
+      const { data }: { data: User.UserPostsResponseDto } = await API.get(`${URI}/posts/favorite?pageNum=${pageParam}`);
+      return data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      return lastPage.posts.last ? null : lastPage.posts.number + 1;
+    },
+    select: data => {
+      return data.pages.flatMap(page => page.posts.content);
+    },
+  });
+};
+
+// SSR API Method
 
 //함수 생성자
 const setGetMethod = (endpoint: string) => {
--- src/fsd_entities/user/api/get/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './csr';
-export * from './ssr';
