10b364d7bad4d702183a4406ab8e792422128bb5
causyj <jainayj804998@gmail.com>
2025-05-18 13:59:59 +0900

    feat: add markAsRead post api when alarm is opened (#185)

--- src/fsd_shared/ui/ListBox.tsx
+++ src/fsd_shared/ui/ListBox.tsx
@@ -2,9 +2,9 @@
 
 import { useCallback, useEffect, useState } from 'react';
 
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-import { Mail, MailOpen } from 'lucide-react';
+import { useNotificationStore } from '@/fsd_entities/notification';
 
 import { useInfiniteScroll } from '@/fsd_shared/hooks/useInfiniteScroll';
 
@@ -26,7 +26,10 @@ interface ListBoxProps {
 export const ListBox = ({ data, link }: ListBoxProps) => {
   const [items, setItems] = useState<CeremonyItem[]>([]);
   const [page, setPage] = useState(0);
+  const router = useRouter();
   const itemsPerPage = 4;
+  const markAsRead = useNotificationStore(state => state.markAsRead);
+
   useEffect(() => {
     const nextItems = data.slice(0, itemsPerPage);
     setItems(nextItems);
@@ -52,40 +55,45 @@ export const ListBox = ({ data, link }: ListBoxProps) => {
   useEffect(() => {
     loadMore();
   }, []);
+
   return (
     <div className="max-h-[400px] max-w-[560px] overflow-y-auto rounded-lg bg-[#D9D9D9] p-4">
       <div className="flex flex-col space-y-4">
-        {items.map(item => (
-          <Link
-            href={
-              link
-                ? `/board/${link.find(l => l.notificationLogId === item.id)?.boardId}/${link.find(l => l.notificationLogId === item.id)?.targetId}`
-                : `/ceremony/${item.id}`
-            }
-            key={item.id}
-            className="relative flex items-center gap-4 rounded-xl bg-[#F4F4F4] p-4 shadow"
-          >
-            {!item.isRead && (
-              <span
-                style={{
-                  position: 'absolute',
-                  top: 6,
-                  left: 6,
-                  width: 10,
-                  height: 10,
-                  borderRadius: '50%',
-                  backgroundColor: 'red',
-                }}
-              />
-            )}
+        {items.map(item => {
+          const targetLink = link
+            ? `/board/${link.find(l => l.notificationLogId === item.id)?.boardId}/${link.find(l => l.notificationLogId === item.id)?.targetId}`
+            : `/ceremony/${item.id}`;
+          return (
+            <div
+              onClick={() => {
+                markAsRead(item.id);
+                router.push(targetLink);
+              }}
+              key={item.id}
+              className="relative flex items-center gap-4 rounded-xl bg-[#F4F4F4] p-4 shadow"
+            >
+              {!item.isRead && (
+                <span
+                  style={{
+                    position: 'absolute',
+                    top: 6,
+                    left: 6,
+                    width: 10,
+                    height: 10,
+                    borderRadius: '50%',
+                    backgroundColor: 'red',
+                  }}
+                />
+              )}
 
-            <div className="text-gray-600">{item.isRead ? <MailOpendIcon size={32} /> : <MailIcon size={32} />}</div>
-            <div className="text-left">
-              <div className="font-medium text-[#212323]">{item.title}</div>
-              <div className="text-sm text-[#212323]">{item.body}</div>
+              <div className="text-gray-600">{item.isRead ? <MailOpendIcon size={32} /> : <MailIcon size={32} />}</div>
+              <div className="text-left">
+                <div className="font-medium text-[#212323]">{item.title}</div>
+                <div className="text-sm text-[#212323]">{item.body}</div>
+              </div>
             </div>
-          </Link>
-        ))}
+          );
+        })}
       </div>
       <div ref={targetRef} className="invisible h-px" />
     </div>
