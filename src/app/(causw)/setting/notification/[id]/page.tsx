0c4df1eb9132b931f345ba0ff80f4bd7f00eca61
causyj <jainayj804998@gmail.com>
2025-05-19 22:34:08 +0900

    chore: reset file(#185)

--- src/app/(causw)/setting/notification/[id]/page.tsx
+++ src/app/(causw)/setting/notification/[id]/page.tsx
@@ -3,23 +3,21 @@
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
-import { OccasionDetailPage } from '@/fsd_widgets/occasion';
-
 import { Header } from '@/entities';
+import { OccasionNotificationDetail } from '@/widget';
 
 const OccasionRequestDetailPage = () => {
   const { occasionId } = useParams<{ occasionId: string }>();
   return (
     <div className="relative left-4 top-3 w-[calc(100%-2rem)] md:left-14 md:top-14 md:w-[calc(100%-7rem)]">
-      <Link href="/setting/notification" className="mb-7 flex items-center text-lg">
+      <Link href="/setting/notification/occasion" className="mb-7 flex items-center text-lg">
         <span className="icon-[weui--back-filled] mr-6 text-3xl font-bold"></span>
         이전
       </Link>
       <Header bold big>
         경조사 정보
       </Header>
-
-      <OccasionDetailPage occasionId={occasionId} />
+      <OccasionNotificationDetail occasionId={occasionId} />
     </div>
   );
 };
