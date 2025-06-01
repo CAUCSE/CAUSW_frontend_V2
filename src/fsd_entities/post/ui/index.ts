e882bea83f9e6edbf6c9f90efa2b88093de542d8
leeyeongjae <xingxing2001@cau.ac.kr>
2025-06-01 18:32:17 +0900

    chore: 기존 버튼 컴포넌트들을 분리된 버튼 컴포넌트로 교체

--- src/fsd_entities/post/ui/index.ts
+++ src/fsd_entities/post/ui/index.ts
@@ -2,3 +2,5 @@ export * from './PostCard';
 export * from './PostCreationForm';
 export * from './PostCreationFormButtonGroup';
 export * from './UploadFilePreview';
+export { CreatePostButton } from './CreatePostButton';
+export { SearchPostButton } from './SearchPostButton';
