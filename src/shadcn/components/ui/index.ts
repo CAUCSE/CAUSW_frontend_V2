152821632d2b1c454e516eccb02afd5a4f7a8c72
lee0jae330 <xingxing2001@cau.ac.kr>
2025-06-03 19:52:42 +0900

    feat: dropdown 컴포넌트 추가

--- src/shadcn/components/ui/index.ts
+++ src/shadcn/components/ui/index.ts
@@ -1,1 +1,15 @@
 export { Button } from './button';
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
