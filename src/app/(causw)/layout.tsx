4e7fd9eea752bd1c2e3eadd3c52c6bd51d8a5ef4
lee0jae330 <xingxing2001@cau.ac.kr>
2025-06-06 17:31:06 +0900

    chore: 레이아웃 수정

--- src/app/(causw)/layout.tsx
+++ src/app/(causw)/layout.tsx
@@ -8,7 +8,7 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
       <div className="grid h-screen w-full grid-rows-[3.4375rem_1fr_4rem] xl:grid-cols-[10rem_1fr_18rem] xl:grid-rows-none">
         <NavigationBar className="relative hidden h-full w-full xl:block" />
         <SideBar className="relative flex h-full w-full items-center justify-end xl:hidden" />
-        <main className="min-h-0 w-full overflow-y-auto rounded-3xl bg-[#F8F8F8]">{children}</main>
+        <main className="h-full w-full overflow-y-auto rounded-3xl bg-[#F8F8F8]">{children}</main>
         <SideBar className="relative hidden h-full w-full flex-col items-center justify-center xl:flex xl:px-2" />
         <NavigationBar className="block h-full w-full xl:hidden" />
       </div>
