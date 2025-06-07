a4bcb91f200506e84a5d607ee661c2f65790f6d6
lee0jae330 <xingxing2001@cau.ac.kr>
2025-06-07 14:35:37 +0900

    feat: 게시글 관련 action 드롭다운 메뉴 추가

---
+++ src/fsd_widgets/post/ui/PostActionDropdown/PostActionDropdown.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
+
+import { EllipsisVertical } from 'lucide-react';
+
+import { useDeletePost, useSubscribePost, useUnsubscribePost } from '@/fsd_entities/post';
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/shadcn/components/ui';
+
+interface PostActionDropdownProps {
+  postId: Post.PostDto['id'];
+  isPostSubscribed: Post.PostDto['isPostSubscribed'];
+  isOwner: Post.PostDto['isOwner'];
+  isPostForm: Post.PostDto['isPostForm'];
+  formData: Post.PostDto['formResponseDto'];
+}
+
+export const PostActionDropdown = ({
+  postId,
+  isPostSubscribed,
+  isOwner,
+  isPostForm,
+  formData,
+}: PostActionDropdownProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { mutate: deletePost } = useDeletePost();
+  const { mutate: unsubscribePost } = useUnsubscribePost();
+  const { mutate: subscribePost } = useSubscribePost();
+
+  const dropdownMenuList: { text: string; handleClick: () => void }[] = [
+    { text: '삭제하기', handleClick: () => deletePost({ postId }) },
+    {
+      text: isPostSubscribed ? '알람 끄기' : '알람 켜기',
+      handleClick: () => {
+        isPostSubscribed ? unsubscribePost({ postId }) : subscribePost({ postId });
+      },
+    },
+    ...(isOwner && isPostForm
+      ? [
+          {
+            text: '신청 현황 보기',
+            handleClick: () => router.push(`${pathname}/formInfo/${formData?.formId}`),
+          },
+        ]
+      : []),
+  ];
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {/* Shadcn 버튼 사용 시 asChild 사용 불가로 인한 일반 button 사용 */}
+        <button className="absolute top-4 right-4 cursor-pointer">
+          <EllipsisVertical className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {dropdownMenuList.map((menu) => (
+          <DropdownMenuItem key={menu.text} onClick={menu.handleClick}>
+            {menu.text}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
