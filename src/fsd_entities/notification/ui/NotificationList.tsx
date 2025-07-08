2c5b11c51b9f88e19439b337be3cb8612976eea8
Rael <emilyekos@gmail.com>
2025-07-09 03:59:42 +0900

    fix: 알림 리스트 클릭 시 notificationType에 따라 동적 라우팅 처리 추가 #161

--- src/fsd_entities/notification/ui/NotificationList.tsx
+++ src/fsd_entities/notification/ui/NotificationList.tsx
@@ -1,55 +1,46 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import unReadMessage from '../../../../public/icons/unread_message.png';
 
 interface NotificationListProps {
-  notifications: Notification[];
+  notifications: Notification.Notification[];
+  notificationType: 'general' | 'ceremony';
   markAsRead: (id: string) => void;
-  matchedBoards: {
-    notificationLogId: string;
-    boardId: string;
-    targetId: string;
-  }[];
 }
 
-const getNotificationRoute = (
-  notification: Notification,
-  matchedBoards: { notificationLogId: string; boardId: string; targetId: string }[],
-) => {
-  const match = matchedBoards.find((m) => m.notificationLogId === notification.notificationLogId);
+export const NotificationList = ({ notifications, notificationType, markAsRead }: NotificationListProps) => {
+  const router = useRouter();
 
-  if (notification.noticeType === 'BOARD' && match?.boardId) {
-    return `/board/${match.boardId}/${notification.targetId}`;
-  }
-  if (notification.noticeType === 'CEREMONY') {
-    return `/ceremony/${notification.targetId}`;
-  }
-
-  return '/';
-};
-
-export const NotificationList = ({ notifications, markAsRead, matchedBoards }: NotificationListProps) => {
   return (
     <ul className="mt-3 space-y-2 rounded-lg bg-gray-200 p-1">
       {notifications.map((notification) => {
-        const matchedBoard = getNotificationRoute(notification, matchedBoards);
+        const targetLink =
+          notificationType === 'general'
+            ? `/board/${notification.targetParentId}/${notification.targetId}`
+            : notification.targetId
+              ? `/ceremony/${notification.targetId}`
+              : `/ceremony/${notification.notificationLogId}`;
 
         return (
-          <li key={notification.notificationLogId} className="rounded-lg bg-white p-1">
-            <Link
-              href={matchedBoard}
-              className="flex items-center gap-3"
-              onClick={() => markAsRead(notification.notificationLogId)}
-            >
-              <Image src={unReadMessage} alt="읽지 않은 알림 아이콘" className="h-5 w-6 pl-1 pt-1" />
+          <li
+            key={notification.notificationLogId}
+            className="cursor-pointer rounded-lg bg-white p-1"
+            onClick={() => {
+              if (!notification.isRead) {
+                markAsRead(notification.notificationLogId);
+              }
+              router.push(targetLink);
+            }}
+          >
+            <div className="flex items-center gap-3">
+              <Image src={unReadMessage} alt="읽지 않은 알림 아이콘" className="h-5 w-6 pt-1 pl-1" />
               <div className="flex flex-col text-sm text-gray-600">
                 <p className="text-l text-black">{notification.title}</p>
                 <p>{notification.body}</p>
               </div>
-            </Link>
+            </div>
           </li>
         );
       })}
