e6f067d9865d68e4378fdaf175a87ecb5b7510ba
lee0jae330 <xingxing2001@cau.ac.kr>
2025-07-10 20:29:44 +0900

    feat: 게시글 생성 관련 버튼 fixed에서 grid로 위치 조정

--- src/fsd_widgets/post/ui/PostCreationFormButtonGrorup/PostCreationFormButtonGroup.tsx
+++ src/fsd_widgets/post/ui/PostCreationFormButtonGrorup/PostCreationFormButtonGroup.tsx
@@ -27,24 +27,22 @@ export const PostCreationFormButtonGroup = ({ handleSubmit }: PostCreationFormBu
   };
 
   return (
-    <div className="fixed bottom-[100px] left-1/2 -translate-x-1/2 xl:bottom-4 xl:left-1/2 xl:space-x-24">
-      <div className="flex grow grid-cols-4 justify-center gap-2 xl:space-x-4">
-        <input type="file" ref={fileInputRef} className="hidden" onChange={handleFileChange} />
-        <button
-          className={`bg-comment-input flex w-16 items-center justify-center rounded-full md:w-20 md:p-3`}
-          onClick={handleFileUploadButtonClick}
-        >
-          <ImageIcon className="h-6 w-6 md:h-8 md:w-8" />
-        </button>
-        <VoteToggle />
-        <ApplicationFormToggle />
-        <button
-          onClick={handleSubmit}
-          className="rounded-full bg-[#F04C23] px-4 py-2 text-sm text-white shadow-md hover:bg-orange-600 focus:outline-hidden md:px-6 md:text-xl xl:ml-20 xl:px-8"
-        >
-          글작성
-        </button>
-      </div>
+    <div className="flex grow grid-cols-4 justify-center gap-2 py-2 xl:gap-4">
+      <input type="file" ref={fileInputRef} className="hidden" onChange={handleFileChange} />
+      <button
+        className={`bg-comment-input flex w-16 items-center justify-center rounded-full md:w-20 md:p-3`}
+        onClick={handleFileUploadButtonClick}
+      >
+        <ImageIcon className="h-6 w-6 md:h-8 md:w-8" />
+      </button>
+      <VoteToggle />
+      <ApplicationFormToggle />
+      <button
+        onClick={handleSubmit}
+        className="rounded-full bg-[#F04C23] px-4 py-2 text-sm text-white shadow-md hover:bg-orange-600 focus:outline-hidden md:px-6 md:text-xl xl:ml-20 xl:px-8"
+      >
+        글작성
+      </button>
     </div>
   );
 };
