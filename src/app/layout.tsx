83507f88ccb2282398ac73300bb14e35d42ef59d
kyh0726 <younghyun75398@gmail.com>
2025-07-15 23:05:53 +0900

    hotfix: theme-color 흰색으로 강제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <meta name="color-scheme" content="light only" />
+          <meta name="theme-color" content="#ffffff" />
           <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
