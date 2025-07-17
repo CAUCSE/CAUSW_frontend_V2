a43e8416cc2e9854a6e1a48514ab284f301555e8
kyh0726 <younghyun75398@gmail.com>
2025-07-17 15:53:51 +0900

    hotfix: 구버전 메타태그 제거 & viewport 객체 export 방식으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
         <head>
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
@@ -157,5 +156,9 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#ffffff',
 };
