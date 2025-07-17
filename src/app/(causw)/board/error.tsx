149e325f10a508bd0dff6377214751ad7df30754
lee0jae330 <xingxing2001@cau.ac.kr>
2025-07-17 22:10:56 +0900

    fix: 에러 페이지 레이아웃 변경

--- src/app/(causw)/board/error.tsx
+++ src/app/(causw)/board/error.tsx
@@ -5,12 +5,10 @@ import Image from 'next/image';
 import HomeLayout from '@/app/(causw)/layout';
 
 const Error = () => (
-  <HomeLayout>
-    <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
-      <Image src="/images/puang-proud.png" alt="404" width={200} height={250}></Image>
-      <span>게시판 조회 도중 에러가 발생했습니다.</span>
-    </div>
-  </HomeLayout>
+  <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center text-xl font-bold">
+    <Image src="/images/puang-proud.png" alt="404" width={200} height={250}></Image>
+    <span>게시판 조회 도중 에러가 발생했습니다.</span>
+  </div>
 );
 
 export default Error;
