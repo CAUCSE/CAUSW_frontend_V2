73a72f3338fbd0a6db9535864eaf36d682e2d9c7
kyh0726 <younghyun75398@gmail.com>
2025-07-20 01:47:58 +0900

    chore: 기존의 로그인 훅 제거

--- src/fsd_shared/hooks/index.ts
+++ src/fsd_shared/hooks/index.ts
@@ -1,6 +1,6 @@
-export { AuthService } from './AuthService';
 export { useInfiniteScroll } from './useInfiniteScroll';
 export { usePreviousValue } from './usePreviousValue';
 export { useIsWindowLg } from './useIsWindowLg';
 export { useTruncateParagraph } from './useTruncateParagraph';
 export { useHasAuth } from './useHasAuth';
+export { useErrorHandler } from './useErrorHandler';
