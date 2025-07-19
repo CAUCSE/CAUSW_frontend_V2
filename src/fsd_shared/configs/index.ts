96eb3b33ce5b5f6ced10ff3154ca76c4b4aeeb4e
kyh0726 <younghyun75398@gmail.com>
2025-07-19 21:50:40 +0900

    feat: export 수정

--- src/fsd_shared/configs/index.ts
+++ src/fsd_shared/configs/index.ts
@@ -1,7 +1,6 @@
 export * from './roles';
 export * from './errorCode';
 export * from './layout';
-export * from './regex';
 export * from './query-key';
 export * from './constants';
 export * from './api';
