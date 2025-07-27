b822210515207fee82bbe0b3ca8555fb60e9afb5
Rael <emilyekos@gmail.com>
2025-07-28 01:29:25 +0900

    feat: 새로운 게시판 UI 구현을 위한 css 수정 #223

--- src/fsd_widgets/board/ui/BoardList/CustomBoard.tsx
+++ src/fsd_widgets/board/ui/BoardList/CustomBoard.tsx
@@ -1,7 +1,7 @@
 import { Board } from '@/fsd_entities/board';
 
 export const CustomBoard = ({ boardInfos }: { boardInfos: Board.BoardResponseDto[] }) => (
-  <div className="grid w-[calc(100%-20px)] grid-cols-1 gap-x-5 gap-y-5 px-2 py-10 lg:grid-cols-2 lg:gap-y-10 xl:px-10">
+  <div className="grid w-full grid-cols-1 gap-5 px-4 py-6 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10 lg:px-28">
     {boardInfos.map((boardInfo, idx) => (
       <Board
         boardId={boardInfo.boardId}
--- src/fsd_widgets/board/ui/BoardList/DefaultNoticeBoard.tsx
+++ src/fsd_widgets/board/ui/BoardList/DefaultNoticeBoard.tsx
@@ -1,7 +1,7 @@
 import { Board } from '@/fsd_entities/board';
 
 export const DefaultNoticeBoard = ({ boardInfos }: { boardInfos: Board.BoardResponseDto[] }) => (
-  <div className="bg-board-background grid w-[calc(100%-20px)] grid-cols-1 gap-x-5 gap-y-5 rounded-2xl border border-red-500 p-10 lg:grid-cols-2 lg:gap-y-10">
+  <div className="grid w-full grid-cols-1 gap-4 rounded-xl px-4 py-6 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10 lg:px-28">
     {boardInfos.map((boardInfo, idx) => (
       <Board
         boardId={boardInfo.boardId}
