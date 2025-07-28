4f854d66fa90d9e9f319ce97bede8a74ac009583
kyh0726 <younghyun75398@gmail.com>
2025-07-28 11:52:15 +0900

    hotfix: signin input 자동 대문자 비활성

--- src/fsd_entities/auth/ui/SignInInput.tsx
+++ src/fsd_entities/auth/ui/SignInInput.tsx
@@ -14,6 +14,7 @@ export const SignInInput = <T extends FieldValues>({ register, name, rules, ...r
     {...register(name, rules)}
     {...rest}
     id="specificInput"
+    autoCapitalize='none'
     className="peer mt-1 h-10 w-56 rounded-xl border-2 border-focus bg-black text-center text-sm text-white opacity-60 placeholder:text-center placeholder:text-xs autofill:bg-black autofill:text-white autofill:shadow-none sm:w-80 sm:placeholder:text-sm"
   />
 );
