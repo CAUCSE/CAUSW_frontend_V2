07e80334026b94b6e045d679cd20bc56784b83fb
Rael <emilyekos@gmail.com>
2025-07-29 06:18:46 +0900

    feat: feat: BoardList에서 사용자 역할과 게시판 리스트 서버 패칭 추가 #223

--- src/fsd_widgets/board/ui/BoardList/BoardList.tsx
+++ src/fsd_widgets/board/ui/BoardList/BoardList.tsx
@@ -1,38 +1,30 @@
-import Link from 'next/link';
-
 import { fetchBoardList } from '@/fsd_entities/board';
 import { getMyRoles } from '@/fsd_entities/user/api';
 
-import { CustomBoard } from './CustomBoard';
-import { DefaultNoticeBoard } from './DefaultNoticeBoard';
+import { BoardListClient } from './BoardListClient';
 
 export const BoardList = async () => {
   const roles = await getMyRoles();
-  const { sortedBoardList, defaultBoardForAdmin, defaultBoardForCommon, customBoardForAdmin, customBoardForCommon } =
-    await fetchBoardList();
+  const {
+    defaultBoardForAdmin,
+    defaultBoardForCommon,
+    customBoardForAdmin,
+    customBoardForCommon,
+    defaultBoardForGraduate,
+    customBoardForGraduate,
+    sortedBoardList,
+  } = await fetchBoardList();
 
   return (
-    <>
-      <div className="h-full w-full py-2">
-        <div className="flex flex-col items-center">
-          {sortedBoardList.filter((board) => board.isDefault).length > 0 &&
-            (roles.includes('ADMIN') ? (
-              <DefaultNoticeBoard boardInfos={defaultBoardForAdmin} />
-            ) : (
-              <DefaultNoticeBoard boardInfos={defaultBoardForCommon} />
-            ))}
-          {roles.includes('ADMIN') ? (
-            <CustomBoard boardInfos={customBoardForAdmin} />
-          ) : (
-            <CustomBoard boardInfos={customBoardForCommon} />
-          )}
-        </div>
-      </div>
-      <Link href={`/board/create`}>
-        <button className="fixed right-6 bottom-24 h-16 w-16 transform rounded-[50px] bg-[#7AB6C1] px-6 py-3 text-3xl font-normal text-white shadow-lg hover:bg-[#5F8E97] xl:right-96 xl:bottom-10 xl:h-24 xl:w-24">
-          +
-        </button>
-      </Link>
-    </>
+    <BoardListClient
+      roles={roles}
+      defaultBoardForAdmin={defaultBoardForAdmin}
+      defaultBoardForCommon={defaultBoardForCommon}
+      customBoardForAdmin={customBoardForAdmin}
+      customBoardForCommon={customBoardForCommon}
+      defaultBoardForGraduate={defaultBoardForGraduate}
+      customBoardForGraduate={customBoardForGraduate}
+      sortedBoardList={sortedBoardList}
+    />
   );
 };
