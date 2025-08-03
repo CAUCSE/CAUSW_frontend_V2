45fe8668960c8dbd9f89f4bebc12a06ca7d99235
lee0jae330 <xingxing2001@cau.ac.kr>
2025-08-03 23:27:03 +0900

    chore: 게시글 신청서 결과 조회 페이지 이전 버튼 레이아웃 조정

--- src/app/(causw)/board/[boardId]/[postId]/formInfo/[formId]/page.tsx
+++ src/app/(causw)/board/[boardId]/[postId]/formInfo/[formId]/page.tsx
@@ -8,18 +8,14 @@ const FormInfoPage = () => {
   const resultView = useFormResultStore((state) => state.resultView);
 
   return (
-    <>
-      <PreviousButton />
-      {
-        <>
-          <FormResultHeader />
-          <main className="flex h-[calc(100%-6rem)] w-full flex-col items-center gap-8 overflow-auto pt-4 pb-10">
-            {resultView === 'summary' && <FormSummaryResult />}
-            {resultView === 'detail' && <FormDetailResult />}
-          </main>
-        </>
-      }
-    </>
+    <div className="h-full w-full pt-3">
+      <PreviousButton className="pl-5" />
+      <FormResultHeader />
+      <main className="flex h-[calc(100%-6rem)] w-full flex-col items-center gap-8 overflow-auto pt-4 pb-10">
+        {resultView === 'summary' && <FormSummaryResult />}
+        {resultView === 'detail' && <FormDetailResult />}
+      </main>
+    </div>
   );
 };
 
