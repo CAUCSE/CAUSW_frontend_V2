49d30218bd963cb9b5bba83f258758b5c6296cd6
kyh0726 <younghyun75398@gmail.com>
2025-08-06 17:49:50 +0900

    feat: 기존의 notificationStore 제거

--- src/fsd_entities/notification/model/index.ts
+++ src/fsd_entities/notification/model/index.ts
@@ -1,5 +1,4 @@
 export * from './usePushNotification';
-export * from './notificationStore';
 export * from '../hooks/queries/useNotificationQuery';
 export * from './useNotificationTabParam';
 export * from './notificationType';
