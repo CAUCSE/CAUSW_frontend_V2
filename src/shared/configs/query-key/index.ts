0189c6b0c0bcc61d228d6d41a01342593fe63ec8
kyh0726 <younghyun75398@gmail.com>
2025-08-13 00:54:30 +0900

    feat: 불필요한 쿼리키 싹다 정리

--- src/shared/configs/query-key/index.ts
+++ src/shared/configs/query-key/index.ts
@@ -1,19 +1,5 @@
-import { bannerQueryKey } from './bannerQueryKey';
-import { boardQueryKey } from './boardQueryKey';
-import { eventQueryKey } from './eventQueryKey';
 import { formQueryKey } from './formQueryKey';
-import { lockerQueryKey } from './lockerQueryKey';
-import { postQueryKey } from './postQueryKey';
-import { settingQueryKey } from './settingQueryKey';
-import { userQueryKey } from './userQueryKey';
 
 export {
-  bannerQueryKey,
-  boardQueryKey,
-  eventQueryKey,
   formQueryKey,
-  lockerQueryKey,
-  postQueryKey,
-  settingQueryKey,
-  userQueryKey,
 };
