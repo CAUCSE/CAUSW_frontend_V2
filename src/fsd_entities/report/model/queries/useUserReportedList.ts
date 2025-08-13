8080e9b7292deda340b2c17a4a7cb9a5c78f3777
Rael <emilyekos@gmail.com>
2025-08-14 06:23:45 +0900

    feat: 특정 유저의 신고 게시글/댓글 관리 훅 #242

---
+++ src/fsd_entities/report/model/queries/useUserReportedList.ts
@@ -0,0 +1,19 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { getUserReportedComments, getUserReportedPosts } from '../../api/get';
+import { ReportedComment, ReportedPost } from '../../config';
+import { reportQueryKey } from '../../config/queryKey';
+
+type ReportType = 'post' | 'comment';
+
+export function useUserReportedList(type: ReportType, userId: string, pageNum = 0) {
+  return useQuery<ReportedPost[] | ReportedComment[]>({
+    queryKey:
+      type === 'post' ? reportQueryKey.userPosts(userId, pageNum) : reportQueryKey.userComments(userId, pageNum),
+    queryFn: () => (type === 'post' ? getUserReportedPosts(userId, pageNum) : getUserReportedComments(userId, pageNum)),
+    enabled: !!userId,
+    staleTime: 30_000,
+  });
+}
