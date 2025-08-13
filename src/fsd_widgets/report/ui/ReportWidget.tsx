cc5628ae47e38bf280af673762a528fde39eaf19
Rael <emilyekos@gmail.com>
2025-08-14 06:22:15 +0900

    feat: ReportList 재사용을 위한 props 분리 #242

--- src/fsd_widgets/report/ui/ReportWidget.tsx
+++ src/fsd_widgets/report/ui/ReportWidget.tsx
@@ -2,19 +2,26 @@
 
 import { useState } from 'react';
 
+import { useSearchParams } from 'next/navigation';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ReportList } from '@/fsd_widgets/report/ui/ReportList';
 import { ReportTabs } from '@/fsd_widgets/report/ui/ReportTabs';
 
+import { useReportedList } from '@/fsd_entities/report';
+
 export function ReportWidget() {
   const [client] = useState(() => new QueryClient());
+  const type = (useSearchParams().get('type') ?? 'post') as 'post' | 'comment';
+  const pageNum = 0;
+  const queryResult = useReportedList(type, pageNum);
 
   return (
     <QueryClientProvider client={client}>
       <div className="min-h-screen max-w-md">
         <ReportTabs />
-        <ReportList />
+        <ReportList {...queryResult} />
       </div>
     </QueryClientProvider>
   );
