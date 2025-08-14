ec36e2e13796a39c099d4874f73fad93eae0a352
iris-starry <iris_starry_@naver.com>
2025-08-14 13:12:18 +0900

    fix: 설정 → 비밀번호 변경 후 페이지 이동

--- src/fsd_entities/auth/model/hooks/useResetPasswordForm.ts
+++ src/fsd_entities/auth/model/hooks/useResetPasswordForm.ts
@@ -1,9 +1,10 @@
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
-
+import { useRouter } from 'next/navigation';
 import { resetPassword } from '../../api/post';
 
 export const useResetPasswordForm = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -17,6 +18,9 @@ export const useResetPasswordForm = () => {
       const { confirmPassword, ...submitData } = data;
       await resetPassword(submitData);
       toast.success('비밀번호가 성공적으로 변경되었습니다.');
+
+      router.push('/setting');
+
       setTimeout(() => {
         reset();
       }, 300);
