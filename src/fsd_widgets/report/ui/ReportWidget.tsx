669d5b98988696a7508d48666646f08aacf691f2
Rael <emilyekos@gmail.com>
2025-08-14 12:53:06 +0900

    config: 파일 정리 #242

--- src/fsd_widgets/report/ui/ReportWidget.tsx
+++ src/fsd_widgets/report/ui/ReportWidget.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-
 import { useSearchParams } from 'next/navigation';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ReportList } from '@/fsd_widgets/report/ui/ReportList';
 import { ReportTabs } from '@/fsd_widgets/report/ui/ReportTabs';
@@ -12,17 +8,14 @@ import { ReportTabs } from '@/fsd_widgets/report/ui/ReportTabs';
 import { useReportedList } from '@/fsd_entities/report';
 
 export function ReportWidget() {
-  const [client] = useState(() => new QueryClient());
   const type = (useSearchParams().get('type') ?? 'post') as 'post' | 'comment';
   const pageNum = 0;
   const queryResult = useReportedList(type, pageNum);
 
   return (
-    <QueryClientProvider client={client}>
-      <div className="min-h-screen max-w-md">
-        <ReportTabs />
-        <ReportList {...queryResult} />
-      </div>
-    </QueryClientProvider>
+    <div className="min-h-screen max-w-md">
+      <ReportTabs />
+      <ReportList {...queryResult} />
+    </div>
   );
 }
