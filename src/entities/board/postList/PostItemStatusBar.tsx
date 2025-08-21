741f8a0b90022d27c28f4d0c2d62e9097c0a9077
causyj <jainayj804998@gmail.com>
2025-08-22 00:48:53 +0900

    fix: post list item status bar layout

--- src/entities/board/postList/PostItemStatusBar.tsx
+++ src/entities/board/postList/PostItemStatusBar.tsx
@@ -1,35 +1,46 @@
-import { Icon } from '@/fsd_shared';
+import clsx from 'clsx';
+import { MessageCircle, Star, ThumbsUp } from 'lucide-react';
+
+import { Divider } from '@/fsd_shared';
 import { getTimeDifference } from '@/utils/format';
+
+import FormIcon from '../../../../public/icons/form_icon.svg';
+import VoteIcon from '../../../../public/icons/vote_icon.svg';
 
 interface PostItemStatusBarProps {
   post: Post.PostResponseDto;
 }
-
+const totalCount = (count: number) => {
+  return count > 999 ? '999+' : count;
+};
 export const PostItemStatusBar = ({ post }: PostItemStatusBarProps) => {
   return (
-    <div className="flex divide-x-2">
+    <div className="flex items-center gap-2">
       <div className="flex gap-2 lg:gap-4">
-        <div className="flex items-center gap-1 sm:gap-2">
-          <Icon iconName="like" />
-          <p className="text-red-500">{post.numLike > 999 ? '999+' : post.numLike}</p>
+        <div className="flex items-center gap-1 text-red-500 sm:gap-2">
+          <ThumbsUp size={16} />
+          <p className="">{totalCount(post.numLike)}</p>
         </div>
-        <div className="hidden items-center gap-2 sm:flex">
-          <Icon iconName="scrap" />
-          <p className="text-yellow-500">{post.numFavorite > 999 ? '999+' : post.numFavorite}</p>
+        <div className="hidden items-center gap-1 text-yellow-500 sm:flex">
+          <Star size={16} />
+          <p>{totalCount(post.numFavorite)}</p>
         </div>
-        <div className="flex items-center gap-2 pr-2">
-          <Icon iconName="comment" />
-          <p className="text-blue-300">{post.numComment > 999 ? '999+' : post.numComment}</p>
+        <div className="flex items-center gap-1 text-blue-300 sm:gap-2">
+          <MessageCircle size={16} />
+          <p>{totalCount(post.numComment)}</p>
         </div>
       </div>
-      <div className="flex items-center gap-2 px-2">
-        <Icon iconName={post.isPostVote ? 'vote_active' : 'vote_inactive'} />
-        <Icon iconName={post.isPostForm ? 'apply_active' : 'apply_inactive'} />
+      <Divider vertical />
+      <div className="flex items-center gap-2">
+        <VoteIcon className={clsx(post.isPostVote ? 'text-[#FFC71B]' : 'text-[#B4B1B1]', 'h-4 w-4')} />
+        <FormIcon className={clsx(post.isPostForm ? 'text-[#FFC71B]' : 'text-[#B4B1B1]', 'h-4 w-4')} />
       </div>
-      <div className="sm:text-md flex items-center gap-2 px-2 text-center text-xs text-gray-300">
+      <Divider vertical />
+      <div className="sm:text-md flex items-center text-center text-xs text-gray-300">
         {getTimeDifference(post.createdAt)}
       </div>
-      <div className="sm:text-md flex items-center gap-2 px-2 text-center text-xs text-gray-300">
+      <Divider vertical />
+      <div className="sm:text-md flex items-center text-center text-xs text-gray-300">
         {post.displayWriterNickname ? post.displayWriterNickname : post.isAnonymous ? '익명' : post.writerNickname}
       </div>
     </div>
