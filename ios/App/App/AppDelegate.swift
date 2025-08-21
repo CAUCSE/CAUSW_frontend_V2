c4cb8cb75618b63f18d0f3bdd9f0437fdec677f9
causyj <jainayj804998@gmail.com>
2025-08-21 23:08:29 +0900

    chore: remove unnecessary comments on ios file(#286-2-fcm)

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -17,22 +17,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
     }
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
             
-            // 3. 알림이 잘 왔는지 확인하는 로그
+            //  알림이 잘 왔는지 확인하는 로그
             let userInfo = notification.request.content.userInfo
-            print("푸시 알림 수신 (앱 실행 중): \(userInfo)")
-            
+        
             // Firebase Messaging에 알림 정보를 전달합니다.
             Messaging.messaging().appDidReceiveMessage(userInfo)
             
             // 알림을 화면에 표시합니다.
             completionHandler([.alert, .sound, .badge])
         }
-    // 이 메서드는 사용자가 알림을 탭했을 때 호출됩니다.
+    // 사용자가 알림을 탭했을 때 호출됩니다.
        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
            
-           // 4. 알림 탭 액션 로그
+           //  알림 탭 액션 로그
            let userInfo = response.notification.request.content.userInfo
-           print("푸시 알림 탭 (앱 백그라운드): \(userInfo)")
+         
            
            // Firebase Messaging에 알림 정보를 전달합니다.
            Messaging.messaging().appDidReceiveMessage(userInfo)
@@ -40,23 +39,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
            completionHandler()
        }
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("푸시 알림 토큰을 성공적으로 받았습니다.")
-               print("토큰: \(deviceToken.description)")
+        
         Messaging.messaging().apnsToken = deviceToken
         Messaging.messaging().token(completion: { (token, error) in
             if let error = error {
-                print("Firebase Messaging 토큰 등록 실패: \(error.localizedDescription)")
-                            
                 NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
             } else if let token = token {
-                print("Firebase Messaging 토큰 등록 성공: \(token)")
                 NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: token)
             }
         })
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("푸시 알림 토큰 등록 실패: \(error.localizedDescription)")
+       
       NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
     }
     func applicationWillResignActive(_ application: UIApplication) {
