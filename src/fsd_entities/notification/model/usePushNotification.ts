6ef48c6b1a5452bd27ee80f25aba98cca6998e4b
causyj <jainayj804998@gmail.com>
2025-08-26 18:54:22 +0900

    chore: add comment (#295)

--- src/fsd_entities/notification/model/usePushNotification.ts
+++ src/fsd_entities/notification/model/usePushNotification.ts
@@ -2,8 +2,11 @@ import { PushNotifications } from '@capacitor/push-notifications';
 import { toast } from 'react-hot-toast';
 
 import { getRccRefresh, STORAGE_KEYS } from '@/fsd_shared/configs';
-import { detectDeviceType } from '@/fsd_shared/model';
+import { detectDeviceType, getClientFCMToken } from '@/fsd_shared/model';
 
+import { requestNotificationPermission } from '@/fsd_shared';
+
+import { getFCMToken } from '../api';
 import { useUpdateFCMToken } from '../hooks/mutations/useUpdateFCMToken';
 
 const FCM_TOKEN_KEY = STORAGE_KEYS.FCM_TOKEN;
@@ -46,6 +49,73 @@ export const usePushNotification = () => {
       toast.error('알림 설정에서 문제가 발생했습니다.');
     }
   };
+  // const compareFCMToken = async (): Promise<void> => {
+  //   try {
+  //     const deviceType = detectDeviceType();
+  //     if (deviceType === 'desktop') {
+  //       return;
+  //     }
+
+  //     // --- Capacitor 앱(iOS, Android) 로직 ---
+  //     // Capacitor 푸시 알림 플러그인을 사용하여 푸시 알림 토큰을 관리합니다.
+  //     if (deviceType === 'ios' || deviceType === 'ipad' || deviceType === 'android') {
+  //       const permStatus = await PushNotifications.checkPermissions();
+  //       if (permStatus.receive === 'prompt') {
+  //         await PushNotifications.requestPermissions();
+  //       }
+
+  //       PushNotifications.addListener('registration', async ({ value }) => {
+  //         const clientFCMToken = value;
+  //         const refreshToken = getRccRefresh();
+  //         const localFCMToken = localStorage.getItem(FCM_TOKEN_KEY);
+
+  //         if (localFCMToken !== clientFCMToken) {
+  //           updateFCMTokenMutation.mutate({ fcmToken: clientFCMToken, refreshToken: refreshToken || '' });
+  //           localStorage.setItem(FCM_TOKEN_KEY, clientFCMToken);
+  //         } else {
+  //           extendFCMTokenMutation.mutate({ fcmToken: clientFCMToken, refreshToken: refreshToken || '' });
+  //         }
+  //       });
+
+  //       PushNotifications.addListener('registrationError', (error) => {
+  //         toast.error('알림 설정에 실패했습니다.');
+  //       });
+
+  //       await PushNotifications.register();
+  //     } else {
+  //       // --- 웹/웹앱 로직 (Notification.permission 사용) ---
+  //       // 웹 브라우저의 기본 알림 API를 사용하여 푸시 알림을 관리합니다.
+  //       if (Notification.permission === 'default') {
+  //         const permission = await requestNotificationPermission();
+  //         if (permission !== 'granted') {
+  //           return;
+  //         }
+  //       } else if (Notification.permission === 'denied') {
+  //         return;
+  //       }
+
+  //       const clientFCMToken = await getClientFCMToken();
+  //       if (!clientFCMToken) {
+  //         toast.error('알림 설정 실패');
+  //         return;
+  //       }
+
+  //       const { fcmToken } = await getFCMToken();
+  //       const refreshToken = await getRccRefresh();
+
+  //       if (!fcmToken.includes(clientFCMToken)) {
+  //         if (localStorage.getItem(FCM_TOKEN_KEY) !== clientFCMToken) {
+  //           updateFCMTokenMutation.mutate({ fcmToken: clientFCMToken, refreshToken: refreshToken || '' });
+  //           localStorage.setItem(FCM_TOKEN_KEY, clientFCMToken);
+  //         } else if (localStorage.getItem(FCM_TOKEN_KEY) === clientFCMToken) {
+  //           extendFCMTokenMutation.mutate({ fcmToken: clientFCMToken, refreshToken: refreshToken || '' });
+  //         }
+  //       }
+  //     }
+  //   } catch (error) {
+  //     toast.error('알림 설정에서 문제가 발생했습니다.');
+  //   }
+  // };
   return {
     compareFCMToken,
   };
