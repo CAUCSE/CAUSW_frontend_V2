2d50ed6722d4cad6c2aad867be4f108d08e089a6
causyj <jainayj804998@gmail.com>
2025-08-31 23:17:06 +0900

    fix: change web icon(#311)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,76 +109,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicons/favicon-96x96.png', rel: 'icon' },
-      {
-        url: '/favicons/apple-icon-57x57.png',
-        sizes: '57x57',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-60x60.png',
-        sizes: '60x60',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-72x72.png',
-        sizes: '72x72',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-76x76.png',
-        sizes: '76x76',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-114x114.png',
-        sizes: '114x114',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-120x120.png',
-        sizes: '120x120',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-144x144.png',
-        sizes: '144x144',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-152x152.png',
-        sizes: '152x152',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/apple-icon-180x180.png',
-        sizes: '180x180',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicons/android-icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        rel: 'icon',
-      },
-      {
-        url: '/favicons/favicon-32x32.png',
-        sizes: '32x32',
-        type: 'image/png',
-        rel: 'icon',
-      },
-      {
-        url: '/favicons/favicon-96x96.png',
-        sizes: '96x96',
-        type: 'image/png',
-        rel: 'icon',
-      },
-      {
-        url: '/favicons/favicon-16x16.png',
-        sizes: '16x16',
-        type: 'image/png',
-        rel: 'icon',
-      },
+      { url: '/favicons/icon_512.png', sizes: '512x512', type: 'image/png', rel: 'icon' },
+      { url: '/favicons/icon_1024.png', sizes: '1024x1024', type: 'image/png', rel: 'icon' },
+      { url: '/favicons/icon_2732.png', sizes: '2732x2732', type: 'image/png', rel: 'icon' },
     ],
   },
 };
