4f0f3dbc7c300dba099b9e3d64955ae8c4211001
kyh0726 <younghyun75398@gmail.com>
2025-09-09 14:21:34 +0900

    chore: react-query-devtools 추가

--- src/fsd_widgets/pageLayout/ui/Providers.tsx
+++ src/fsd_widgets/pageLayout/ui/Providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -9,6 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
