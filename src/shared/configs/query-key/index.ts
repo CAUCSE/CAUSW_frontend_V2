eb4755eaf8e9b51e0233316dfec7adfda1b3924e
kyh0726 <younghyun75398@gmail.com>
2025-09-15 03:46:56 +0900

    chore: index 파일 정리

--- src/shared/configs/query-key/index.ts
+++ src/shared/configs/query-key/index.ts
@@ -1,7 +1,4 @@
-export { boardQueryKey } from './boardQueryKey';
-export { eventQueryKey } from './eventQueryKey';
 export { formQueryKey } from './formQueryKey';
 export { lockerQueryKey } from './lockerQueryKey';
-export { postQueryKey } from './postQueryKey';
 export { bannerQueryKey } from './bannerQueryKey';
 export { calendarQueryKey } from './calendarQueryKey';
