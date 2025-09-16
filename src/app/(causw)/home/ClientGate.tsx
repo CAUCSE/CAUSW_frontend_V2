0c82db6bd7f1c031b884ff6ea03fa900be4fa280
Rael <emilyekos@gmail.com>
2025-09-16 19:08:18 +0900

    fix: ClientGate에서 dynamic import 제거 후 일반 import로 교체하여 새로고침 시 깜빡임 현상 제거 #338

--- src/app/(causw)/home/ClientGate.tsx
+++ src/app/(causw)/home/ClientGate.tsx
@@ -1,23 +1,11 @@
-
-// 초기 렌더링 시점에 불러오지 않고, 클라이언트에서만 동적으로 불러옵니다.
-import dynamic from 'next/dynamic';
-
-// 클라이언트에서 비동기적으로 로드되는 동안 로딩 컴포넌트를 보여줍니다.
-import Loading from '@/app/loading';
-
 import { fetchMyInfo } from '@/entities/user/api';
 
-// 클라이언트 전용 페이지 컴포넌트
-const ClientHomePage = dynamic(() => import('./ClientPage'), { ssr: true, loading: () => <Loading /> });
-// 졸업생 전용 페이지 컴포넌트
-const ClientGraduatePage = dynamic(() => import('./ClientGraduatePage'), { ssr: true, loading: () => <Loading /> });
+import ClientGraduatePage from './ClientGraduatePage';
+import ClientHomePage from './ClientPage';
 
 export default async function ClientGate({ events }) {
-
   const user = await fetchMyInfo();
-  
-  const isGraduated = user.academicStatus === "GRADUATED";
+  const isGraduated = user.academicStatus === 'GRADUATED';
 
-  
   return isGraduated ? <ClientGraduatePage events={events} /> : <ClientHomePage events={events} />;
 }
