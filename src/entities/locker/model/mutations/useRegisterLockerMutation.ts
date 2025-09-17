25e178c2b61fb43783861b83b8ab4b354527dbf5
Rael <emilyekos@gmail.com>
2025-09-18 01:05:48 +0900

    feat: 토스트 메시지 추가 #338

--- src/entities/locker/model/mutations/useRegisterLockerMutation.ts
+++ src/entities/locker/model/mutations/useRegisterLockerMutation.ts
@@ -1,15 +1,32 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import toast from 'react-hot-toast';
+
+import { LOCKER_CONSTANT } from '@/shared';
 
 import { putLockerRegister } from '../../api';
 import { lockerQueryKey } from '../../config';
 
 export const useRegisterLockerMutation = (opts?: { onSuccess?: () => void }) => {
-  const qc = useQueryClient();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (lockerId: string) => putLockerRegister(lockerId),
-    onSuccess: () => {
-      qc.invalidateQueries({ queryKey: lockerQueryKey.all });
+    onSuccess: (res: AxiosResponse<{ myLocker?: { lockerNumber: string } }>) => {
+      queryClient.invalidateQueries({ queryKey: lockerQueryKey.all });
+
+      // 사물함 위치 정보 추후 추가 예정
+      // const { floor } = LOCKER_CONSTANT();
+      // const myLocker = res.data.myLocker;
+      // const location = myLocker
+      //   ? `${floor[myLocker.lockerNumber.split(' ')[0]]} ${myLocker.lockerNumber.split(' ')[1]}번`
+      //   : '없음';
+
+      toast.success(`사물함 등록이 완료되었습니다. 위치: ${location}`);
       opts?.onSuccess?.();
+    },
+
+    onError: () => {
+      toast.error('사물함 등록에 실패했습니다.');
     },
   });
 };
--- src/entities/locker/model/mutations/useReturnLockerMutation.ts
+++ src/entities/locker/model/mutations/useReturnLockerMutation.ts
@@ -1,14 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 import { putLockerReturn } from '../../api';
 import { lockerQueryKey } from '../../config';
 
 export const useReturnLockerMutation = () => {
-  const qc = useQueryClient();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (lockerId: string) => putLockerReturn(lockerId),
     onSuccess: () => {
-      qc.invalidateQueries({ queryKey: lockerQueryKey.all });
+      queryClient.invalidateQueries({ queryKey: lockerQueryKey.all });
+      toast.success('사물함 반납이 완료되었습니다.');
+    },
+    onError: () => {
+      toast.error('사물함 반납에 실패했습니다.');
     },
   });
 };
