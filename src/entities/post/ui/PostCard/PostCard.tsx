d7262ef50eafed0e1650fce077f00e11c0d4c316
Rael <emilyekos@gmail.com>
2025-09-28 00:12:43 +0900

    feat: 게시글 목록 UI 개선사항 반영 최종 #355

--- src/entities/post/ui/PostCard/PostCard.tsx
+++ src/entities/post/ui/PostCard/PostCard.tsx
@@ -16,29 +16,27 @@ export const PostCard = ({ post, targetUrl }: PostCardProps) => {
 
   return (
     <div
-      className="flex h-29.5 w-full items-center rounded-xl bg-white px-2.5 py-4 shadow-lg sm:h-42 md:p-6"
+      className="flex h-29.5 w-full items-center rounded-lg bg-white p-4 shadow-lg sm:h-42 md:p-6"
       onClick={() => {
         targetUrl ? router.push(targetUrl) : router.push(`${pathname}/${post.id}`);
       }}
     >
-      <div className="flex h-full w-full items-center justify-between gap-1.5 sm:gap-3">
-        <div className="grid h-full w-59 grid-rows-[1fr_1.5rem] sm:w-7/8">
-          <p className="text-xl font-bold md:text-[32px]">{post.title}</p>
-          <PostCardStatusBar post={post} />
+      <div className="grid h-full w-full grid-rows-[1fr_1.5rem]">
+        <div className="flex h-15 w-full justify-between md:h-21.25">
+          <p className="line-clamp-2 overflow-hidden text-xl font-bold break-all md:text-[32px]">{post.title}</p>
+          <div className="h-15 w-15 shrink-0 overflow-hidden rounded-lg sm:h-30 sm:w-30">
+            {post.postAttachImage && (
+              <Image
+                src={post.postAttachImage}
+                alt="post_thumbnail"
+                width={120}
+                height={120}
+                className="object-cover"
+              />
+            )}
+          </div>
         </div>
-        <div className="h-24 w-24 overflow-hidden rounded-xl sm:h-30 sm:w-30">
-          {post.postAttachImage ? (
-            <Image
-              src={post.postAttachImage}
-              alt="post_thumbnail"
-              width={120}
-              height={120}
-              className="rounded-xl object-cover"
-            />
-          ) : (
-            <div className="h-full w-full"></div>
-          )}
-        </div>
+        <PostCardStatusBar post={post} />
       </div>
     </div>
   );
