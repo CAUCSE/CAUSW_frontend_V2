cc8681b1cb91eab551bb7a19b840c14ed10d3614
kyh0726 <younghyun75398@gmail.com>
2025-10-04 20:41:43 +0900

    chore: 질문 글 여부 칼럼 조회해서 ui 표시하도록 수정

--- src/entities/post/ui/PostCard/PostCard.tsx
+++ src/entities/post/ui/PostCard/PostCard.tsx
@@ -26,6 +26,7 @@ export const PostCard = ({ post, targetUrl }: PostCardProps) => {
       <div className="grid h-full w-full grid-rows-[1fr_1.5rem]">
         <div className="flex h-15 w-full justify-between gap-[22px] md:h-[85px] md:gap-[30px]">
           <p className="line-clamp-2 h-14 text-xl font-bold md:h-20 md:text-[32px]">
+            {post.isQuestion ? '[질문] ' : ''}
             {post.title}
           </p>
           <div className="h-15 w-15 shrink-0 overflow-hidden rounded-lg sm:h-30 sm:w-30">
