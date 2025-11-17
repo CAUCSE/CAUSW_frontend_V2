d0237e40d8f13bba983a42b0c157e17bd949b74d
iris-starry <iris_starry_@naver.com>
2025-11-17 16:09:43 +0900

    fix: #392 모바일 CeremonySectionTitle 텍스트 잘림 현상 해결

--- src/entities/ceremony/ui/AdminCeremonySectionTitle.tsx
+++ src/entities/ceremony/ui/AdminCeremonySectionTitle.tsx
@@ -11,16 +11,16 @@ export const CeremonySectionTitle = ({
       className={clsx(
         title === MESSAGES.CEREMONY.DETAIL_CONTENTS
           ? 'flex flex-col gap-y-2'
-          : 'grid grid-cols-[auto_1fr] gap-x-2 md:block md:gap-y-2',
+          : 'flex flex-col gap-y-2 md:grid md:grid-cols-[auto_1fr] md:gap-x-2',
       )}
     >
-      <h1 className="min-w-[120px] text-lg font-semibold md:text-2xl">
+      <h1 className="text-lg font-semibold md:min-w-[120px] md:text-2xl">
         {title}
       </h1>
 
       <p
         className={clsx(
-          'text-xl font-medium break-keep whitespace-pre-line text-[#888888] md:text-[#000000]',
+          'text-xl font-medium break-all whitespace-pre-line text-[#888888] md:text-[#000000]',
           title === MESSAGES.CEREMONY.DETAIL_CONTENTS
             ? 'rounded-2xl border border-[#000000] p-6 md:border-[#d9d9d9]'
             : 'border-b border-[#000000] md:border-none',
